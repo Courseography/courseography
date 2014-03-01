e42d8352e6db9365a6626d2d24739ac10413a089
cygnet <istewartbinks@gmail.com>
2014-03-01 17:30:42 -0500

    Function for live search of timetable getting better.

--- timeTableRetriever.py
+++ timeTableRetriever.py
@@ -12,9 +12,10 @@ htmlData = html.read()
 course = re.compile('^CSC\d{3}(Y|H)1$')
 section = re.compile('^[FSY]{1}$')
 lecture = re.compile('^([TL]){1}[0123456789]{2}0{1}\d{1}$')
-time = re.compile('^[MWTRF]{1,3}\d{0,2}[-]?\d{0,2}$|Cancel|TBA')
+time = re.compile('^[MWTRF]{1,5}\d{0,2}[-]?\d{0,2}$|Cancel|TBA')
 instructor = re.compile('^\w\.|Tba|Staff')
-location = re.compile('^\w{2} \d{1,4}')
+location = re.compile('^\w{2} \d{1,4}|SS')
+roomChange = re.compile('change')
 
 # Different cases for extracted data
 class MyHTMLParser(HTMLParser):
@@ -26,8 +27,11 @@ class MyHTMLParser(HTMLParser):
     timeMemory = ''
     locationMemory = ''
     instructorMemory = ''
+    roomChangeMemory = False
+    roomSwitch = False
     switch = False
     fieldMemory = 0
+
     coloursToCourses = {
     	
     }
@@ -56,7 +60,9 @@ class MyHTMLParser(HTMLParser):
     	# The switch indicates that this field comes directly after the course field, 
     	# to prevent waitlist fields from being entered.
     	elif section.match(data) and self.switch:
-	        print('<tr class=timeTableRow><td class=timeTableCourse>' 
+	        print('<tr class="searchClass">'+ ' <tr class=timeTableRow><td class=timeTableCourse id='
+	        	+ self.courseMemory 
+	        	+' style="background: backgroundInstertion">' 
 	        	+ self.courseMemory 
 	        	+'</td><td class=timeTableBlockSection>' 
 	        	+ data 
@@ -64,11 +70,15 @@ class MyHTMLParser(HTMLParser):
 	        self.sectionMemory = data
 	        self.fieldMemory = 1
 	        self.switch = False
+    	elif "change" in data:
+    		self.roomChangeMemory = True
     	
     	# Checks if this is a lecture/tutorial, and remembers it.
     	elif lecture.match(data):
 	        self.typeMemory = data
 	        self.fieldMemory = 2
+	        self.roomChangeMemory = False
+	        self.roomSwitch = False
     	
     	# Checks if data is a time.
     	elif time.match(data):
@@ -77,7 +87,7 @@ class MyHTMLParser(HTMLParser):
 
 	        # For cancelled courses.
 	        if data == 'Cancel':
-	        	print('<tr class=timeTableRow><td class=timeTableBlock></td>'
+	        	print('<tr class=timeTableRow><td class=timeTableCourse>' + self.courseMemory + '</td>'
 	        		+'<td class=timeTableBlock></td><td class=timeTableBlockTypeLecture>' 
 	        		+ self.typeMemory +'</td><td class=timeTableBlockTime>' 
 	        		+ data
@@ -86,21 +96,37 @@ class MyHTMLParser(HTMLParser):
 	        # For Tutorials. (Either 'L'ecture or 'T'utorial) 
 	        # Time is the last field for a tutorial section.
 	        if self.typeMemory[0] == 'T':
-	            print('<tr class=timeTableRow><td class=timeTableBlock>'
-	            	+'</td><td class=timeTableBlock></td><td class=timeTableBlockTypeTutorial>'
+	            print('<tr class=timeTableRow><td class=timeTableCourse>'
+	            	 + self.courseMemory +'</td><td class=timeTableBlock></td><td class=timeTableBlockTypeTutorial>'
 	            	+ self.typeMemory 
 	            	+'</td><td class=timeTableBlockTime>' 
 	            	+ data
 	            	+'</td><td class=timeTableBlock></td><td class=timeTableBlock></td>')
-    	
+	        if self.roomChangeMemory == True:
+	        	self.roomSwitch = True
     	# Checks if data is a location.
     	elif location.match(data):
 	        self.locationMemory = data
+	        if self.roomSwitch == True:
+	        	print('<tr class=timeTableRow><td></td><td class=timeTableBlock></td>'
+	        		+'<td class=timeTableBlock>'
+	        		+ self.typeMemory 
+	        		+'</td><td class=timeTableBlock>' 
+	        		+ self.timeMemory
+	        		+ '</td><td class=timeTableBlock>'
+	        		+self.locationMemory
+	        		+'</td><td class=timeTableBlock>'
+	        		+ self.instructorMemory
+	        		+'</td>')
+	        self.roomChangeMemory = False
+	        self.roomSwitch = False
 	        self.fieldMemory = 4
     	
     	# Checks if data is an instructor.
     	elif instructor.match(data):
-	        print('<tr class=timeTableRow><td class=timeTableBlock></td>'
+	        print('<tr class=timeTableRow><td class=timeTableCourse>'
+	        	
+	        	+ self.courseMemory +'</td>' 
 	        	+'<td class=timeTableBlock></td><td class=timeTableBlockType>'
 	        	+ self.typeMemory 
 	        	+'</td><td class=timeTableBlockTime>' 
@@ -125,6 +151,6 @@ print('<tr class=timeTableRow>'
 	+ '<td class=timeTableHeader>Time</td>'
 	+ '<td class=timeTableHeader>Location</td>'
 	+ '<td class=timeTableHeader>Instructor</td>'
-	+ '</tr><div><p>')
+	+ '</tr><tr class="lonelyDiv">')
 parser.feed(str(htmlData))
-print('</p></div></table>')
+print('</tr></div></table>')
