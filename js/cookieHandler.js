e7aab0736faee936b1cc755c23e7d44d40d032af
cygnet <istewartbinks@gmail.com>
2014-05-16 23:47:29 -0400

    minor adjustments

--- js/cookieHandler.js
+++ js/cookieHandler.js
@@ -16,7 +16,9 @@ var ca = document.cookie.split(';');
 for(var i=0; i<ca.length; i++) 
   {
   var c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  if (c.indexOf(name)==0) {
+  return c.substring(name.length,c.length);
+    console.log("cookie" + c.substring(name.length,c.length));
   }
 return "inactive";
 }
