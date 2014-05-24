d4ce704746dec091edf9c3b5c81ae614eb9ebdd2
cygnet <istewartbinks@gmail.com>
2014-05-24 01:58:08 -0400

    update timetable creator

--- timetable_creator/timetable_util.js
+++ timetable_creator/timetable_util.js
@@ -8,6 +8,13 @@ xmlhttp.open("GET","../res/timetable2014.txt",false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML;
 var httpResponse = xmlhttp.responseText;
-//var csvNewline = csvText.split('\n');
-console.log(httpResponse);
 document.getElementById("content").innerHtml = httpResponse;
+var csvSplitNewline = httpResponse.split('\n');
+var i;
+contentString = "";
+for(i=0;i<csvSplitNewline.length;i++) {
+	if(csvSplitNewline[i].indexOf("CSC")>-1) {
+		contentString = consoleString + "<option>" + csvSplitNewline.substring(0,indexOf("CSC")+6) + "</option>";
+	}
+}
+console.log(contentString);
