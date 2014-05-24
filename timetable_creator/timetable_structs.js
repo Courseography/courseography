0f8239fb5289a80db0f519cfb4b3dfe746db970c
cygnet <istewartbinks@gmail.com>
2014-05-23 23:36:42 -0400

    init timetable creator

---
+++ timetable_creator/timetable_structs.js
@@ -0,0 +1,14 @@
+function Course() {
+	var lectures  = [];
+	var tutorials = []
+}
+
+function Lecture(title, time, assignedTutorial) {
+	var title           = title;
+	var time            = time;
+	var lectureTutorial = assignedTutorial;
+}
+
+function Tutorial(time) {
+	var time = time;
+}
---
+++ timetable_creator/timetable_util.js
@@ -0,0 +1,13 @@
+function Course() {
+	var lectures  = [];
+	var tutorials = []
+}
+
+function Lecture(time, assignedTutorial) {
+	var time            = time;
+	var lectureTutorial = assignedTutorial;
+}
+
+function Tutorial(time) {
+	var time = time;
+}
