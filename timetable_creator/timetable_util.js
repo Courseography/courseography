1feb0f8e6ac3ba15f7972a71372c0548faf5e5ab
cygnet <istewartbinks@gmail.com>
2014-05-24 02:19:04 -0400

    update timetable structure

--- timetable_creator/timetable_util.js
+++ timetable_creator/timetable_util.js
@@ -16,10 +16,20 @@ var csvSplitNewline = httpResponse.split('\n');
 var i;
 contentString = "";
 for(i=0;i<csvSplitNewline.length;i++) {
-	var splitLine = csvSplitNewline[i].split(',');
-	var course = splitLine[0];
-	if(course.indexOf("CSC")>-1) {
+	var splitLine  = csvSplitNewline[i].split(',');
+	var course     = splitLine[0];
+	var semester   = splitLine[1];
+	var title      = splitLine[2];
+	var section    = splitLine[3];
+	var time       = splitLine[4];
+	var type       = splitLine[5];
+	var instructor = splitLine[6];
+	var capacity   = splitLine[7];
+	if(course.indexOf("CSC")>-1 && contentString.indexOf(course) <= -1;) {
 		contentString = contentString + "<option value="+course+">" + course + "</option>";
+	} else if(type === 'L') { //Lecture
+
+	} else if(type === 'T') { //Tutorial
 
 	}
 }
