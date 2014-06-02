7eedb6c59b2b40651aef6107dc66e00228a22c47
cygnet <istewartbinks@gmail.com>
2014-06-01 20:20:25 -0400

    Hover function works, not all cases yet checked

--- timetable_creator/timetable_util.js
+++ timetable_creator/timetable_util.js
@@ -1,4 +1,4 @@
-// Get csv file
+
 if (window.XMLHttpRequest) {
   xmlhttp=new XMLHttpRequest();
 }
@@ -10,40 +10,145 @@ xmlhttp.send();
 xmlDoc=xmlhttp.responseXML;
 var result;
 var httpResponse = xmlhttp.responseText;
-console.log(result);
-document.getElementById("content").innerHTML = result; //Temp
 var csvSplitNewline = httpResponse.split('\n');
 var i;
 var courseSelect = document.getElementById("course-select");
 contentString = "";
 
 function addCourseToList(course) {
+	var courseNode = getCourse(course);
+	if(typeof(courseNode.F) !== "undefined" || typeof(courseNode.Y) !== "undefined") {
 		var entry    = document.createElement('li');
 		var header   = document.createElement('h3');
 		var sections = document.createElement('div');
-		var courseNode = getCourse(course);
 		header.appendChild(document.createTextNode(course));
-		header.setAttribute("class", "course");
+		entry.setAttribute("class", "course");
 		sections.id = course + "Sections";
 		var sectionList = document.createElement('ul');
 		var fall = document.createElement("li");
 		if(typeof(courseNode.F) !== "undefined") {
-		$.each(courseNode.F.lectures, function(i, lecture){
-			var section = document.createElement("li");
-			section.appendChild(document.createTextNode(lecture.section));
-			sectionList.appendChild(section);
-			console.log(lecture.section);
-		});
+			$.each(courseNode.F.lectures, function(i, lecture){
+				var section = document.createElement("li");
+				section.setAttribute("class", "section");
+				section.appendChild(document.createTextNode(lecture.section));
+				var sectionTimes = getSectionTimeSlot(lecture);
+				$.each(sectionTimes, function(i, time) {
+					console.log(time + " " + courseNode.name);
+					$(section).mouseover(function(){
+						document.getElementById(time).innerHTML = lecture.section;
+					});
+
+					$(section).mouseout(function(){
+						document.getElementById(time).innerHTML = "";
+					});
+
+				});
+
+				sectionList.appendChild(section);
+			});
+
+		} else {
+
+			$.each(courseNode.Y.lectures, function(i, lecture){
+				var section = document.createElement("li");
+				section.setAttribute("class", "section");
+				section.appendChild(document.createTextNode(lecture.section));
+				sectionList.appendChild(section);
+			});
 
 		}
+
 		sections.appendChild(sectionList);
 		entry.appendChild(header);
 		entry.appendChild(sections);
 		courseSelect.appendChild(entry);
 		return entry;
+	}
 }
 
-function addCourse() {
+var day           = /M|T|W|R|F/;
+var time          = /1|2|3|4|5|6|7|8|9/;
+var timeSecondDig = /0|1|2/;
+var hyphen        = /-/;
+var comma         = /,/;
+
+function getSectionTimeSlot(lecture) {
+	var firstTimeSlot;
+	var secondTimeSlot;
+	var thirdTimeSlot;
+	var timeLength = lecture.time.length;
+	var timeSlots  = [];
+	var times = lecture.time.split(',');
+	var lectureTimes = times[0];
+	$.each(times, function(i, lectureTime) {
+	if(lectureTime.charAt(0).match(day) !== null) {
+		firstTimeSlot = lectureTime.charAt(0);
+	}
+
+	if(lectureTime.charAt(1).match(day) !== null) {
+		secondTimeSlot = lectureTime.charAt(1);
+		if(lectureTime.charAt(2).match(day) !== null) {
+			thirdTimeSlot  = lectureTime.charAt(2).match(day);
+			firstTimeSlot  = firstTimeSlot  + lectureTime.charAt(3);
+			secondTimeSlot = secondTimeSlot + lectureTime.charAt(3);
+			thirdTimeSlot  = thirdTimeSlot  + lectureTime.charAt(3);
+			if(lectureTime.charAt(3) === "1" && lectureTime.charAt(4).match(timeSecondDig)) {
+				firstTimeSlot  = firstTimeSlot  + lectureTime.charAt(4);
+				secondTimeSlot = secondTimeSlot + lectureTime.charAt(4);
+				thirdTimeSlot = thirdTimeSlot + lectureTime.charAt(4);
+			}
+			timeSlots.push(firstTimeSlot);
+			timeSlots.push(secondTimeSlot);
+			timeSlots.push(thirdTimeSlot);
+
+		} else {
+			firstTimeSlot  = firstTimeSlot  + lectureTime.charAt(2);
+			secondTimeSlot = secondTimeSlot + lectureTime.charAt(2);
+
+
+			if(lectureTime.charAt(2) === "1"  && lectureTime.charAt(3).match(timeSecondDig)) {
+				firstTimeSlot  = firstTimeSlot  + lectureTime.charAt(3);
+				secondTimeSlot = secondTimeSlot + lectureTime.charAt(3);
+			}
+			timeSlots.push(firstTimeSlot);
+			timeSlots.push(secondTimeSlot);
+
+		}
+
+
+
+	} else if(lectureTime.charAt(1).match(time)) {
+		firstTimeSlot = firstTimeSlot + lectureTime.charAt(1);
+		if(lectureTime.charAt(1) === "1" && lectureTime.charAt(2).match(timeSecondDig)) {
+			firstTimeSlot = firstTimeSlot + lectureTime.charAt(2);
+			if(timeLength > 2 && lectureTime.charAt(3).match(hyphen)) {
+				console.log("parsed!:" + lectureTime.substring(1,3));
+				if(timeLength > 5 && firstTimeSlot.charAt(4) === "1") {
+					var difference = lectureTime.substring(4,6) - lectureTime.substring(1,3);
+
+				} else {
+					var difference = lectureTime.charAt(1) - (lectureTime.substring(1,3) - 12);
+				}
+			for(var i = 1; i < difference; i++) {
+					var newTime = parseInt(firstTimeSlot.substring(1,3)) + 1;
+					firstTimeSlot = firstTimeSlot.charAt(0) + newTime;
+					timeSlots.push(firstTimeSlot);
+				}
+			}
+		}
+		timeSlots.push(firstTimeSlot);
+	}
+
+	if(timeLength > 2 && lectureTime.charAt(2).match(hyphen)) {
+		var difference = lectureTime.charAt(3) - lectureTime.charAt(1);
+		for(var i = 1; i < difference; i++) {
+			var newTime = parseInt(firstTimeSlot.charAt(1)) + 1;
+			firstTimeSlot = firstTimeSlot.charAt(0) + newTime;
+			timeSlots.push(firstTimeSlot);
+		}
+	}
+});
+	return timeSlots;
 }
 
 function getCourse(courseCode) {
@@ -79,30 +184,10 @@ function addSectionsToList(course) {
 }
 
 function linkCourseToLI() {
-	$(".course").each(function() {
+	$(".course h3").each(function() {
 		$(this).data(this.innerHTML, getCourse(this.innerHTML));
-		$(this).mouseover(function(){document.getElementById("M9").innerHTML = $(this).data(this.innerHTML).F.lectures});
-		$(this).mouseout(function(){document.getElementById("M9").innerHTML = "mouse out!"});
-		getCourseTime(this);
 		addSectionsToList(this);
 	});
-}
-var time;
-var day = /M|T|W|R|F/;
-function getCourseTime(course) {
-	if(typeof($(course).data(course.innerHTML).F) === "undefined"){
-
-	} else {
-		time = $(course).data(course.innerHTML).F.lectures[0].time;
-		// console.log(time.length);
-		// console.log(time.charAt(0).match(day));
-		if(time.charAt(0).match(day) === null) {
-			console.log("WE FOUND A NULL");
-		} else {
-
-		}
-		console.log($(course).data(course.innerHTML).F.lectures[0].time);
-	}
 }
 
 function setupList() {
@@ -116,7 +201,6 @@ function setupList() {
 	var notYetLogged  = contentString.indexOf(course) <= -1;
 	if(isACourse && notYetLogged) {
 		addCourseToList(course);
-		console.log(getCourse(course).name);
 		contentString = contentString + course;
 	}
 }
@@ -125,5 +209,5 @@ function setupList() {
 $( document ).ready(function() {
     setupList();
 	linkCourseToLI();
-	$("#course-select").accordion({autoHeight:false});
+	$("#course-select").accordion({heightStyle: "content"});
 });
