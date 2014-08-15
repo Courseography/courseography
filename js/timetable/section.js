89fa5ce3fddb650ebf7ac7626a7af4ca89346287
david-yz-liu <david.yz.liu@gmail.com>
2014-08-15 12:37:01 -0400

    Minor tweaks

--- js/timetable/section.js
+++ js/timetable/section.js
@@ -67,7 +67,6 @@ Section.prototype.onclick = function () {
 
 
 Section.prototype.setTime = function (time) {
-    console.log(time);
     $(time).html(this.courseName)
            .attr("clicked", "true")
            .attr("type", this.type);
