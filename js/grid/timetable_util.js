de9c9ceba2d05479fdc8d7f00c27b7dc5fb8f3f6
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-09-07 17:45:10 -0400

    Modals: Create common util file.

--- js/grid/timetable_util.js
+++ js/grid/timetable_util.js
@@ -1,46 +1,3 @@
-/* Array utilities */
-function inArray(item, array) {
-    'use strict';
-
-    return $.inArray(item, array) > -1;
-}
-
-
-function removeFromArray(item, array) {
-    'use strict';
-
-    var index = array.indexOf(item);
-    array.splice(index, 1);
-    return index;
-}
-
-
-/* These specifically manipulate the two global arrays,
-courseObjects and selectedLectures. */
-function removeCourseObject(courseName) {
-    'use strict';
-
-    for (var i = 0; i < courseObjects.length; i++) {
-        if (courseName === courseObjects[i].name) {
-            courseObjects.splice(i, 1);
-            break;
-        }
-    }
-}
-
-
-function getCourseObject(courseName, courseArray) {
-    'use strict';
-
-    for (var i = 0; i < courseArray.length; i++) {
-        if (courseArray[i].name === courseName) {
-            return courseArray[i];
-        }
-    }
-    return undefined;
-}
-
-
 function updateSelectedLectures(sectionId) {
     'use strict';
 
@@ -68,37 +25,6 @@ function getVeryLargeCourseArray() {
     });
 
     return splitArray;
-}
-
-
-function fetchCourse(name) {
-    'use strict';
-
-    var course;
-    $.ajax({
-        url: 'res/courses/' + name + '.txt',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            course = data;
-        },
-        error: function () {
-            throw 'No course file';
-        }
-    });
-    courseCache.push(course);
-    return course;
-}
-
-
-function getCourse(name) {
-    'use strict';
-
-    var course = getCourseObject(name, courseCache);
-    if (course === undefined) {
-        course = fetchCourse(name);
-    }
-    return course;
 }
 
 
@@ -172,6 +98,7 @@ function refreshStarredCourses() {
         }
     });
 }
+
 
 /* Cookie Interaction */
 function restoreFromCookies() {
@@ -331,22 +258,4 @@ function renderClearCourseInformation() {
     $('#section-stats-section').empty();
     $('#section-stats-instructor').empty();
     $('#section-stats-enrol').empty();
-}
-
-
-// Parse times for cell ids.
-function convertTimes(times) {
-    'use strict';
-
-    var timeList = [];
-    var time;
-
-    for(var i = 0; i < times.length; i++) {
-        var timeString = 'MTWRF'.charAt(times[i][0]);
-        time = times[i][1];
-        timeString = timeString + time;
-        timeList.push(timeString);
-    }
-
-    return timeList;
 }
