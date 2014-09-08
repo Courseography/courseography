f345ffdaebdbaaed040287218bc0cd8eeb94872e
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-09-08 01:45:33 -0400

    Modals: Clean up style for modal.js, change " to ' where appropriate.

--- js/graph/modal.js
+++ js/graph/modal.js
@@ -4,23 +4,28 @@
 function createModalDiv(id) {
     'use strict';
 
-    var contentDiv = $("<div></div>");
+    var contentDiv = $('<div></div>');
     contentDiv.attr('id', 'modal-content-container');
 
     var courseDescription = fetchCourseDescription(id);
-    var p = $("<p></p>").css("color", "white").html(courseDescription);
-    contentDiv.append(p);
+    var p = $('<p></p>').css('color', 'white').html(courseDescription);
     var bottomContentDiv = $('<div></div>');
-    bottomContentDiv.attr('id', 'bottom-content-container')
     var video = setupVideoPlayer(id);
     var timetable = setupTimeslot(id);
     var relatedLinks = setupRelatedLinks(id);
+
+
+    bottomContentDiv.attr('id', 'bottom-content-container');
+
+    contentDiv.append(p);
     contentDiv.css('overflow', 'auto');
     contentDiv.append(timetable);
     contentDiv.append(bottomContentDiv);
+
     if (video) {
         bottomContentDiv.append(video);
     }
+
     bottomContentDiv.append(relatedLinks);
     return contentDiv;
 }
@@ -29,9 +34,9 @@ function createModalDiv(id) {
 function setupVideoPlayer(id) {
     'use strict';
 
-    var url = "http://www.cs.toronto.edu/~liudavid/" + id.toLowerCase().substring(0,6) + ".mp4";
-    var videoExists;
+    var url = 'http://www.cs.toronto.edu/~liudavid/' + id.toLowerCase() + '.mp4';
     var exists = urlExists(url);
+
     if (!exists) {
         return false;
     }
@@ -41,9 +46,9 @@ function setupVideoPlayer(id) {
             .css('float', 'left')
             .css('width', '100%');
     var video = $('<video id="course_video" class="video-js vjs-default-skin" controls preload="auto" width="100%" height="400"></video>');
-    var src = $("<source></source>")
-        .attr("src", url)
-        .attr("type", "video/mp4");
+    var src = $('<source></source>')
+        .attr('src', url)
+        .attr('type', 'video/mp4');
     video.append(src);
     videoDiv.append(video);
     return videoDiv;
@@ -87,24 +92,24 @@ function setupRelatedLinks(id) {
 function displayToolTip(nodeId) {
     'use strict';
 
-    var rectObject = $("#" + nodeId).find("rect");
+    var rectObject = $('#' + nodeId).find('rect');
 
     // The tooltip displays with a width of 222. If the node has an x position of
     // less than 222, the tooltip will be cut off by the svg boundaries. In this case,
     // we display the tooltip on the left.
-    var rightSide = rectObject.attr("x") > 222;
+    var rightSide = rectObject.attr('x') > 222;
 
     // The tooltip is offset with a 'padding' of 5.
     if (rightSide) {
-        var xPos = parseFloat(rectObject.attr("x")) - 65;
+        var xPos = parseFloat(rectObject.attr('x')) - 65;
     } else {
-        var xPos = parseFloat(rectObject.attr("x")) + 45;
+        var xPos = parseFloat(rectObject.attr('x')) + 45;
     }
 
-    var yPos = rectObject.attr("y");
+    var yPos = rectObject.attr('y');
     var g = createG(nodeId);
-    createRect(g, "node-tooltip", nodeId + "-tooltip", xPos, yPos, 60, 30, "black");
-    createText(g, nodeId, "node-tooltip", nodeId + "-tooltip", xPos, yPos, 60, 30, "black");
+    createRect(g, 'node-tooltip', nodeId + '-tooltip', xPos, yPos, 60, 30, 'black');
+    createText(g, nodeId, 'node-tooltip', nodeId + '-tooltip', xPos, yPos, 60, 30, 'black');
 }
 
 
@@ -122,17 +127,17 @@ function createRect(g, rectClass, rectId, posX, posY, width, height, color) {
     'use strict';
 
     var rect = $(document.createElementNS('http://www.w3.org/2000/svg', 'rect'))
-        .attr("class", rectClass + "-rect " + rectId + "-rect")
-        .attr("id", rectId + "-rect")
-        .attr("x", posX)
-        .attr("y", posY)
-        .attr("rx", 10)
-        .attr("ry", 10)
-        .attr("fill", "white")
-        .attr("stroke", color)
-        .attr("stroke-width", 2)
-        .attr("width", width)
-        .attr("height", height);
+        .attr('class', rectClass + '-rect ' + rectId + '-rect')
+        .attr('id', rectId + '-rect')
+        .attr('x', posX)
+        .attr('y', posY)
+        .attr('rx', 10)
+        .attr('ry', 10)
+        .attr('fill', 'white')
+        .attr('stroke', color)
+        .attr('stroke-width', 2)
+        .attr('width', width)
+        .attr('height', height);
 
     g.append(rect);
 
@@ -140,7 +145,7 @@ function createRect(g, rectClass, rectId, posX, posY, width, height, color) {
         function () {
         clearAllTimeouts();
     }, function () {
-        $('.tooltip-group').fadeOut(1000, function () {$(this).remove();});
+        $('.tooltip-group').fadeOut(1000, function () {$(this).remove(); });
     });
 }
 
@@ -160,11 +165,11 @@ function createText(g, nodeId, rectClass, rectId, posX, posY, width, height, col
     'use strict';
 
     var text = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
-        .text("Info")
-        .attr("class", rectClass + "-text " + rectId + "-text")
-        .attr("id", rectId + "-text")
-        .attr("x", parseFloat(posX)+ 30)
-        .attr("y", parseFloat(posY) + 20);
+        .text('Info')
+        .attr('class', rectClass + '-text ' + rectId + '-text')
+        .attr('id', rectId + '-text')
+        .attr('x', parseFloat(posX) + 30)
+        .attr('y', parseFloat(posY) + 20);
     g.append(text);
 }
 
@@ -174,7 +179,7 @@ function createG(nodeId) {
 
     var g = $(document.createElementNS('http://www.w3.org/2000/svg', 'g'));
     g.attr('class', 'tooltip-group')
-        .css("cursor", "pointer")
+        .css('cursor', 'pointer')
         .click(function () {
             openModal(nodeId);
         });
@@ -184,17 +189,17 @@ function createG(nodeId) {
 
 function openModal(nodeId) {
     'use strict';
-    
-    if ($(".modal").length === 0) {
+
+    if ($('.modal').length === 0) {
                 $('.infoTabs').hide();
                 var div = createModalDiv(nodeId);
                 div.attr('title', getCourseTitle(nodeId))
-                   .addClass("modal").dialog({
+                   .addClass('modal').dialog({
                         autoOpen: true,
                         modal: true,
                         minWidth: 1000,
                         minHeight: 600,
-                        closeText: "X",
+                        closeText: 'X',
                         open: function(event, ui) { $('.ui-widget-overlay').bind('click', function(){ div.dialog('close'); }); },
                         close: function () {
                             $(this).remove();
@@ -222,23 +227,9 @@ function enableVideoJS() {
 }
 
 
-function getVideo(id) {
-    'use strict';
-
-    var lowercaseId = id.toLowerCase();
-    $.ajax({
-        url: 'res/courses/' + name + '.txt',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-        }
-    });
-}
-
-
 function urlExists(url) {
     'use strict';
-    
+
     var exists;
     $.ajax({
         type: 'HEAD',
