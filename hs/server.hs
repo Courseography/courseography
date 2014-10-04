b4f1cade4e44e8fc028dbf16850810e5955995de
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-10-04 03:12:20 -0400

    happstack: Grid now gets courses correctly, more functions added.

--- hs/server.hs
+++ hs/server.hs
@@ -29,51 +29,28 @@ graphResponse =
     gridTemplate "Courseography - Grid"
                 [H.meta ! A.name "keywords"
                         ! A.content "happstack, david, liu",
-                 makeLink "stylesheet" "text/css" "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css",
-                 makeLink "stylesheet" "text/css" "static/style/grid/timetable_styles.css"
+                 insertTimetableLinks
                 ]
-                (do  makeDiv "" "row header" $ do
-                         makeDiv "header" "col-md-6 col-xs-6" $ do
-                                makeH2 "" "" "2014-2015 Timetable"
-                         makeDiv "" "col-md-6 col-xs-6" $ do
-                                makeA "" "" "static/planner.html" $ do
-                                    makeH2 "home-link" "" "Back to Graph"
+                (do  insertGridHeader
 
-                     makeDiv "dialog" "" "Conflicting courses are difficult to manage. Make sure you understand the added responsibility of having two or more conflicting courses."
+                     insertConflictDialog
 
                      makeDiv "" "row main" $ do
-                         makeDiv "course-select-wrapper" "col-md-2 col-xs-6" $ do
-                               makeUl "course-select" "trapScroll-enabled" $ do
-                                        makeLi "clear-all" "" $ do
-                                            makeH3 "" "" "Clear All"
+                         insertCoursePanel
 
-                         makeDiv "search-layout" "col-md-2 col-xs-6 col-md-push-8" $ do
-                                 makeDiv "filter-container" "" $ do
-                                     makeForm "" "" "return false;" $ do
-                                         makeInput "course-filter" "form-control" "Enter a course!" "off" "text"
-                                 makeDiv "search-container" "" $ do
-                                     makeDiv "search-list" "" ""
+                         insertSearchPanel
 
-                         makeDiv "" "col-md-8 col-xs-12 col-md-pull-2" $ do
-                             makeDiv "info" "row" $ do
-                                 makeDiv "info-layout" "" $ do
-                                     makeH2 "" "" $ do
-                                         makeSpan "course-info-code" "" ""
-                                         makeSpan "course-info-title" "" ""
-
-                                     makeH4 "" "" $ do
-                                         makeSpan "section-stats-section" "" ""
-                                         makeSpan "section-stats-instructor" "" ""
-
-                                     makeP "section-stats-enrol" "" ""
-
-                     -- Include all compiled plugins (below), or include individual files as needed -->
+                         insertInfoPanel
 
                      insertTimetableScripts
                          )
 
+-- Create <links/>
+insertTimetableLinks :: H.Html
+insertTimetableLinks = do makeLink "stylesheet" "text/css" "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+                          makeLink "stylesheet" "text/css" "static/style/grid/timetable_styles.css"
 
-
+-- Create <scripts/>
 insertTimetableScripts :: H.Html
 insertTimetableScripts = do insertjQuery
                             makeScript "static/js/grid/timetable_util.js"
@@ -87,8 +64,50 @@ insertTimetableScripts = do insertjQuery
                             makeScript "static/js/common/objects/section.js"
                             makeScript "static/js/common/utilities/util.js"
 
+-- Insert the header of the Grid. This contains the year of the timetable, and
+-- a link back to the Graph.
+insertGridHeader :: H.Html
+insertGridHeader =  makeDiv "" "row header" $ do
+                            makeDiv "header" "col-md-6 col-xs-6" $ do
+                                    makeH2 "" "" "2014-2015 Timetable"
+                            makeDiv "" "col-md-6 col-xs-6" $ do
+                                    makeA "" "" "static/planner.html" $ do
+                                          makeH2 "home-link" "" "Back to Graph"
+
+insertConflictDialog :: H.Html
+insertConflictDialog = makeDiv "dialog" "" "Conflicting courses are difficult to manage. Make sure you understand the added responsibility of having two or more conflicting courses."
+
+insertCoursePanel :: H.Html
+insertCoursePanel = makeDiv "course-select-wrapper" "col-md-2 col-xs-6" $ do
+                            makeUl "course-select" "trapScroll-enabled" $ do
+                                   makeLi "clear-all" "" $ do
+                                          makeH3 "" "" "Clear All"
+
+insertSearchPanel :: H.Html
+insertSearchPanel =  makeDiv "search-layout" "col-md-2 col-xs-6 col-md-push-8" $ do
+                             makeDiv "filter-container" "" $ do
+                                     makeForm "" "" "return false;" $ do
+                                     makeInput "course-filter" "form-control" "Enter a course!" "off" "text"
+                             makeDiv "search-container" "" $ do
+                                     makeDiv "search-list" "" ""
+
+insertInfoPanel :: H.Html
+insertInfoPanel = makeDiv "" "col-md-8 col-xs-12 col-md-pull-2" $ do
+                            makeDiv "info" "row" $ do
+                                    makeDiv "info-layout" "" $ do
+                                            makeH2 "" "" $ do
+                                                   makeSpan "course-info-code" "" ""
+                                                   makeSpan "course-info-title" "" ""
+
+                                            makeH4 "" "" $ do
+                                                   makeSpan "section-stats-section" "" ""
+                                                   makeSpan "section-stats-instructor" "" ""
+
+                                            makeP "section-stats-enrol" "" ""
+
 insertjQuery :: H.Html
 insertjQuery = makeScript "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"
+
 graph :: String
 graph = "planner.html"
 
@@ -99,11 +118,11 @@ static :: String
 static = "static"
 
 staticDir :: String
-staticDir = "/home/cynic/2/cscourseplanner"
+staticDir = "/home/cynic/courseography"
 
 main :: IO ()
 main = simpleHTTP nullConf $
   msum [ dir graph $ graphResponse,
          dir grid $ graphResponse,
-         dir static $ serveDirectory DisableBrowsing [] staticDir
+         dir static $ serveDirectory EnableBrowsing [] staticDir
        ]
