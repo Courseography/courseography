08b988c36d76c1f621bb785c6962333d1e4c615a
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-11-13 23:57:05 -0500

    happstack: Parser starting to consider regions.

---
+++ utilities/svg_parsing/region.py
@@ -0,0 +1,7 @@
+class Region:
+
+	def __init__(self, d):
+		self.d = d
+
+	def output_haskell(self):
+		print("S.path ! A.style \"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\" ! A.d \"" + self.d + "\"")
--- utilities/svg_parsing/svg_parser.py
+++ utilities/svg_parsing/svg_parser.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 import sys
 from path import *
 from rect import *
+from region import *
 paths = []
 rects = []
+regions = []
 
 def read_svg():
 	with open("../../graph_regions.svg", "r") as svg_file:
@@ -21,7 +23,11 @@ def output_svg():
 	print_header()
 	print("svgDoc :: S.Svg")
 	print("svgDoc = S.docTypeSvg ! A.version \"1.1\" ! A.width \"1052.3622\" ! A.height \"744.09448\" $ do")
-
+	
+	print("    S.g $ do")
+	for i in regions:
+		print("        ", end="")
+		i.output_haskell()
 
 	print("    S.g $ do")
 	for i in rects:
@@ -43,19 +49,25 @@ def print_header():
 
 
 def process_path(elem):
-			paths.append(Path(elem.get("d")))
+	if elem.parent.get("id") == "layer3":
+		regions.append(Region(elem.get("d")))
+	elif elem.parent.get("id") == "layer2":
+		pass
+	else:
+		paths.append(Path(elem.get("d")))
 
 def process_rect(elem):
-			rect = elem.parent.find_previous_sibling().find("rect")
-			if rect == None:
-				return
-			width = rect.get("width")
-			height = rect.get("height")
-			x = rect.get("x")
-			y = rect.get("y")
-			transform = elem.parent.get("transform")
-			text = elem.text
-			rects.append(Rect(width, height, x, y, transform, text))
+	rect = elem.parent.find_previous_sibling().find("rect")
+	if rect == None:
+		return
+	width = rect.get("width")
+	height = rect.get("height")
+	x = rect.get("x")
+	y = rect.get("y")
+	transform = elem.parent.get("transform")
+	text = elem.text
+	rects.append(Rect(width, height, x, y, transform, text))
+
 
 if __name__ == "__main__":
 	read_svg()
