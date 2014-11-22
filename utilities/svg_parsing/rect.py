d915ffc36beef181a0a108b766f76375647f60b2
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-11-21 23:13:48 -0500

    happstack: Transform now accounted for in x,y coordinates.

--- utilities/svg_parsing/rect.py
+++ utilities/svg_parsing/rect.py
@@ -3,16 +3,16 @@ class Rect:
 	def __init__(self, width, height, x, y, transform, style):
 		self.width =  width
 		self.height =  height
-		self.x =  x
-		self.y =  y
-		self.text_x = float(x) + (float(width)/2)
-		self.text_y =  float(y) + (float(height)/2)
+		self.x =  float(x) + float(transform[transform.find("(") + 1 : transform.find(",")])
+		self.y =  float(y) + float(transform[transform.find(",") + 1 : transform.find(")")])
+		self.text_x = float(self.x) + (float(width)/2)
+		self.text_y =  float(self.y) + (float(height)/2)
 		self.parent_transform =  transform
 		self.text = ""
 		self.style = style
 
 	def output_haskell(self):
-		print("S.g ! A.transform \"" + self.parent_transform + "\" ! A.style \"" + self.style + "\" $ do \n"  + 
+		print("S.g ! A.style \"" + self.style + "\" $ do \n"  + 
 			  "             S.rect" +
 			  " ! A.class_ \"node\"" +
 			  " ! A.id_ \"" + 
@@ -20,8 +20,8 @@ class Rect:
 			  "\"" +
 		      " ! A.width \"" + self.width +
 		      "\" ! A.height \"" + self.height + 
-		      "\" ! A.x \"" + self.x +
-		      "\" ! A.y \"" + self.y +
+		      "\" ! A.x \"" + str(self.x) +
+		      "\" ! A.y \"" + str(self.y) +
 		      "\" ! A.stroke \"#ccc\" ! A.fill \"#fff\" \n" + 
 		      "             S.text_ " +
 		      " ! A.x \"" + str(self.text_x) + 
@@ -29,8 +29,8 @@ class Rect:
 		      '" $ "'+ self.text + '"')
 
 	def __contains__(self, text):
-		dx = float(text.get("x")) - float(self.x)
-		dy = float(text.get("y")) - float(self.y)
+		dx = float(text.get("x")) + float(self.parent_transform[self.parent_transform.find("(") + 1 : self.parent_transform.find(",")]) - float(self.x)
+		dy = float(text.get("y")) + float(self.parent_transform[self.parent_transform.find(",") + 1 : self.parent_transform.find(")")]) - float(self.y)
 		offset = 9
 		return dx >= -1 * offset and \
 		       dx <= float(self.width) + offset and \
