c7e3ba2c74f1957cca26c47a9b6e0c5e87bd5d71
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-11-21 22:48:26 -0500

    happstack: Add persistent HS file.

---
+++ hs/database.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+import           Control.Monad.IO.Class  (liftIO)
+import           Control.Monad.Logger    (runStderrLoggingT)
+import           Database.Persist
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Courses
+    Department String
+    cNum Int
+    breadth Int
+    title String
+    description String
+    manualTutorialEnrolment Bool
+    manualPracticalEnrolment Bool
+    prereqs [String]
+    exclusions [String]
+    distribution String
+    prep String
+    deriving Show
+
+Lectures
+    Department String
+    cNum Int
+    session String
+    lId String
+    times [[Int]]
+    capacity Int
+    enrolled Int
+    waitlist Int
+    extra Int
+    location String
+    time_str String
+    deriving Show
+
+
+Tutorials
+    Department String
+    cNum Int
+    tId String
+    times [[Int]]
+    deriving Show
+
+Breadth
+    bId Int
+    description String
+    deriving Show
+|]
