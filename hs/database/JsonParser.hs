5c091ea32e81059b8a95754fe174ab139182c724
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-12-26 16:38:43 -0500

    database: Lecture insertion includes capacity.

--- hs/database/JsonParser.hs
+++ hs/database/JsonParser.hs
@@ -54,7 +54,7 @@ Lectures
     session Text
     section Text
     times [Time]
-    --capacity Int
+    capacity Int
     --enrolled Int
     --waitlist Int
     extra Int
@@ -219,6 +219,7 @@ insertLecture session course lecture = runSqlite dbStr $ do
                                                           session
                                                           (section lecture)
                                                           (Prelude.map Time (time lecture))
+                                                          (cap lecture)
                                                           (extra lecture)
                                                           (time_str lecture)
 
@@ -240,4 +241,4 @@ query = runSqlite dbStr $ do
         rawQuery sql [] $$ CL.mapM_ (liftIO . print)
 
 dbStr :: Text
-dbStr = "data23.sqlite3"
+dbStr = "data24.sqlite3"
