d21a9165d41e12393239f41e5a5df8e3b7e6b57c
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-13 22:09:01 -0500

    add-fb-login-button: Replace all tabs with spaces.

--- js/common/facebook_login.js
+++ js/common/facebook_login.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-	$.ajaxSetup({ cache: true });
-	$.getScript('//connect.facebook.net/en_UK/all.js', function() {
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_UK/all.js', function() {
 
-	    FB.init({
-	      appId: '442286309258193',
-	        xfbml      : true,
-	        version    : 'v2.1'
-	    });
+        FB.init({
+          appId: '442286309258193',
+            xfbml      : true,
+            version    : 'v2.1'
+        });
 
-	    FB.getLoginStatus(function (response) {
-		    if (response.status === 'connected') {
-		        console.log('Logged in.');
-		        console.log('Welcome!  Fetching your information.... ');
+        FB.getLoginStatus(function (response) {
+            if (response.status === 'connected') {
+                console.log('Logged in.');
+                console.log('Welcome!  Fetching your information.... ');
 
                 // Add the user's name to the navigation bar.
-		        FB.api('/me', function (response) {
-		            console.log('Successful login for: ' + response.name);
-		            $('#facebook-name').html(response.name);
-		        });
+                FB.api('/me', function (response) {
+                    console.log('Successful login for: ' + response.name);
+                    $('#facebook-name').html(response.name);
+                });
 
-		    } else {
-		        console.log('Not logged in.');
-		    }
-		});
-	});
+            } else {
+                console.log('Not logged in.');
+            }
+        });
+    });
 });
