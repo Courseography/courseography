80e49b93cf0cd01f4451500a46eb110e90b6fde9
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-13 19:19:13 -0500

    add-fb-login-button: Change app id to test app id.

--- js/common/facebook_login.js
+++ js/common/facebook_login.js
@@ -3,6 +3,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=432140593606098&version=v2.0";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=442286309258193&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
