73fd77838489199be342d50bc7936a5d7c1b13ce
Christine Murad <christine.murad@mail.utoronto.ca>
2015-01-17 16:46:29 -0500

    initial-layout: Specialist/Major/Minor sections are now divs that are toggled through javascript

---
+++ post/change_div.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$("#div_specialist, #div_major, #div_minor").hide();
+});
+
+
+$("#specialist").click(function (e) {
+	e.preventDefault();
+	$("#div_specialist, #div_major, #div_minor").hide();
+	$("#div_specialist").show();
+});
+		
+$("#major").click(function (e) {
+	e.preventDefault();
+	$("#div_specialist, #div_major, #div_minor").hide();
+	$("#div_major").show();
+});
+		
+$("#minor").click (function (e) {
+	e.preventDefault();
+	$("#div_specialist, #div_major, #div_minor").hide();
+	$("#div_minor").show();
+});
