de32147cc2fff0d4ed52300a821e1c1aa1e27c5a
Christine Murad <christine.murad@mail.utoronto.ca>
2015-01-19 00:45:40 -0500

    initial-layout: js code cleaning - single quotes and 'use-strict' declaration

--- post/change_div.js
+++ post/change_div.js
@@ -1,42 +1,58 @@
 $(document).ready(function() {
+	'use-strict';
+	
 	$("#div_specialist, #div_major, #div_minor").hide();
 	$(".reqs").hide();
 });
 
 
-$("#specialist").click(function (e) {
+$('#specialist').click(function (e) {
+	'use-strict';
+	
 	e.preventDefault();
-	$("#div_specialist, #div_major, #div_minor").hide();
-	$(".reqs").hide();
-	$("#div_specialist").show();
+	$('#div_specialist, #div_major, #div_minor').hide();
+	$('.reqs').hide();
+	$('#div_specialist').show();
 });
-		
-$("#major").click(function (e) {
+	
+	
+$('#major').click(function (e) {
+	'use-strict';
+	
 	e.preventDefault();
-	$("#div_specialist, #div_major, #div_minor").hide();
-	$(".reqs").hide();
-	$("#div_major").show();
+	$('#div_specialist, #div_major, #div_minor').hide();
+	$('.reqs').hide();
+	$('#div_major').show();
 });
-		
-$("#minor").click (function (e) {
+	
+	
+$('#minor').click (function (e) {
+	'user-strict';
+	
 	e.preventDefault();
-	$("#div_specialist, #div_major, #div_minor").hide();
-	$(".reqs").hide();
-	$("#div_minor").show();
+	$('#div_specialist, #div_major, #div_minor').hide();
+	$('.reqs').hide();
+	$('#div_minor').show();
 });
 
-$(".csc").click (function (e) {
+
+$('.csc').click (function (e) {
+	'use-strict';
+
 	e.preventDefault();
-	$(".reqs").hide();
-	if ($(this).parent().parent().parent().attr("id") == "div_specialist") {
-		$("#specialist_csc").show();
+	$('.reqs').hide();
+	if ($(this).parent().parent().parent().attr("id") == 'div_specialist') {
+		$('#specialist_csc').show();
 	}
 });
 
-$(".mat").click (function (e) {
+
+$('.mat').click (function (e) {
+	'use-strict';
+
 	e.preventDefault();
-	$(".reqs").hide();
-	if ($(this).parent().parent().parent().attr("id") == "div_specialist") {
-		$("#specialist_mat").show();
+	$('.reqs').hide();
+	if ($(this).parent().parent().parent().attr('id') == 'div_specialist') {
+		$('#specialist_mat').show();
 	}
 });
