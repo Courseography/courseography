03a3eb091f4f17737efd0b21f7fe3f5262f8a922
Christine Murad <christine.murad@mail.utoronto.ca>
2015-01-18 22:43:00 -0500

    initial-layout: added functionality to switch divs when correct category is clicked

--- post/change_div.js
+++ post/change_div.js
@@ -1,22 +1,42 @@
 $(document).ready(function() {
 	$("#div_specialist, #div_major, #div_minor").hide();
+	$(".reqs").hide();
 });
 
 
 $("#specialist").click(function (e) {
 	e.preventDefault();
 	$("#div_specialist, #div_major, #div_minor").hide();
+	$(".reqs").hide();
 	$("#div_specialist").show();
 });
 		
 $("#major").click(function (e) {
 	e.preventDefault();
 	$("#div_specialist, #div_major, #div_minor").hide();
+	$(".reqs").hide();
 	$("#div_major").show();
 });
 		
 $("#minor").click (function (e) {
 	e.preventDefault();
 	$("#div_specialist, #div_major, #div_minor").hide();
+	$(".reqs").hide();
 	$("#div_minor").show();
+});
+
+$(".csc").click (function (e) {
+	e.preventDefault();
+	$(".reqs").hide();
+	if ($(this).parent().parent().parent().attr("id") == "div_specialist") {
+		$("#specialist_csc").show();
+	}
+});
+
+$(".mat").click (function (e) {
+	e.preventDefault();
+	$(".reqs").hide();
+	if ($(this).parent().parent().parent().attr("id") == "div_specialist") {
+		$("#specialist_mat").show();
+	}
 });
