9df7d0bf666f10835f38f3bff7625d40d3537d6b
jmatholia <jmatholia@gmail.com>
2015-01-21 16:35:44 -0500

    experiemnt.js: onclick handelers for canvas and text

--- js/experiment.js
+++ js/experiment.js
@@ -1,12 +1,14 @@
-var c = document.getElementById("theCanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF00FF";
-ctx.fillRect(0, 0, 80, 100);
+function drawRect() {
+	var c = document.getElementById("theCanvas");
+	var ctx = c.getContext("2d");
+	ctx.fillStyle = "#FF00FF";
+	ctx.fillRect(0, 0, 80, 100);
+}
 
 $( "#target" ).click(function() {
   alert( "Handler for .click() called." );
 });
 
-//$( "#myCanvas" ).click(function() {
- // drawRect();
-//});
+$( "#theCanvas" ).click(function() {
+  drawRect();
+});
