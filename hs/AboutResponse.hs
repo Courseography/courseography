374fd962e18e62554302d5c2f061d4828d5f0380
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-23 20:33:13 -0500

    issue-285: Add doc to AboutResponse.hs.

--- hs/AboutResponse.hs
+++ hs/AboutResponse.hs
@@ -23,5 +23,7 @@ aboutResponse aboutContents =
                 )
                 ""
 
+-- | AboutHtml takes in the contents of the README.md file (the GitHub README file) and translates
+-- the markdown to blaze-HTML.
 aboutHtml :: String -> H.Html
 aboutHtml contents = H.div ! A.id "aboutDiv" $ writeHtml def $ readMarkdown def $ contents
