cc3f3740c6c7d1b1000d025727eb5f678f49550e
Christine Murad <christine.murad@mail.utoronto.ca>
2015-01-25 19:38:42 -0500

    add-courses: changed stylng and javascript so its apparent which tab you are currently viewing

--- post/change_div.js
+++ post/change_div.js
@@ -9,8 +9,20 @@ $('#specialist').click(function (e) {
     'use-strict';
 	
     e.preventDefault();
+	// reset nav bar colours
+	$('nav ul li').css('background-color', 'white');
+    $('nav > ul > li > a').css('color', 'black');
+
+	
+	// hide all divs
     $('#div_specialist, #div_major, #div_minor').hide();
+	
+	// show current div
     $('#div_specialist').show();
+	
+	// change current nav
+	$('#specialist').css('background-color', '#9C9C9C');
+    $('#specialist').css('#specialist > a').css('color', 'white');
 });
 	
 	
@@ -18,8 +30,19 @@ $('#major').click(function (e) {
     'use-strict';
 	
     e.preventDefault();
+	// reset nav bar colours
+	$('nav ul li').css('background-color', 'white');
+    $('nav > ul > li > a').css('color', 'black');
+	
+	// hide all divs
     $('#div_specialist, #div_major, #div_minor').hide();
+	
+	// show current div
     $('#div_major').show();
+	
+	// change current nav
+	$('#major').css('background-color', '#9C9C9C');
+    $('#major').css('#major > a').css('color', 'white');
 });
 	
 	
@@ -27,6 +50,19 @@ $('#minor').click (function (e) {
     'user-strict';
 	
     e.preventDefault();
+	// reset nav bar colours
+	$('nav ul li').css('background-color', 'white');
+    $('nav > ul > li > a').css('color', 'black');
+
+	
+	// hide all divs
     $('#div_specialist, #div_major, #div_minor').hide();
+	
+	// show current div
     $('#div_minor').show();
+	
+	// change current nav
+	$('#minor').css('background-color', '#9C9C9C');
+    $('#minor').css('#minor > a').css('color', 'white');
 });
+
