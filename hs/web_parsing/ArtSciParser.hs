58c518c7bfa86470ca72451937f7b99d7adb1443
shaftoes <samuel.shaftoe@mail.utoronto.ca>
2015-01-25 19:26:10 -0500

    cleaned up formatting

--- hs/web_parsing/ArtSciParser.hs
+++ hs/web_parsing/ArtSciParser.hs
@@ -6,6 +6,12 @@ import qualified Data.Text.IO as B
 import Data.List.Utils
 import Tables
 
+{----------------------------------------------------------------------------------------
+TODO
+    use Data.Text type instead of String for all functions
+    remove duplicates
+----------------------------------------------------------------------------------------}
+
 fasCalendarURL :: String
 fasCalendarURL = "http://www.artsandscience.utoronto.ca/ofr/calendar/"
 
@@ -17,14 +23,10 @@ toDelete = ["199299398399Big_Ideas_(Faculty_of_Arts_&_Science_Programs).html",
             "Joint_Courses.html",
             "Writing_in_the_Faculty_of_Arts_&_Science.html"]
 
-{-returns a list of strings, each element an html page referring to a specific department
-TODO:
-    remove the following html pages due ot redundency/uselessness
-        Writing in the Faculty of Arts and Science
-    removeDuplicates on list (many of the pages for smaller programs simply link to already seen pages)
-        e.g: semiotics, visual studies
-    ensure no duplicates via joint courses page
--}
+{----------------------------------------------------------------------------------------
+INPUT: 'processed' main page
+OUTPUT: a list of department html page names
+-----------------------------------------------------------------------------------------}
 getDeptList :: [Tag String] -> [String]
 getDeptList tags =
     let lists = sections (~== TagOpen "ul" [("class","simple")]) tags
@@ -33,15 +35,21 @@ getDeptList tags =
         rawList = map (\x -> case x of TagOpen "a" [("href", link)] -> link) as
     in nub $ filter (\dept -> not (dept `elem` toDelete)) rawList
 
-{-
-takes in a string representation of the ArtSci html page, converts it to a list of Tags (used in TagSoup), then calls getDeptlist to return a list of department pages in string format.
--}
+{----------------------------------------------------------------------------------------
+INPUT: a string representation of the ArtSci html page
+OUTPUT: a list of department html page names
+
+a simple 'preprocessing' function
+----------------------------------------------------------------------------------------}
 doStuff :: String -> [String]
 doStuff s = 
     let tags = parseTags s
     in  getDeptList tags
 
-{-Currently takes in an html filename of a department (which are found from getDeptList), and returns a list of all course related strings and tags found in that department-}
+{----------------------------------------------------------------------------------------
+INPUT: an html filename of a department (which are found from getDeptList)
+OUTPUT: a list of all course related strings and tags found in that department
+----------------------------------------------------------------------------------------}
 getCalendar :: String -> IO ()
 getCalendar str = do
     let path = fasCalendarURL ++ str
@@ -49,8 +57,11 @@ getCalendar str = do
               -- fetch document and return it (as a 'String'.)
     body <- getResponseBody rsp
     let tags = filter isComment $ parseTags body
+    --course description begins after the final h2 heading
     let coursesSoup = lastH2 tags
+    --partitions tags into sections, where each element starts with course name
     let courses = map (filter (~== TagText "")) $ partitions isCourseTitle coursesSoup
+    --converts course partitions into course Records
     let course = map (processCourseToData . filter (~/= TagText "\r\n")) courses
     mapM_ (B.putStrLn . name)  course
     --print  course
@@ -63,50 +74,15 @@ getCalendar str = do
         isCourseTitle (TagOpen _ attrs) = any (\x -> fst x == "name" && length (snd x) == 8) attrs
         isCourseTitle _ = False
 
-processCourse :: [Tag String] -> String
-processCourse tags =
-    let cleanTags = map cleanTag tags
-        title = find (~== TagText "") cleanTags
-        titleText = getTitleText title
-    in --[titleText]
-        --map getText cleanTags
-        titleText
-    where
-        getTitleText (Just (TagText s)) = replace "\160\160\160\160" " " s
-
-        cleanTag (TagText s) = TagText (replace "\r\n                   " " " s)
-
-        getText (TagText s) = s
-
-
-{-
-course codes can be found in body <a href="csc_...#asdf">
-only other links either contain utoronto, or maps.google in href property
--}
-{-
-getCrossListedCourses :: IO()
-getCrossListedCourses =  do
-    let path = fasCalendarURL ++ "Joint_Courses.html"
-    rsp <- simpleHTTP (getRequest path)
-              -- fetch document and return it (as a 'String'.)
-    body <- getResponseBody rsp
-    let tags  = partitions (isTagOpenName "a") (parseTags body)
-    let names = map (takeWhile (\x -> not (isTagClose x))) tags
-    let removeUseless = filter (\x -> not (isOtherLink (head x))) names
-    mapM_ (fromTagText . head . tail) removeUseless
-    where
-        isOtherLink (TagOpen _ [("href", link)]) = isInfixOf ".ca" link
-        isOtherLink (TagOpen _ [("name", _)]) = True
-        isotherLink _ = False
--}
-{-
-takes a list of tags representing a single course, and returns Course record with its information
-In general
--}
+{----------------------------------------------------------------------------------------
+INPUT: a list of tags representing a single course, 
+OUTPUT: Course 'record' containing course info
+----------------------------------------------------------------------------------------}
 processCourseToData :: [Tag String] -> Course
 processCourseToData tags  =
     let cleanTags = map cleanTag tags
         getTitle = find (~== TagText "") cleanTags
+        --splitat 8 since first 8 represnt course code
         courseNames = splitAt 8 $ removeTitleGarbage $ removeLectureSection $ getTitle
     in Course {breadth = Nothing, 
             description = Nothing, 
@@ -138,3 +114,41 @@ main = do
     let depts = doStuff body
     --getCalendar $ filter (== "crs_csc.htm") depts
     mapM_ getCalendar depts
+
+{----------------------------------------------------------------------------------------
+course codes can be found in body <a href="csc_...#asdf">
+only other links either contain utoronto, or maps.google in href property
+----------------------------------------------------------------------------------------}
+{-
+getCrossListedCourses :: IO()
+getCrossListedCourses =  do
+    let path = fasCalendarURL ++ "Joint_Courses.html"
+    rsp <- simpleHTTP (getRequest path)
+              -- fetch document and return it (as a 'String'.)
+    body <- getResponseBody rsp
+    let tags  = partitions (isTagOpenName "a") (parseTags body)
+    let names = map (takeWhile (\x -> not (isTagClose x))) tags
+    let removeUseless = filter (\x -> not (isOtherLink (head x))) names
+    mapM_ (fromTagText . head . tail) removeUseless
+    where
+        isOtherLink (TagOpen _ [("href", link)]) = isInfixOf ".ca" link
+        isOtherLink (TagOpen _ [("name", _)]) = True
+        isotherLink _ = False
+-}
+
+{----------------------------------------------------------------------------------------
+INPUT: a list of tags representing a single course
+OUTPUT: 
+----------------------------------------------------------------------------------------}
+processCourse :: [Tag String] -> String
+processCourse tags =
+    let cleanTags = map cleanTag tags
+        title = find (~== TagText "") cleanTags
+        titleText = getTitleText title
+    in --[titleText]
+        --map getText cleanTags
+        titleText
+    where
+        getTitleText (Just (TagText s)) = replace "\160\160\160\160" " " s
+        cleanTag (TagText s) = TagText (replace "\r\n                   " " " s)
+        getText (TagText s) = s
