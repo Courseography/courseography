b356c37e56622a2e28bc67340df3607cbde864f6
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-30 02:09:30 -0500

    init-parser: Made own function to parse HaXml Element.

--- hs/parser/parser.hs
+++ hs/parser/parser.hs
@@ -6,16 +6,23 @@ import Text.XML.HaXml.Wrappers
 import Text.XML.HaXml.Types
 import Text.XML.HaXml.Combinators
 import Text.XML.HaXml.Util
+import Text.XML.HaXml.XmlContent.Parser
 import Text.XML.HaXml.Namespaces
 
 main :: IO ()
 main = do x <- readFile "../../res/graphs/graph_regions.svg"
           let at = AttValue $ [Left "913.45227"]
           let y = xmlParse "output.error" x
-          let z = deep $ attrval (N "x", at)
-
+          let a = attrval (N "x", at)
+          let z = deep $ a
+          let c = head $ map contentElem $ parseContent z y
+          print $ parseContent z y
           print $ map tagTextContent $ parseContent z y
+          print $ getName c
 
 -- | I took this from onContent, and modified it quite a bit.
 parseContent :: CFilter i -> Document i -> [Content i]
 parseContent filter (Document p s e m) = filter (CElem e undefined)
+
+getName :: Element s -> QName
+getName (Elem a _ _) = a
