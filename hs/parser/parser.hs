d993ae7db1729dd6f1ffc9ce625802a8afc5515f
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-30 00:15:19 -0500

    init-parser: Can apply CFilter to Document!

--- hs/parser/parser.hs
+++ hs/parser/parser.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Text.XML.HaXml
+import Text.XML.HaXml.ByteStringPP
+import Text.XML.HaXml.Wrappers
+
 main :: IO ()
-main = print "Init"
+main = do x <- readFile "../../res/graphs/graph_regions.svg"
+          let y = xmlParse "output.error" x
+          let z = path [tag "svg"]
+          print $ onContent z y
+
