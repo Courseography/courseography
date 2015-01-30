465f428fc1bb9b283d08b97c730179d6c9adc8dd
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-30 00:54:16 -0500

    init-parser: Modify HaXml src to create function parseContent.

--- hs/parser/parser.hs
+++ hs/parser/parser.hs
@@ -7,6 +7,9 @@ import Text.XML.HaXml.Wrappers
 main :: IO ()
 main = do x <- readFile "../../res/graphs/graph_regions.svg"
           let y = xmlParse "output.error" x
-          let z = path [tag "svg"]
-          print $ onContent z y
+          let z = path [children, tag "g", children, tag "path"]
+          print $ parseContent z y
 
+-- | I took this from onContent, and modified it quite a bit.
+parseContent :: CFilter i -> Document i -> [Content i]
+parseContent filter (Document p s e m) = filter (CElem e undefined)
