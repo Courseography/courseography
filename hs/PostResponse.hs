2500dd2034afe94a57746e181eb47668c229555e
Christine Murad <christine.murad@mail.utoronto.ca>
2015-02-01 18:28:16 -0500

    move-to-haskell: Removed redundant '$ do's

--- hs/PostResponse.hs
+++ hs/PostResponse.hs
@@ -40,10 +40,8 @@ checkPost =  do
                 H.a ! A.href "" $ "Minor"
                 H.div ! A.id "min_creds" $ "(0/4.0)"
         H.div ! A.id "div_specialist" $ do
-            H.p $ do
-                "CSC108H (Introduction to Computer Programming)"
-            H.p $ do
-                "CSC148H (Introduction to Computer Science)"
+            H.p "CSC108H (Introduction to Computer Programming)"
+            H.p "CSC148H (Introduction to Computer Science)"
             H.p $ do
                 "CSC165H (Mathematical Expression and Reasoning for Computer Science)"
                 H.i " and "
@@ -58,12 +56,9 @@ checkPost =  do
                 "MAT137Y (Calculus)"
                 H.i " or "
                 "MAT157Y (Analysis 1)"
-            H.p $ do
-                "CSC207H (Software Design)"
-            H.p $ do
-                "CSC209H (Software Tools And System Programming)"
-            H.p $ do
-                "CSC258H (Computer Organization)"
+            H.p "CSC207H (Software Design)"
+            H.p "CSC209H (Software Tools And System Programming)"
+            H.p "CSC258H (Computer Organization)"
             H.p $ do
                 "CSC263H (Data Structures and Analysis)"
                 H.i " or "
@@ -78,21 +73,15 @@ checkPost =  do
                 "STA255H (Statistical Analysis)"
                 H.i " or "
                 "STA257H (Probability and Statistics 1)"
-            H.p $ do
-                "CSC369H (Operating Systems)"
-            H.p $ do
-                "CSC373H (Algorithm Design, Analysis, and Complexity)"
-            H.p $ do
-                "Any 400-level CSC course, BCB410H, BCB420H, BCB430Y (1.5 FCEs)"
-            H.p $ do
-                "Any 300-level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs)"
+            H.p "CSC369H (Operating Systems)"
+            H.p "CSC373H (Algorithm Design, Analysis, and Complexity)"
+            H.p "Any 400-level CSC course, BCB410H, BCB420H, BCB430Y (1.5 FCEs)"
+            H.p "Any 300-level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs)"
             H.p $ H.em "Any from this list: CSC301H, CSC318H, CSC404H, CSC411H, CSC418H, CSC420H, CSC428H, CSC454H, CSC485H, CSC490H, CSC491H, CSC494H (2.0 FCEs)"
             H.p $ H.em "No more than 1.0 FCEs from CSC490H, CSC491H, CSC494H, CSC495H, BCB430Y"
         H.div ! A.id "div_major" $ do
-            H.p $ do
-                "CSC108H (Introduction to Computer Programming)"
-            H.p $ do
-                "CSC148H (Introduction to Computer Science)"
+            H.p "CSC108H (Introduction to Computer Programming)"
+            H.p "CSC148H (Introduction to Computer Science)"
             H.p $ do
                 "CSC165H (Mathematical Expression and Reasoning for Computer Science)"
                 H.i " and "
@@ -107,10 +96,8 @@ checkPost =  do
                 "MAT137Y (Calculus)"
                 H.i " or "
                 "MAT157Y (Analysis 1)"
-            H.p $ do
-                "CSC207H (Software Design)"
-            H.p $ do
-                "CSC258H (Computer Organization)"
+            H.p "CSC207H (Software Design)"
+            H.p "CSC258H (Computer Organization)"
             H.p $ do
                 "CSC263H (Data Structures and Analysis)"
                 H.i " or "
@@ -121,24 +108,19 @@ checkPost =  do
                 "STA255H (Statistical Analysis)"
                 H.i " or "
                 "STA257H (Probability and Statistics 1)"
-            H.p $ do
-                "Any 400-level CSC course, BCB410H, BCB420H, BCB430Y (1.5 FCEs)"
-            H.p $ do
-                "Any 300-level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H, (1.5 FCEs)"
+            H.p "Any 400-level CSC course, BCB410H, BCB420H, BCB430Y (1.5 FCEs)"
+            H.p "Any 300-level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H, (1.5 FCEs)"
             H.p $ H.em "Any from this list: CSC301H, CSC318H, CSC404H, CSC411H, CSC418H, CSC420H, CSC428H, CSC454H, CSC485H, CSC490H, CSC491H, CSC494H (2.0 FCEs)"
             H.p $ H.em "No more than 1.0 FCEs from CSC490H, CSC491H, CSC494H, CSC495H, BCB430Y"
         H.div ! A.id "div_minor" $ do
-            H.p $ do
-                "CSC108H (Introduction to Computer Programming)"
-            H.p $ do
-                "CSC148H (Introduction to Computer Science)"
+            H.p "CSC108H (Introduction to Computer Programming)"
+            H.p "CSC148H (Introduction to Computer Science)"
             H.p $ do
                 "CSC165H (Mathematical Expression and Reasoning for Computer Science)"
                 H.i " and "
                 "CSC236H (Intro to the Theory of Computation)"
                 H.i " or "
                 "CSC240H (Enriched Intro to the Theory of Computation)"
-            H.p $ do
-                "Any 300/400-level CSC course (atleast 1.0 FCE), CSC209H, CSC258H, CSC263H/CSC265H (1.5 FCEs)"
+            H.p "Any 300/400-level CSC course (atleast 1.0 FCE), CSC209H, CSC258H, CSC263H/CSC265H (1.5 FCEs)"
 
                    
