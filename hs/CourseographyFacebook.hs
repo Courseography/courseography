39390f1437216788891a5c9aa87113e6968e0c89
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-01 01:24:11 -0500

    convert-svg-png: Add meeting changes, clean up code.

--- hs/CourseographyFacebook.hs
+++ hs/CourseographyFacebook.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, GADTs #-}
 module CourseographyFacebook where
 
 import qualified Facebook as FB
@@ -6,24 +6,24 @@ import Control.Monad.IO.Class  (liftIO)
 import qualified Data.Text as T
 import Happstack.Server
 import Happstack.Server.Internal.Multipart
-import Network.HTTP.Conduit (withManager, parseUrl, httpLbs, RequestBody(..))
+import Network.HTTP.Conduit (withManager, parseUrl, httpLbs)
 import Control.Monad.Trans.Resource
 import qualified Data.Conduit.List as CL
 import Data.Conduit
-import Data.Text.Encoding as TE
 import Database.Persist
 import ConvertSVGToPNG
 import JsonParser
 import System.Process
 import GraphResponse
 import GHC.IO.Exception
-import Data.Maybe
 import Tables
-import Text.Digestive.Form as TEF
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
 import Database.Persist.Sqlite
 import Network.HTTP.Client.MultipartFormData
+import Network.HTTP.Client (RequestBody(..))
+import Network (withSocketsDo)
+
 
 postFB :: String
 postFB = "post-fb"
@@ -44,25 +44,29 @@ url2 :: FB.RedirectUrl
 url2 = "http://localhost:8000/test-post"
 
 perms :: [FB.Permission]
-perms = []
+perms = ["publish_actions"]
+
+postPhoto :: FB.UserAccessToken -> IO Response
+postPhoto (FB.UserAccessToken _ b _) = withSocketsDo $ withManager $ \m -> do
+    fbpost <- parseUrl $ "https://graph.facebook.com/v2.2/me/photos?access_token=" ++ T.unpack b
+    flip httpLbs m =<<
+        (formDataBody [partBS "message" "Test Message",
+                       partFileSource "graph" "graph.png"]
+                      fbpost)
+    return $ toResponse postFB
 
 -- | Constructs the Facebook authorization URL. This method does not actually
 -- interact with Facebook.
 retrieveAuthURL :: T.Text -> IO String
 retrieveAuthURL url = 
-	performFBAction $ do
+    performFBAction $ do
         fbAuthUrl <- FB.getUserAccessTokenStep1 url perms
         return $ T.unpack fbAuthUrl
 
--- | The arguments passed to the API. The first argument is the key of the query data ('code')
--- and the second argument is the code that was retrieved in the first authorization step.
-args :: String -> String -> FB.Argument
-args arg1 arg2 = (BS.pack arg1, BS.pack arg2)
-
 -- | Retrieves the user's email.
-retrieveFBData :: String -> IO Response
+retrieveFBData :: BS.ByteString -> IO Response
 retrieveFBData code = 
-	performFBAction $ do
+    performFBAction $ do
         token <- getToken url1 code
         user <- FB.getUser "me" [] (Just token)
         liftIO $ insertIdIntoDb (FB.userId user)
@@ -71,7 +75,7 @@ retrieveFBData code =
 -- | Inserts a string into the database along with the current user's Facebook ID.
 insertIdIntoDb :: FB.Id -> IO ()
 insertIdIntoDb id_ = 
-	runSqlite fbdbStr $ do
+    runSqlite fbdbStr $ do
         runMigration migrateAll
         insert_ $ FacebookTest (show id_) "Test String"
         liftIO $ print "Inserted..."
@@ -85,34 +89,25 @@ performFBAction action = withManager $ \manager -> FB.runFacebookT app manager a
 -- | Posts a message to the user's Facebook feed, with the code 'code'. GraphResponse
 -- is then sent back to the user.
 postToFacebook :: String -> ServerPart Response
-postToFacebook code = (liftIO $ performPost code) >> graphResponse
+postToFacebook code = (liftIO $ performPost (BS.pack code)) >> graphResponse
 
 -- | Performs the posting to facebook.
-performPost :: String -> IO Response
+performPost :: BS.ByteString -> IO Response
 performPost code = do
-	x <- BS.readFile "test.png"
-	--let y = show $ BodyPart "source" x
-	performFBAction $ do
-        liftIO createPNGFile
-        postToFB (BS.unpack x) code =<< getToken url2 code
-        liftIO $ removePNG "test.png"
-        liftIO $ print "File Deleted."
+    createPNGFile
+    performFBAction $ do
+        token <- getToken url2 code
+        liftIO $ postPhoto token
         return $ toResponse postFB
 
 createPNGFile :: IO ExitCode
-createPNGFile =  do 
-                   (inp,out,err,pid) <- convertSVGToPNG "test.png" "../res/graphs/graph_regions.svg"
-                   liftIO $ waitForProcess pid
+createPNGFile =  do (inp,out,err,pid) <- convertSVGToPNG "graph.png" "../res/graphs/graph_regions.svg"
+                    liftIO $ waitForProcess pid
 
 -- | Gets a user access token.
-getToken :: (MonadResource m, MonadBaseControl IO m) => FB.RedirectUrl -> String -> FB.FacebookT FB.Auth m FB.UserAccessToken
-getToken url code = FB.getUserAccessTokenStep2 url [args "code" code]
-
--- | Posts a message to Facebook.
-postToFB :: (MonadResource m, MonadBaseControl IO m) => String -> String -> FB.UserAccessToken -> FB.FacebookT FB.Auth m FB.Id
-postToFB dataString code token = FB.postObject "me/photos" [args "message" "Test Post Pls Ignore",
-                                                            args "url" $ "http://localhost:8000/static/hs/test2.png"] token
+getToken :: (MonadResource m, MonadBaseControl IO m) => FB.RedirectUrl -> BS.ByteString -> FB.FacebookT FB.Auth m FB.UserAccessToken
+getToken url code = FB.getUserAccessTokenStep2 url [("code", code)]
 
 -- | Gets a users Facebook email.
 getEmail :: String -> ServerPart Response
-getEmail code = liftIO $ retrieveFBData code
+getEmail code = liftIO $ retrieveFBData (BS.pack code)
