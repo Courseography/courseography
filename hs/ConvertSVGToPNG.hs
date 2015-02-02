ea6f5ba9ed47a756be5e4cd433b7fa1bfcd6d58a
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-02 00:32:32 -0500

    convert-svg-png: Clean up ConvertSVGToPNG.hs.

--- hs/ConvertSVGToPNG.hs
+++ hs/ConvertSVGToPNG.hs
@@ -4,8 +4,9 @@ import System.Process
 import GHC.IO.Handle.Types
 import Control.Monad.IO.Class  (liftIO)
 import GHC.IO.Exception
-import System.IO (hFlush, hIsEOF, hShow, hReady, hGetContents)
 
+-- | Converts an SVG file to a PNG file. Note that image magik's 'convert' command
+-- can take in file descriptors.
 convertSVGToPNG :: String -> String -> IO
                      (Maybe Handle,
                       Maybe Handle,
@@ -22,6 +23,7 @@ convertSVGToPNG inName outName = createProcess $ CreateProcess
                                   CreatePipe
                                   False
                                   False
+-- | Removes a file.
 removePNG :: String -> IO
                      (Maybe Handle,
                       Maybe Handle,
@@ -38,7 +40,8 @@ removePNG name = createProcess $ CreateProcess
                                   False
                                   False
 
--- Note: hGetContents can be used to read Handles.
+-- Note: hGetContents can be used to read Handles. Useful when trying to read from
+-- stdout.
 createPNGFile :: String -> IO ExitCode
 createPNGFile uniqueName = do (inp, out, err, pid) <- convertSVGToPNG uniqueName "../res/graphs/graph_regions.svg"
                               liftIO $ print "Waiting for process..."
