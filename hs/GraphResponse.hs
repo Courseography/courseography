430b907dae7c22857ff130ba95669a013c15c5d6
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-03 04:01:00 -0500

    clean-up-parser: Remove tabs.

--- hs/GraphResponse.hs
+++ hs/GraphResponse.hs
@@ -31,38 +31,6 @@ fceCountDiv = createTag H.div "FCECountDiv" "" $ do
                   createTag H.span "FCEcount" "" "0.0"
                   "FCEs" -- Being difficult. Won't show up correctly.
 
-welcomeTab :: H.Html
-welcomeTab = createTag H.div "welcome" "infoTab" $
-                     createTag H.div "" "infoTabContent" $ do
-                         H.h2 "Welcome!"
-                         H.p $ do
-                             "The graph above displays the prerequisite links connecting courses "
-                             "in our department. Select courses to plan your enrolments for "
-                             "future terms! Courses that you've selected but have missing "
-                             "prerequisites will be highlighted in red. "
-
-                         H.p $ do
-                             "Check out the different tabs to access helpful features for your "
-                             "planning. Also, here's a"
-                             makeA "" "" "res/full_graph.jpg" "_blank" "printable version "
-                             "of the graph."
-
-                         --createTag H.br "" ""
-                         H.p $ do
-                             "Courseography is an ongoing "
-                             makeA "" "" "https://github.com/Ian-Stewart-Binks/courseography" "_blank" "project "
-                             "maintained by Ian Stewart-Binks and"
-                             makeA "" "" "http://www.cs.toronto.edu/~liudavid/" "_blank" " David Liu "
-                             "."
-                             "Ideas for new features, better design, and (especially) bug reports"
-                             "are always welcome!"
-                             "Please send all feedback to"
-                             makeA "" "" "mailto:cs.toronto.courseplanner@gmail.com" "_blank" " this address "
-                             "."
-                             "If you see a bug, please do let us know which browser and version you're using."
-                             "And if there's a display issue, giving us your screen display info"
-                             "(e.g., resolution) will be rather helpful. Thanks!"
-
 focusesTab :: H.Html
 focusesTab = ""
 
@@ -77,6 +45,4 @@ timetableTab = createTag H.div "timetable" "infoTab" $ do
                                makeInput "filter" "text-input" "" "" "text"
                            createTag H.div "timetableContainer" "" ""
 
-checkMyPostTab :: H.Html
-checkMyPostTab = ""
 
