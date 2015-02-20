e8b9fca63820aae2db42360172c72dfde7b0f1a3
foreverwander <foreverwander@users.noreply.github.com>
2015-02-19 22:27:06 -0500

    Expansion and compression of cells for half

--- js/grid/mouse_events.js
+++ js/grid/mouse_events.js
@@ -86,6 +86,14 @@ function alertUserOfConflict() {
  */
 function renderClearTime(time) {
     'use strict';
+    
+    var m;
+    
+    m= time.indexOf('E');
+    
+    if (m != -1) {
+        time = time.slice(0,m) + time.charAt(m+1);
+    }
 
     $(time).html('')
            .attr('clicked', 'false')
@@ -103,12 +111,27 @@ function renderClearTime(time) {
 function renderClearHover(time) {
     'use strict';
 
-    if ($(time).attr('clicked') !== 'true') {
-            $(time).html('');
-        }
-    $(time).attr('hover', 'off');
-}
+    var n;
+    var m;
 
+    n = time.indexOf('H');
+    m = time.indexOf('E');
+        
+    if (n != -1 && $(time).attr('clicked') !== 'true') {
+        compressCell(time.slice(2,n), time.charAt(1), time.charAt(n+1));
+    }
+    
+    if (m != -1) {
+        compressCell(time.slice(2,m), time.charAt(1), time.charAt(m+1));
+        time = time.slice(0,m) + time.charAt(m+1);
+    }
+    
+    if ($(time).attr('clicked') !== 'true') {
+        $(time).html('');
+    }
+    $(time).attr('hover', 'off');
+
+}
 
 /**
  * Renders a cell on hover.
@@ -119,49 +142,92 @@ function renderAddHover(time, section) {
     'use strict';
 
     var n;
+    var m;
 
     n = time.indexOf('H');
+    m = time.indexOf('E');
 
     if (n != -1) {
-        extendRow(time.slice(2,n));
-    } 
-
-    if ($(time).attr('clicked') !== 'true') {
-        $(time).html(section.courseName)
-               .attr('hover', 'good');
-    } else if ($(time).html() === section.courseName &&
-               $(time).attr('type') === section.type) {
-        $(time).attr('hover', 'remove');
+        extendCell(time.slice(2,n), time.charAt(1), time.charAt(n+1));
+        ptime = time.slice(0,n) + time.charAt(n+1);
+        
+        if ($(time).attr('clicked') !== 'true') {
+            $(time).html(section.courseName)
+                   .attr('hover', 'good');
+        } else if ($(time).html() === section.courseName &&
+                   $(time).attr('type') === section.type) {
+            $(time).attr('hover', 'remove');
+        } else if ($(ptime).attr('clicked') === 'true') {
+            $(time).attr('hover', 'conflict');
+        } else {
+            $(time).attr('hover', 'conflict');
+        }
+        
+    } else if (m != -1) {
+        extendCell(time.slice(2,m), time.charAt(1), time.charAt(m+1));
+        ptime = time.slice(0,m) + time.charAt(m+1);
+        
+        if ($(ptime).attr('clicked') !== 'true') {
+            $(ptime).html(section.courseName)
+                   .attr('hover', 'good');
+        } else if ($(ptime).html() === section.courseName &&
+                   $(ptime).attr('type') === section.type) {
+            $(ptime).attr('hover', 'remove');
+        } else {
+            $(ptime).attr('hover', 'conflict');
+        }
+        
     } else {
-        $(time).attr('hover', 'conflict');
+        
+        if ($(time).attr('clicked') !== 'true') {
+            $(time).html(section.courseName)
+                   .attr('hover', 'good');
+        } else if ($(time).html() === section.courseName &&
+                   $(time).attr('type') === section.type) {
+            $(time).attr('hover', 'remove');
+        } else {
+            $(time).attr('hover', 'conflict');
+        }
+        
     }
 }
+
 /**
- * Extends a given row to display half hour sections.
+ * Extends a given cell to display half hour sections.
  * @param {string} time The full hour time of the row.
+ * @param {string} week The week of the timetable cell.
+ * @param {string} term The term of the timetable cell.
  */
-function extendRow(time) {
+function extendCell(time, week, term) {
     'use strict';
 
-    var weekPrefixArray = ['M', 'T', 'W', 'R', 'F'];
-    var pcells = [];
-    var ccells = [];
-    var cellID;
+    var pcell;
+    var ccell;
 
-    for (var k = 0; k < 5; k++) {
-        cellID = '#' + weekPrefixArray[k] + time + 'F';
-        pcells[pcells.length] = cellID;
-        cellID = '#' + weekPrefixArray[k] + time + 'S';
-        pcells[pcells.length] = cellID;
-        cellID = '#' + weekPrefixArray[k] + time +'H' + 'F';
-        ccells[ccells.length] = cellID;
-        cellID = '#' + weekPrefixArray[k] + time +'H'+ 'S';
-        ccells[ccells.length] = cellID;
-    }
+    pcell = '#' + week + time + term;
+    ccell = '#' + week + time + term;
 
-    for (var i = 0; i < 10; i++) {
-        $(pcells[i]).attr('rowspan', '1');
-        $(ccells[i]).attr('display', 'table-cell');
-    }
+    $(pcell).attr('rowspan', '1');
+    $(ccell).attr('display', 'table-cell');
+
+}
+
+/**
+ * Compress a given cell to hide half hour sections.
+ * @param {string} time The full hour time of the row.
+ * @param {string} week The week of the timetable cell.
+ * @param {string} term The term of the timetable cell.
+ */
+function compressCell(time, week, term) {
+    'use strict';
+
+    var pcell;
+    var ccell;
+
+    pcell = '#' + week + time + term;
+    ccell = '#' + week + time + term;
+
+    $(pcell).attr('rowspan', '2');
+    $(ccell).attr('display', 'none');
 
 }
