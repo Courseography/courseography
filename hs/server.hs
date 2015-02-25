8a85e17350aeaca902635b4457c6de151261313d
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-25 01:53:03 -0500

    hs-dir-structure: Clean up server.

--- hs/server.hs
+++ hs/server.hs
@@ -24,14 +24,14 @@ main = do
 
         msum [ dir "grid" gridResponse,
                dir "graph" graphResponse,
-               dir "draw" $ drawResponse,
+               dir "draw" drawResponse,
                --dir "about" $ aboutResponse contents,
-               dir "post" $ postResponse,
+               dir "post" postResponse,
                dir "static" $ serveDirectory EnableBrowsing [] staticDir,
                dir "course" $ look "name" >>= retrieveCourse, 
                dir "all-courses" $ liftIO allCourses
                ]
 
 retrieveCourse :: String -> ServerPart Response
-retrieveCourse course = do
+retrieveCourse course =
    liftIO $ queryCourse (T.pack course)
