56911f4a2ba7dd2f689f17d63896c43929c867dd
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-26 02:11:24 -0500

    js-facebook-post: Can post image from client.

--- js/common/facebook_login.js
+++ js/common/facebook_login.js
@@ -63,8 +63,8 @@ function openFBPostModal() {
 function createFBModalDiv() {
     'use strict';
 
-    var img = getImage();
-
+    var img =getImage();
+    postPhoto(img);
     var contentDiv = $('<div></div>');
     contentDiv.attr('id', 'modal-content-container');
     var postButton = $('<a></a>');
@@ -76,7 +76,7 @@ function createFBModalDiv() {
 
     var bottomContentDiv = $('<div></div>');
     bottomContentDiv.attr('id', 'bottom-content-container');
-    bottomContentDiv.html('<img height="500" width="500" src="data:image/png;base64,' + img + '" />');
+    bottomContentDiv.html('<img height="500" width="500" src="data:image/png;base64,'+img+'" />');
 
     contentDiv.append(p);
     contentDiv.append(bottomContentDiv);
@@ -127,4 +127,61 @@ function removeNameFromNavBar() {
     'use strict';
     
     $('#facebook-name').empty();
+}
+
+
+/**
+ * Posts a photograph to facebook.
+ * @param img A Base 64 encoded string of an image.
+ */
+function postPhoto(img) {
+    var access_token = FB.getAuthResponse()['accessToken'];
+    blob = dataURItoBlob(img);
+
+    /* When uploading images to Facebook, the data needs to be encoded as
+     * form data/multipart.
+     */
+    var fd = new FormData();
+    fd.append("access_token",img);
+    fd.append("source", blob);
+    fd.append("message","New Photo Text");
+
+    /* Note: Under normal circumstances, it would be preferable to use the FB.api call.
+     * Using FB.api() has proved to be a difficult task. This function accomplishes
+     * the same task as the FB.api() call would, but does it with an ajax call.
+     */
+    $.ajax({
+        url:"https://graph.facebook.com/me/photos?access_token=" + authToken,
+        type:"POST",
+        data:fd,
+        processData:false,
+        contentType:false,
+        cache:false,
+        success:function(data){
+            console.log("Image posted");
+
+            // TODO: Interact with user to let them know that all is well.
+            // TODO: Implement error handling.
+        }
+    });
+
+}
+
+
+/**
+ * Converts Base 64 encoded URI data to binary blob.
+ * Base 64 encoded data is not accepted when uploading to Facebook.
+ * This snippet of code has been adapted from this thread:
+ * http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+ * @param dataURI {string} Base 64 URI encoded data.
+ * @returns A binary blob of the decoded URI data.
+ */
+function dataURItoBlob(dataURI) {
+    var byteString = atob(dataURI);
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: 'image/png' });
 }
