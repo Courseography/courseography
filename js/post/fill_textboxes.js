0b6da9325bf8a5f617374ead6596352b4e68daba
Christine Murad <christine.murad@mail.utoronto.ca>
2015-03-07 21:56:01 -0500

    post-refactoring-two: Changed parameter 'post' for fill* functions

--- js/post/fill_textboxes.js
+++ js/post/fill_textboxes.js
@@ -1,18 +1,20 @@
-
+/**
+ * Fills textboxes in 300 level category,
+ * @param {string} 
+**/
 function fill300Textboxes(post, postElement, category) {
     'use strict';
 
-    for (var m = window[post].index300 + 1; m < activeCourses.length; m++) {
-        if (window[post].index300 === activeCourses.length || window[post].filledTextboxes300 === window[post].textboxes300) {
+    for (var m = post.index300 + 1; m < activeCourses.length; m++) {
+        if (post.index300 === activeCourses.length || post.filledTextboxes300 === post.textboxes300) {
             break;
         }
         var course = activeCourses[m];
         if (course.indexOf('CSC3') != -1 && course.indexOf('CSC373') === -1 && course.indexOf('CSC369') === - 1) {
-            postElement[window[post].filledTextboxes300].value = activeCourses[m];
-            //postElement[i].disabled = true;
-            window[post].index300 = m;
-            window[post]['filledTextboxes' + category] += 1;
-            window[post].creditCount += 0.5;
+            postElement[post.filledTextboxes300].value = activeCourses[m];
+            post.index300 = m;
+            post['filledTextboxes' + category] += 1;
+            post.creditCount += 0.5;
         }
     }
 }
@@ -21,17 +23,17 @@ function fill300Textboxes(post, postElement, category) {
 function fill400Textboxes(post, postElement, category) {
     'use strict';
 
-    for (var m = window[post].index400 + 1; m < activeCourses.length; m++) {
-        if (window[post].index400 === activeCourses.length || window[post].filledTextboxes400 === window[post].textboxes400) {
+    for (var m = post.index400 + 1; m < activeCourses.length; m++) {
+        if (post.index400 === activeCourses.length || post.filledTextboxes400 === post.textboxes400) {
             break;
         }
         var course = activeCourses[m];
         if (course.indexOf('CSC4') != -1) {
-            postElement[window[post].filledTextboxes400].value = activeCourses[m];
+            postElement[post.filledTextboxes400].value = activeCourses[m];
             //postElement[i].disabled = true;
-            window[post].index400 = m;
-            window[post]['filledTextboxes' + category] += 1;
-            window[post].creditCount += 0.5;
+            post.index400 = m;
+            post['filledTextboxes' + category] += 1;
+            post.creditCount += 0.5;
         }
     }
 }
@@ -57,14 +59,14 @@ function fill300s() {
     }
     
     // fill courses that have been selected
-    fill300Textboxes('specialist', spec300s, '300');
-    fill300Textboxes('major', maj300s, '300');
+    fill300Textboxes(specialist, spec300s, '300');
+    fill300Textboxes(major, maj300s, '300');
 
     if (specialist.filledTextboxes300 < specialist.textboxes300) {
-        fill400Textboxes('specialist', spec300s, '300');
+        fill400Textboxes(specialist, spec300s, '300');
     }
     if (major.filledTextboxes300 < major.textboxes300) {
-        fill400Textboxes('major', maj300s, '300');
+        fill400Textboxes(major, maj300s, '300');
     }
     
     // add extra 200 courses for minor if extra space
@@ -96,23 +98,23 @@ function fill400s() {
     }
     
     // fill courses that have been selected
-    fill400Textboxes('specialist', spec400s, '400');
-    fill400Textboxes('major', maj400s, '400');
+    fill400Textboxes(specialist, spec400s, '400');
+    fill400Textboxes(major, maj400s, '400');
 }
  
 function fillExtraTextboxes(post, postElement, level) {
-    for (var i = window[post]['index' + level] + 1; i < activeCourses.length; i++) {
-        if (window[post]['index' + level] === activeCourses.length || window[post].filledTextboxesExtra === window[post].textboxesExtra) {
+    for (var i = post['index' + level] + 1; i < activeCourses.length; i++) {
+        if (post['index' + level] === activeCourses.length || post.filledTextboxesExtra === post.textboxesExtra) {
             break;
         }
         var course = activeCourses[i];
-        if (postElement[window[post].filledTextboxesExtra].value === '' && course.indexOf('CSC' + level.charAt(0)) != -1 
+        if (postElement[post.filledTextboxesExtra].value === '' && course.indexOf('CSC' + level.charAt(0)) != -1 
             && course.indexOf('CSC373') === -1 && course.indexOf('CSC369') === - 1) {
-            postElement[window[post].filledTextboxesExtra].value = activeCourses[i];
-            postElement[window[post].filledTextboxesExtra].disabled = true;
-            window[post]['index' + level] = i;
-            window[post].filledTextboxesExtra += 1;
-            window[post].creditCount += 0.5;
+            postElement[post.filledTextboxesExtra].value = activeCourses[i];
+            postElement[post.filledTextboxesExtra].disabled = true;
+            post['index' + level] = i;
+            post.filledTextboxesExtra += 1;
+            post.creditCount += 0.5;
         }
     }
 }
@@ -158,19 +160,19 @@ function fillExtra() {
     }
 
     // fill courses that have been selected
-    fillExtraTextboxes('specialist', spec_extra, '300');
-    
-    fillExtraTextboxes('major', maj_extra, '300');
-    fillExtraTextboxes('minor', min_extra, '300')
+    fillExtraTextboxes(specialist, spec_extra, '300');
+
+    fillExtraTextboxes(major, maj_extra, '300');
+    fillExtraTextboxes(minor, min_extra, '300')
 
     if (specialist.filledTextboxesExtra < specialist.textboxesExtra) {
-        fillExtraTextboxes('specialist', spec_extra, '400');
+        fillExtraTextboxes(specialist, spec_extra, '400');
     } 
     if (major.filledTextboxesExtra < major.textboxesExtra) {
-        fillExtraTextboxes('major', maj_extra, '400');
+        fillExtraTextboxes(major, maj_extra, '400');
     }
     if (minor.filledTextboxesExtra < minor.textboxesExtra) {
-        fillExtraTextboxes('minor', min_extra, '400');
+        fillExtraTextboxes(minor, min_extra, '400');
     }
 
 }
