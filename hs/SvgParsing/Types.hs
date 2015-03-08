010cf08d3518d74c3954c69b94fd3576d39d3ff6
cygnet <iansb@cs.toronto.edu>
2015-03-08 17:31:16 -0400

    svg-refactoring: Remove graph obj.

--- hs/SvgParsing/Types.hs
+++ hs/SvgParsing/Types.hs
@@ -1,26 +1,5 @@
 module SvgParsing.Types where
 
-import Database.Tables
-
--- | A Graph.
-data Graph =
-    Graph { 
-            gId :: Int,
-            title :: String
-          } deriving Show
-
--- | A Path.
---data Path =
---    Path {
---           pathId :: String,
---           points :: [Point],
---           pathFill :: String,
---           pathStroke :: String,
---           pathIsRegion :: Bool,
---           source :: String,
---           target :: String
---         } deriving Show
-
 data Style =
     Style {
             transform :: (Double, Double),
