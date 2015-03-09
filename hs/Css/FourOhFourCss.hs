cabd53e8a6a1b1e9bbdf7fb33deba0f66a896173
jmatholia <jmatholia@gmail.com>
2015-03-09 07:35:47 -0400

    Added picture of dragon

---
+++ hs/Css/FourOhFourCss.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Css.FourOhFourCss where
+
+import Clay
+import Prelude hiding ((**))
+import Data.Monoid
+import Css.Constants
+
+{- aboutStyles
+ - Generates CSS for the about page. -}
+
+fourOhFourStyles = "#picDiv" ? do
+	width $ px 1000
+	height $ px 800
+	"background-image" -: "url(http://1.bp.blogspot.com/-_jh9V05IGrg/Um7Vq-tvJ-I/AAAAAAAADD0/yrLmyK55hyM/s1600/dragon_smores_C.jpg)"
