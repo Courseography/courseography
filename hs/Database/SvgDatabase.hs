25f3337e1c3a1cc2570fa80725d0ec6c88f90756
cygnet <iansb@cs.toronto.edu>
2015-03-08 21:01:20 -0400

    svg-refactoring: Add Database.

--- hs/Database/SvgDatabase.hs
+++ hs/Database/SvgDatabase.hs
@@ -6,21 +6,9 @@ import Control.Monad.IO.Class  (liftIO, MonadIO)
 import Database.Persist.Sqlite
 import Database.Persist
 import Control.Monad.Trans.Reader
-import Database.Tables
-import SvgParsing.Types
-import SvgParsing.ParserUtil
 import qualified Data.Conduit.List as CL
 import Data.Conduit
 import Database.JsonParser
-
-insertShapes :: MonadIO m0 => ([Path],[Shape],[Text]) -> ReaderT SqlBackend m0 ()
-insertShapes (a,b,c) = foldl (>>) (return ()) $ map insert_ b
-
-insertPaths :: MonadIO m0 => ([Path],[Shape],[Text]) -> ReaderT SqlBackend m0 ()
-insertPaths (a,b,c) = foldl (>>) (return ()) $ map insert_ a
-
-insertTexts :: MonadIO m0 => ([Path],[Shape],[Text]) -> ReaderT SqlBackend m0 ()
-insertTexts (a,b,c) = foldl (>>) (return ()) $ map insert_ c
 
 -- | Prints the database table 'rects'.
 printDB :: IO ()
