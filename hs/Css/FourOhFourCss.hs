eeff6e03df3d1a2a50e57d557ef164e487f1a8dc
jmatholia <jmatholia@gmail.com>
2015-03-09 07:45:10 -0400

    Fixed picture dimensions

--- hs/Css/FourOhFourCss.hs
+++ hs/Css/FourOhFourCss.hs
@@ -11,6 +11,6 @@ import Css.Constants
  - Generates CSS for the about page. -}
 
 fourOhFourStyles = "#picDiv" ? do
-	width $ px 1000
-	height $ px 800
+	width $ px 1100
+	height $ px 750
 	"background-image" -: "url(http://1.bp.blogspot.com/-_jh9V05IGrg/Um7Vq-tvJ-I/AAAAAAAADD0/yrLmyK55hyM/s1600/dragon_smores_C.jpg)"
