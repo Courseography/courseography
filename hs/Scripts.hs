09c0f043e152006ea33899493e139f288c07334a
Christine Murad <christine.murad@mail.utoronto.ca>
2015-03-11 23:24:36 -0400

    focuses-fix: Changed directory structure for sidebar files

--- hs/Scripts.hs
+++ hs/Scripts.hs
@@ -33,8 +33,8 @@ plannerScripts = concatHtml (map makeScript["https://ajax.googleapis.com/ajax/li
                                            "static/js/graph/mouse_events.js",
                                            "static/js/graph/setup.js", 
                                            "static/js/post/update_post.js",
-                                           "static/js/graph/sidebar_divs.js",
-                                           "static/js/graph/sidebar_events.js"])
+                                           "static/js/graph/sidebar/sidebar_divs.js",
+                                           "static/js/graph/sidebar/sidebar_events.js"])
 
 timetableScripts :: H.Html
 timetableScripts = do jQuery
