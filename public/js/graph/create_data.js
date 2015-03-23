c832e3e908d475cba369526129eff24b8dc58e58
Christine Murad <christine.murad@mail.utoronto.ca>
2015-03-23 01:54:06 -0400

    issue-378: Added missing course

--- public/js/graph/create_data.js
+++ public/js/graph/create_data.js
@@ -12,7 +12,8 @@ var allCourses = ['CSC108', 'CSC148', 'CSC165', 'CSC207', 'CSC236',
                   'CSC404', 'CSC410', 'CSC411', 'CSC412', 'CSC418',
                   'CSC420', 'CSC428', 'CSC386', 'CSC438', 'CSC443',
                   'CSC446', 'CSC448', 'CSC454', 'CSC456', 'CSC458',
-                  'CSC463', 'CSC469', 'CSC486', 'CSC488', 'ECE489'];
+                  'CSC463', 'CSC465', 'CSC469', 'CSC486', 'CSC488', 
+                  'ECE489'];
 
 var areaNames = ['theory', 'core', 'se', 'systems', 'hci',
                  'graphics', 'num', 'ai', 'dbweb'];
