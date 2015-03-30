939b70e12bd925cb9f148cfc6f713c56a24be5e4
Christine Murad <christine.murad@mail.utoronto.ca>
2015-03-30 01:16:23 -0400

    fcecount-fix: Changed attribute grabbed for title

--- public/js/graph/sidebar/sidebar_events.js
+++ public/js/graph/sidebar/sidebar_events.js
@@ -31,7 +31,7 @@ function createGraphButtons(graphs) {
     'use strict';
 
     for (var i = 0; i < graphs.length; i++) {
-        var graphTitle = graphs[i].graph_title.toLowerCase();
+        var graphTitle = graphs[i].title.toLowerCase();
         var graphButton = '<div id = "graph-' + graphTitle +'" class = "graph-button">';
         $('#graphs').append(graphButton);
         $('#graph-' + graphTitle).html(graphTitle.toUpperCase());
