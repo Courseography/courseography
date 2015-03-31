ba7ec65af55ca639c20af746a041db42be875000
cygnet <iansb@cs.toronto.edu>
2015-03-30 23:45:54 -0400

    clean-up-fb: Clean up server.hs.

--- hs/server.hs
+++ hs/server.hs
@@ -25,20 +25,20 @@ main = do
     cwd <- getCurrentDirectory
     redirectUrlGraphEmail <- retrieveAuthURL testUrl
     redirectUrlGraphPost <- retrieveAuthURL testPostUrl
-    let staticDir = (encodeString $ parent $ decodeString cwd) ++ "public/"
+    let staticDir = encodeString (parent $ decodeString cwd) ++ "public/"
     contents <- readFile "../README.md"
     print "Server is running..."
     simpleHTTP nullConf $
         msum [ dir "grid" gridResponse,
                dir "graph" graphResponse,
-               dir "image" $ graphImageResponse,
+               dir "image" graphImageResponse,
                dir "timetable-image" $ look "courses" >>= \x -> look "session" >>= timetableImageResponse x,
                dir "graph-fb" $ seeOther redirectUrlGraphEmail $ toResponse "",
                dir "post-fb" $ seeOther redirectUrlGraphPost $ toResponse "",
                dir "test" $ look "code" >>= getEmail,
                dir "test-post" $ look "code" >>= postToFacebook,
-               dir "post" $ postResponse,
-               dir "draw" $ drawResponse,
+               dir "post" postResponse,
+               dir "draw" drawResponse,
                --dir "about" $ aboutResponse contents,
                dir "static" $ serveDirectory EnableBrowsing [] staticDir,
                dir "course" $ look "name" >>= retrieveCourse,
@@ -46,6 +46,6 @@ main = do
                dir "graphs" $ liftIO queryGraphs,
                dir "course-info" $ look "dept" >>= courseInfo,
                dir "depts" $ liftIO deptList,
-               dir "timesearch" $ searchResponse,
+               dir "timesearch" searchResponse,
                fourOhFourResponse
                ]
