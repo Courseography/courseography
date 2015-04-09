b2b16f60c5b388d90d1eb7c53e4b6a35ea77eb12
cygnet <iansb@cs.toronto.edu>
2015-04-09 17:25:09 -0400

    privacy-policy: Add privacy response and server.

---
+++ hs/PrivacyResponse.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module AboutResponse where
+
+import           Text.Blaze ((!))
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Happstack.Server
+import MakeElements
+import Text.Pandoc
+import MasterTemplate
+
+privacyResponse :: String -> ServerPart Response
+privacyResponse privacyContents =
+   ok $ toResponse $
+    masterTemplate "Courseography - Privacy Policy"
+                [H.meta ! A.name "keywords"
+                        ! A.content "",
+                 aboutLinks
+                ]
+                (do
+                    header "privacy"
+                    privacyHtml privacyResponse
+                )
+                ""
+
+-- | AboutHtml takes in the contents of the README.md file (the GitHub README file) and translates
+--   the markdown to blaze-HTML.
+privacyHtml :: String -> H.Html
+privacyHtml contents = H.div $ mdToHTML contents
+
+-- | mdToHTML takes in the contents of a file written in Mark Down and converts it to 
+-- blaze-HTML.
+mdToHTML :: String -> H.Html
+mdToHTML contents = writeHtml def $ readMarkdown def contents
