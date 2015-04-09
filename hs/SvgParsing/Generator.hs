4f848a253aac905c1b81743a233acadceaad3204
cygnet <iansb@cs.toronto.edu>
2015-04-08 21:04:11 -0400

    refactor: Change generator to 80 line char limit.

--- hs/SvgParsing/Generator.hs
+++ hs/SvgParsing/Generator.hs
@@ -47,7 +47,8 @@ makeSVGDoc courseMap rects ellipses edges regions regionTexts =
                  ! S.customAttribute "xmlns:svg" "http://www.w3.org/2000/svg"
                  ! S.customAttribute "xmlns:dc" "http://purl.org/dc/elements/1.1/"
                  ! S.customAttribute "xmlns:cc" "http://creativecommons.org/ns#"
-                 ! S.customAttribute "xmlns:rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                 ! S.customAttribute "xmlns:rdf" 
+                                     "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
                  ! A.version "1.1" $ do
                       makeSVGDefs
                       S.g ! A.id_ "regions" $
@@ -71,31 +72,46 @@ makeSVGDoc courseMap rects ellipses edges regions regionTexts =
 -- | Builds an SVG document.
 buildSVG :: Int64                -- ^ The ID of the graph that is being built.
          -> M.Map String String  -- ^ A map of courses that holds the course
-                                 --   ID as a key, and the data-active attribute
-                                 --   as the course's value.
+                                 --   ID as a key, and the data-active 
+                                 --   attribute as the course's value.
                                  --   The data-active attribute is used in the
-                                 --   interactive graph to indicate which courses
-                                 --   the user has selected.
-         -> String               -- ^ The filename that this graph will be written to.
+                                 --   interactive graph to indicate which 
+                                 --   courses the user has selected.
+         -> String               -- ^ The filename that this graph will be 
+                                 --   written to.
          -> IO ()
 buildSVG gId courseMap filename =
     runSqlite dbStr $ do
-        sqlRects    :: [Entity Shape]    <- selectList [ShapeType_ <-. [Node, Hybrid], ShapeGId ==. gId] []
-        sqlTexts    :: [Entity Text]     <- selectList [TextGId ==. gId] []
-        sqlPaths    :: [Entity Path]     <- selectList [PathGId ==. gId] []
-        sqlEllipses :: [Entity Shape]    <- selectList [ShapeType_ ==. BoolNode, ShapeGId ==. gId] []
+        sqlRects    :: [Entity Shape] <- selectList 
+                                             [ShapeType_ <-. [Node, Hybrid],
+                                              ShapeGId ==. gId] []
+        sqlTexts    :: [Entity Text]  <- selectList [TextGId ==. gId] []
+        sqlPaths    :: [Entity Path]  <- selectList [PathGId ==. gId] []
+        sqlEllipses :: [Entity Shape] <- selectList 
+                                             [ShapeType_ ==. BoolNode,
+                                              ShapeGId ==. gId] []
         let courseStyleMap = M.map convertSelectionToStyle courseMap
             texts          = map entityVal sqlTexts
             rects          = map (buildRect texts . entityVal) sqlRects
-            ellipses       = zipWith (buildEllipses texts) (map entityVal sqlEllipses) [1..]
-            paths          = zipWith (buildPath rects ellipses) (map entityVal sqlPaths) [1..]
+            ellipses       = zipWith (buildEllipses texts)
+                                     (map entityVal sqlEllipses) [1..]
+            paths          = zipWith (buildPath rects ellipses) 
+                                     (map entityVal sqlPaths) [1..]
             regions        = filter pathIsRegion paths
             edges          = filter (not . pathIsRegion) paths
-            regionTexts    = filter (not . intersectsWithShape (rects ++ ellipses)) texts
-            stringSVG = renderSvg $ makeSVGDoc courseStyleMap rects ellipses edges regions regionTexts
+            regionTexts    = filter (not .
+                                     intersectsWithShape (rects ++ ellipses))
+                                    texts
+            stringSVG      = renderSvg $ makeSVGDoc courseStyleMap
+                                                    rects
+                                                    ellipses
+                                                    edges
+                                                    regions
+                                                    regionTexts
         liftIO $ writeFile filename stringSVG
 
--- | Gets a tuple from areaMap where id_ is in the list of courses for that tuple.
+-- | Gets a tuple from areaMap where id_ is in the list of courses for that
+--   tuple.
 getTuple :: String -- ^ The course's ID.
          -> Maybe (T.Text, String)
 getTuple id_
@@ -103,11 +119,13 @@ getTuple id_
     | otherwise   = Just $ snd $ M.elemAt 0 tuples
     where tuples = M.filterWithKey (\k _ -> id_ `elem` k) areaMap
 
--- | Gets an area from areaMap where id_ is in the list of courses for the corresponding tuple.
+-- | Gets an area from areaMap where id_ is in the list of courses for the
+--   corresponding tuple.
 getArea :: String -> String
 getArea id_ = maybe "" snd $ getTuple id_
 
--- | Gets the fill from areaMap where id_ is in the list of courses for the corresponding tuple.
+-- | Gets the fill from areaMap where id_ is in the list of courses for the
+--   corresponding tuple.
 getFill :: String -- ^ The course's ID.
         -> String -- ^ The course's allocated fill value.
 getFill id_ = maybe "grey" (T.unpack . fst) $ getTuple id_
@@ -129,7 +147,8 @@ makeSVGDefs =
 
 -- | Determines if a text intersects with a shape.
 intersectsWithShape :: [Shape] -> Text -> Bool
-intersectsWithShape shapes text = any (intersectsWithPoint (textPos text)) shapes
+intersectsWithShape shapes text =
+    any (intersectsWithPoint (textPos text)) shapes
 
 -- | Converts a `Rect` to SVG.
 convertRectToSVG :: M.Map String String -> Shape -> S.Svg
@@ -137,14 +156,16 @@ convertRectToSVG courseMap rect
     | shapeFill rect == "none" = S.rect
     | otherwise =
         let style = case shapeType_ rect of
-                        Node -> fromMaybe "" $ M.lookup (shapeId_ rect) courseMap
+                        Node -> fromMaybe "" $ M.lookup (shapeId_ rect)
+                                                        courseMap
                         _    -> ""
             class_ = case shapeType_ rect of
                          Node -> "node"
                          Hybrid -> "hybrid"
         in S.g ! A.id_ (stringValue $ filter (/=',') $ shapeId_ rect)
                ! A.class_ (stringValue class_)
-               ! S.customAttribute "data-group" (stringValue (getArea (shapeId_ rect)))
+               ! S.customAttribute "data-group" (stringValue 
+                                                 (getArea (shapeId_ rect)))
                ! S.customAttribute "text-rendering" "geometricPrecision"
                ! S.customAttribute "shape-rendering" "geometricPrecision"
                ! A.style (stringValue style) $
@@ -154,8 +175,11 @@ convertRectToSVG courseMap rect
                          ! A.y (stringValue . show . snd $ shapePos rect)
                          ! A.width (stringValue . show $ shapeWidth rect)
                          ! A.height (stringValue . show $ shapeHeight rect)
-                         ! A.style (stringValue $ "fill:" ++ getFill (shapeId_ rect) ++ ";")
-                  concatSVG $ map (convertTextToSVG (shapeType_ rect)) (shapeText rect)
+                         ! A.style (stringValue $ "fill:" ++
+                                    getFill (shapeId_ rect) ++
+                                    ";")
+                  concatSVG $ map (convertTextToSVG (shapeType_ rect))
+                                  (shapeText rect)
 
 -- | Maps styles to courses based on the courses status.
 convertSelectionToStyle :: String -> String
@@ -189,8 +213,10 @@ convertEdgeToSVG path =
            ! A.class_ "path"
            ! A.d (stringValue $ 'M' : buildPathString (pathPoints path))
            ! A.markerEnd "url(#arrow)"
-           ! S.customAttribute "source-node" (stringValue $ filter (/=',') $ pathSource path)
-           ! S.customAttribute "target-node" (stringValue $ filter (/=',') $ pathTarget path)
+           ! S.customAttribute "source-node" (stringValue $ filter (/=',') 
+                                                          $ pathSource path)
+           ! S.customAttribute "target-node" (stringValue $ filter (/=',') 
+                                                          $ pathTarget path)
            ! A.style (stringValue $ "fill:" ++
                       pathFill path ++
                       ";fill-opacity:1;")
@@ -217,7 +243,8 @@ convertEllipseToSVG ellipse =
                       ! A.style "stroke:#000000;fill:none;"
             concatSVG $ map (convertTextToSVG BoolNode) (shapeText ellipse)
 
-getTextStyle :: ShapeType -- ^ The parent element of the Text element in question.
+getTextStyle :: ShapeType -- ^ The parent element of the Text element in 
+                          --   question.
              -> String
 getTextStyle Hybrid    = hybridTextStyle
 getTextStyle BoolNode  = ellipseTextStyle
@@ -232,11 +259,13 @@ areaMap =  M.fromList
            (["csc165", "csc236", "csc240", "csc263", "csc265",
              "csc310", "csc324", "csc373", "csc438", "csc448",
              "csc463"], (theoryDark, "theory")),
-           (["calc1", "sta1", "sta2", "lin1", "csc108", "csc148", "csc104", "csc120", "csc490",
-             "csc491", "csc494", "csc495"], (coreDark, "core")),
+           (["calc1", "sta1", "sta2", "lin1", "csc108", "csc148", "csc104",
+             "csc120", "csc490", "csc491", "csc494", "csc495"],
+             (coreDark, "core")),
            (["csc207", "csc301", "csc302", "csc410", "csc465"], (seDark, "se")),
            (["csc209", "csc258", "csc358", "csc369", "csc372",
-             "csc458", "csc469", "csc488", "ece385", "ece489"], (systemsDark, "systems")),
+             "csc458", "csc469", "csc488", "ece385", "ece489"],
+             (systemsDark, "systems")),
            (["csc200", "csc300",  "csc318", "csc404", "csc428",
              "csc454"], (hciDark, "hci")),
            (["csc320", "csc418", "csc420"], (graphicsDark, "graphics")),
