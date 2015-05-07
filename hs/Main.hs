6dfc360882b886312a8afcbbb440b8ec800bdc56
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-05-07 14:52:18 -0400

    Fix up case where no task is found.

--- hs/Main.hs
+++ hs/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import System.IO (stderr, hPutStrLn)
 import System.Environment (getArgs)
-import Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+import Data.List (intercalate)
+import qualified Data.Map.Strict as Map
 
 -- internal dependencies
 import Server (runServer)
@@ -13,10 +16,18 @@ taskNamesToTasks = Map.fromList [("server", runServer),
                                  ("database", setupDatabase),
                                  ("graphs", parsePrebuiltSvgs)]
 
+getTask :: String -> Maybe (IO ())
+getTask taskName = Map.lookup taskName taskNamesToTasks
+
 main :: IO ()
 main = do
     args <- getArgs
-    let command = if length args == 0 then "server" else args !! 0
+    let taskName = if length args == 0 then "server" else args !! 0
 
     -- extract and perform the task we want to run
-    taskNamesToTasks ! command
+    let taskNames = Map.keys taskNamesToTasks
+    let availableTasksStr = "[" ++ (intercalate ", " taskNames) ++ "]"
+    let lookupFailedMsg = "No task named " ++ taskName ++
+                          ", available tasks are " ++ availableTasksStr
+
+    fromMaybe (hPutStrLn stderr lookupFailedMsg) (getTask taskName)
