6b1216f62e46a407780ae517e913fabe0c9ca9a7
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-05-14 12:55:26 -0400

    Allow specifying stats department graph with a URL parameter.

--- public/js/graph/setup.js
+++ public/js/graph/setup.js
@@ -21,7 +21,18 @@ var nodes = [];               // List of all nodes
 $(document).ready(function () {
     'use strict';
 
-    var active = getCookie('active-graph');
+    var urlSpecifiedGraph = getURLParameter('dept');
+
+    // HACK: Temporary workaround for giving the statistics department a link to our graph.
+    // Should be replaced with a more general solution.
+    var active;
+    if (urlSpecifiedGraph === 'sta') {
+        active = '2';
+    } else if (urlSpecifiedGraph !== null) {
+        active = '1';
+    } else {
+        active = getCookie('active-graph');
+    }
 
     if (active !== '') {
         loadGraph(active);
