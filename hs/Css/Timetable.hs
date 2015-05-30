97e5fa0c8c4e073668b4f0c38d56934732782111
Christine Murad <christine.murad@mail.utoronto.ca>
2015-05-30 13:40:02 -0400

    css-overflow-fix: Fixed not being able to scroll when graph is bigger than screen

--- hs/Css/Timetable.hs
+++ hs/Css/Timetable.hs
@@ -11,8 +11,6 @@ import Css.Constants
  - Generates all CSS required for the timetable page. -}
 timetableStyles :: Css
 timetableStyles = do
-    body ? do
-        overflowX hidden
     ".main" ? do -- TODO: change to id, and pick better name
         height (pct 84)
         margin0
