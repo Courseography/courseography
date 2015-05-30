f419271ad87271b09aaae1b0043b87adb7d88d10
Christian <christian.garciasalguero@mail.utoronto.ca>
2015-05-30 14:13:39 -0400

    import file to server

--- hs/server.hs
+++ hs/server.hs
@@ -14,6 +14,7 @@ import FourOhFourResponse
 import SearchResponse
 import AboutResponse
 import PrivacyResponse
+import CalendarResponse
 import Database.CourseQueries (retrieveCourse, allCourses, queryGraphs, courseInfo, deptList)
 import Css.CssGen
 import Filesystem.Path.CurrentOS
