ffaa884600f80351e2954e2bd47b90d7842444fb
Christian <christian.garciasalguero@mail.utoronto.ca>
2015-06-01 11:54:37 -0400

    on click event

--- hs/MasterTemplate.hs
+++ hs/MasterTemplate.hs
@@ -36,7 +36,7 @@ header page =
             H.li $ makeA "" "" "draw" "" "Draw"
             H.li $ makeA "" "" "post" "" "Check My POSt!"
             H.li $ makeA "" "" "about" "" "About"
-            H.li $ makeA "" "" "calendar" "" "Calendar"
+            (H.li $ makeA "calendar" "" "calendar" "" "Calendar").addEventListener('click', getCalendarEvents())
         if page `elem` ["graph", "grid"]
         then
             H.div ! A.id "nav-fb" $ do
