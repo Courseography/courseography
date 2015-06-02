e13c989485c81f149169198b07cc88a902a5fa81
Christian <christian.garciasalguero@mail.utoronto.ca>
2015-06-02 10:22:04 -0400

    calendar_events.js function(){}

--- public/js/calendar/calendar_events.js
+++ public/js/calendar/calendar_events.js
@@ -17,9 +17,9 @@
 var calendarTag = document.getElementsByTagName('calendar');
 //calendarTag.onclick=getCalendarEvents("fall"){"hs/MasterTemplate.hs"};
 //calendarTag.onclick=getCalendarEvents("fall"){"/hs/MasterTemplate.hs"};
-calendarTag.onclick=getCalendarEvents("fall"){"courseography/hs/MasterTemplate.hs"};
+//calendarTag.onclick=getCalendarEvents("fall"){"courseography/hs/MasterTemplate.hs"};
 //calendarTag.onclick=getCalendarEvents("fall"){"static/hs/MasterTemplate.hs"};
-//calendarTag.onclick=function(){getCalendarEvents('fall')};
+calendarTag.onclick=function(){getCalendarEvents('fall')};
 //calendarTag.onclick=function(){myscript};
 
 //calendar.addEventListener('click', function() {
