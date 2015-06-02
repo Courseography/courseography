4bf18e025af2a83571f26d33e89048ff334ab354
Christian <christian.garciasalguero@mail.utoronto.ca>
2015-06-02 11:48:10 -0400

    calendar instead calendarlink

--- hs/MasterTemplate.hs
+++ hs/MasterTemplate.hs
@@ -36,7 +36,7 @@ header page =
             H.li $ makeA "" "" "draw" "" "Draw"
             H.li $ makeA "" "" "post" "" "Check My POSt!"
             H.li $ makeA "" "" "about" "" "About"
-            H.li $ makeA "calendarlink" "" "calendar" "" "Calendar"
+            H.li $ makeA "calendar" "" "calendar" "" "Calendar"
         if page `elem` ["graph", "grid"]
         then
             H.div ! A.id "nav-fb" $ do
