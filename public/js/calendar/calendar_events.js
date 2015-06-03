f5c1547acb466a58564d8efed2705d5ef9ecd4a5
Christian <christian.garciasalguero@mail.utoronto.ca>
2015-06-03 12:12:46 -0400

    async

--- public/js/calendar/calendar_events.js
+++ public/js/calendar/calendar_events.js
@@ -22,7 +22,7 @@ function getCalendarEvents(session) {
     session = session.charAt(0).toUpperCase() + session.slice(1);
     $.ajax({
         url: 'calendar',
-        //async: false,
+        async: false,
         data: {courses: courses, session: session},
         success: function (data) {
             events = data;
@@ -32,7 +32,7 @@ function getCalendarEvents(session) {
             throw 'No CSV file generated';
         }
     });
-
+    system.out.println(data)
     return events;
 }
 
