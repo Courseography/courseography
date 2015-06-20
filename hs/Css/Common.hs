b48beedaa2ace788862c5d0ac48f910f41efff4d
cchens <mail@christinachen.ca>
2015-06-20 01:35:18 -0400

    header-logo: cleaned up shadows for text and logo

--- hs/Css/Common.hs
+++ hs/Css/Common.hs
@@ -37,7 +37,7 @@ headerCSS = do
     ".header" ?
         do margin0
            padding 0 (em 0.5) 0 (em 0.5)
-           backgroundColor $ parse "#EED2EE"
+           backgroundColor $ parse "#8A79A1"
            border solid (px 1) black
            color white
            img ?
@@ -58,10 +58,11 @@ headerCSS = do
                 display inlineBlock
                 padding 0 (px 10) 0 (px 10)
                 a <? do
+                    fontSize $ pt 17.9
                     color white
-                    "text-shadow" -: "-2px 2px 2px #000, 2px 2px 2px #000, -2px -2px 2px #000, 2px -2px 2px #000" 
+                    "text-shadow" -: "0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000, 0 0 2px #000;" 
                     hover & do
-                        color gray
+                        color lightgray
             height (px 50)
     "#nav-fb" ? do
         float floatRight
