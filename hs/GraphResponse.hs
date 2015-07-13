53adda4c3ed7956dd9827f6b2d6cf91dc1191c9d
Christine Murad <christine.murad@mail.utoronto.ca>
2015-07-12 22:49:01 -0400

    issue-664: Changed position of focus and graph bar navs

--- hs/GraphResponse.hs
+++ hs/GraphResponse.hs
@@ -54,10 +54,10 @@ sideBar = do
             H.div ! A.id "fcecount" $ ""
             H.button ! A.id "reset" $ "Reset Graph"
         H.nav ! A.id "sidebar-nav" $ H.ul $ do
-            H.li ! A.id "focuses-nav" $ do
-                H.a ! A.href "" $ "Focuses"
             H.li ! A.id "graphs-nav" $ do
                 H.a ! A.href "" $ "Graphs"
+            H.li ! A.id "focuses-nav" $ do
+                H.a ! A.href "" $ "Focuses"
         H.div ! A.id "focuses" $ do
             H.p ! A.id "sci" ! A.class_ "focus" $ "Scientific Computing"
             H.div ! A.id "sci-details" ! A.class_ "details" $ ""
