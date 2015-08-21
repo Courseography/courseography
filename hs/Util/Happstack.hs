96002220af86e40965ebb13eaa089e8bff03cc50
david-yz-liu <liudavid@cs.toronto.edu>
2015-08-21 17:55:27 -0400

    util-dir: more Json response helper

---
+++ hs/Util/Happstack.hs
@@ -0,0 +1,14 @@
+module Util.Happstack
+    (createJSONResponse) where
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Happstack.Server
+import Data.Aeson (ToJSON, encode)
+
+-- | Creates a JSON response.
+createJSONResponse :: ToJSON a => a -> Response
+createJSONResponse x = toResponseBS (BS.pack "application/json") (encodeJSON x)
+    where
+        encodeJSON :: ToJSON a => a -> BSL.ByteString
+        encodeJSON = BSL.filter (/= '\\') . encode
