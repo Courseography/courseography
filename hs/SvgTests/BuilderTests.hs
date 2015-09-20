8dae359d11adcf2615573b22c924dad442757000
Philip Kukulak <philip.kukulak@mail.utoronto.ca>
2015-09-20 16:19:39 -0400

    Added test to BuilderTests.

--- hs/SvgTests/BuilderTests.hs
+++ hs/SvgTests/BuilderTests.hs
@@ -1,3 +1,10 @@
-module BuilderTests where
+module SvgTests.BuilderTests where
 
 import Svg.Builder
+import Test.QuickCheck
+
+prop1_sanitizeId str =
+    let sanitized = sanitizeId str
+        illegals  = map (\c -> not $ elem c ",()/<>% ") sanitized
+        allTrue = and illegals
+    in  allTrue
