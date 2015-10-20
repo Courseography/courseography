e352b087620a5cb99f661ae8f6e7b8117253ed93
Christine Murad <christine.murad@mail.utoronto.ca>
2015-10-20 02:13:41 -0400

    react-specialist: Created new component for categories with multiple courses

--- public/js/post/post.js
+++ public/js/post/post.js
@@ -32,6 +32,56 @@ var CourseCode = React.createClass({
     }
 })
 
+var MultipleCourseCode = React.createClass({
+    getInitialState: function() {
+        return {
+            completed: false,
+            completedTextBoxes: 0
+        }
+    },
+    
+    
+    toggleFullInfo: function() {
+          $('#' + this.props.courseID + ' > .more-info').toggle();
+    },
+
+    checkIfCompleted: function() {
+       this.setState({completed: this.state.completedTextBoxes == this.props.data.textBoxNumber});
+    },
+
+    handleKeyDown: function(e) {
+        if (e.keyCode === 13) {
+            this.setState({completedTextBoxes: this.state.completedTextBoxes += 1});
+            this.checkIfCompleted();
+        }  
+    },
+
+    render: function() {
+
+        var categoryClasses = 'code';
+        var courseClasses = 'full_name';
+
+        if (this.state.completed) {
+            categoryClasses += ' category_selected';
+            courseClasses += ' course_selected';
+        }
+
+        return (
+            <div id={this.props.courseID} className='course'>
+                <p className = {categoryClasses} onClick={this.toggleFullInfo}> {this.props.data.categoryName} </p>
+                <div className='more-info'>
+                    <p className = {courseClasses}> 
+                        {Array.apply(0, Array(this.props.data.textBoxNumber)).map(function (x, i) {
+                            return <input type='text' onKeyDown={this.handleKeyDown} />;
+                        })}
+                    </p>
+                </div>
+            </div>
+        );
+    }
+})
+
+
 var SpecialistPost = React.createClass({
     getInitialState: function() {
         return {
@@ -52,11 +102,14 @@ var SpecialistPost = React.createClass({
                 {this.state.courses.map(function (course) {
                     return <CourseCode courseID={course.toLowerCase()} />;
                 })}
-                <p className="code"> Any 400-level CSC course, BCB410H, BCB420H, BCB430Y, ECE489H (1.5 FCEs) </p>
-                <p className="code"> Any 300+ level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs) </p>
-                <p className="code"> Any of the following: 300+ level CSC course; MAT: 235/237/257, any 300+
-                                     except for 329, 390, & 391; STA: 248, 261, any 300+; ECE: 385H/489H;
-                                     BCB: 410H/420H/430Y (1.5 FCEs) </p>
+                <MultipleCourseCode courseID="spec400" data={{textBoxNumber: 3, 
+                    categoryName: "Any 400-level CSC course, BCB410H, BCB420H, BCB430Y, ECE489H (1.5 FCEs)"}} />
+                <MultipleCourseCode courseID="spec300" data={{textBoxNumber: 3, 
+                    categoryName: "Any 300+ level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs)"}} />
+                <MultipleCourseCode courseID="specextra" data={{textBoxNumber: 4, 
+                    categoryName: "Any of the following: 300+ level CSC course; MAT: 235/237/257, any 300+ \
+                                     except for 329, 390, & 391; STA: 248, 261, any 300+; ECE: 385H/489H; \
+                                     BCB: 410H/420H/430Y (2.0 FCEs)"}} />
                 <p className="code"> Any from this list: CSC301H, CSC318H, CSC404H, CSC411H, CSC418H, CSC420H, 
                     CSC428H, CSC454H, CSC485H, CSC490H, CSC491H, CSC494H, or PEY (0.5 FCEs) 
                     ** Note: Type 'PEY' for Check my POSt to recognize it ** </p>
