7bcd2a52cbde47497a74c0dcc7bcae19efe52fbd
Christine Murad <christine.murad@mail.utoronto.ca>
2015-10-19 22:50:47 -0400

    react-specialist: Created initial Specialist React component

--- public/js/post/post.js
+++ public/js/post/post.js
@@ -32,5 +32,38 @@ var CourseCode = React.createClass({
     }
 })
 
+var SpecialistPost = React.createClass({
+    getInitialState: function() {
+        return {
+            selected: true,
+            courses: ['CSC108', 'CSC148', 'CSC165', 'MAT135', 'CSC207',
+                      'CSC209', 'CSC236', 'CSC258', 'CSC263',
+                      'MAT221', 'STA247', 'CSC369', 'CSC373']
+        }
+    },
 
-React.render(<CourseCode courseID = 'csc108'/>, document.getElementById('spec_csc108'))
+    componentWillMount: function() {
+        this.setState({selected: getCookie('specialist') === "active"});
+    },
+
+    render: function() {
+        return (
+            <div id="specialist_div">
+                {this.state.courses.map(function (course) {
+                    return <CourseCode courseID={course.toLowerCase()} />;
+                })}
+                <p className="code"> Any 400-level CSC course, BCB410H, BCB420H, BCB430Y, ECE489H (1.5 FCEs) </p>
+                <p className="code"> Any 300+ level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs) </p>
+                <p className="code"> Any of the following: 300+ level CSC course; MAT: 235/237/257, any 300+
+                                     except for 329, 390, & 391; STA: 248, 261, any 300+; ECE: 385H/489H;
+                                     BCB: 410H/420H/430Y (1.5 FCEs) </p>
+                <p className="code"> Any from this list: CSC301H, CSC318H, CSC404H, CSC411H, CSC418H, CSC420H, 
+                    CSC428H, CSC454H, CSC485H, CSC490H, CSC491H, CSC494H, or PEY (0.5 FCEs) 
+                    ** Note: Type 'PEY' for Check my POSt to recognize it ** </p>
+            </div>
+        );
+    }
+})
+
+
+React.render(<SpecialistPost />, document.getElementById('div_specialist'));
