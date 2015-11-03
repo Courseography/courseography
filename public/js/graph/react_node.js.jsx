a24f57998caa7c1c929176f91978b5f955dd8e6a
Ryan Fan <ryanscfan@gmail.com>
2015-11-03 08:13:09 -0500

    Added React Regions, Bools, Edges and Labels

--- public/js/graph/react_node.js.jsx
+++ public/js/graph/react_node.js.jsx
@@ -13,8 +13,8 @@ function getAttributes(svgAttributes) {
     //Using a for loop instead of converting to Array
     for (var i = 0; i < svgAttributes.length; i++) {
         var item = svgAttributes[i];
-        //Will be hard-coding in className and textAnchor
-        if (item.name!='class' && item.name!='text-anchor'){
+        //Will be hard-coding in className and textAnchor and markerEnd
+        if (item.name!='class' && item.name!='text-anchor' && item.name!='marker-end'){
             attrs[item.name] = item.value; 
         }
     }
@@ -70,16 +70,80 @@ var ReactSVG = React.createClass({
     render: function() {
         return (
             <svg width={this.props.width} height={this.props.height}>
-                {/*<ReactRegions/>*/}
+                <ReactRegions/>
                 <ReactNodes/>
-                {/*<ReactRegions/>*/}
-                {/*<ReactBools/>*/}
-                {/*<ReactEdges/>*/}
-                {/*<ReactRegionLabels/>*/}
+                <ReactBools/>
+                <ReactEdges/>
+                <ReactRegionLabels/>
             </svg>
         );
     }
 });
+
+var ReactRegionLabels = React.createClass({
+    getInitialState: function() {
+        return {
+            labels_list: []
+        };
+    },
+    
+    componentDidMount: function() {
+        var dict_list = [];
+        $('#region-labels > text').map(function(key, element) {
+            var entry = {};
+            entry['id'] = element.id;
+            entry['attributes'] = getAttributes(element.attributes);
+            entry['style'] = getStyles(entry['attributes']['style']);
+            entry['innerHTML'] = element.innerHTML;
+            dict_list.push(entry);
+        });
+        this.setState({labels_list:dict_list});
+    },
+    
+    render: function() {
+        return (
+            <g id='region-labels'>
+                {this.state.labels_list.map(function(entry, value) {
+                    console.log(entry['attributes']);
+                    return <text {... entry['attributes']} style={entry['style']}>{entry['innerHTML']}</text>
+                })}
+            </g>
+        );
+    }
+});
+
+var ReactRegions = React.createClass({
+    getInitialState: function() {
+        return {
+            regions_list: []
+        };
+    },
+    
+    componentDidMount: function() {
+        this.setState({regions_list:getNodes('.region')});
+    },
+    
+    render: function() {
+        return (
+            <g id='regions'>
+                {this.state.regions_list.map(function(entry, value) {
+                    return <ReactRegion className='region' key={entry['id']} attributes={entry['attributes']} styles={entry['style']}/>
+                })}
+            </g>
+        );
+    }
+});
+
+var ReactRegion = React.createClass({
+    render: function() {
+        //hard-coded className
+        return (
+            <path className={this.props.className} {... this.props.attributes} style={this.props.styles}>
+            </path>
+        );
+    }
+});
+
 
 var ReactNodes = React.createClass({
     getInitialState: function() {
@@ -96,12 +160,12 @@ var ReactNodes = React.createClass({
     
     render: function() {
         return (
-            <g id='nodes'>
+            <g id='nodes' stroke='black'>
                 {this.state.nodes_list.map(function(entry, value) {
-                    return <ReactNode className='node' key={entry['id']} attributes={entry['attributes']} style={entry['style']} children={entry['children']}/>
+                    return <ReactNode className='node' key={entry['id']} attributes={entry['attributes']} styles={entry['style']} children={entry['children']}/>
                 })}
                 {this.state.hybrids_list.map(function(entry, value) {
-                    return <ReactNode className='hybrid' key={entry['id']} attributes={entry['attributes']} style={entry['style']} children={entry['children']}/>
+                    return <ReactNode className='hybrid' key={entry['id']} attributes={entry['attributes']} styles={entry['style']} children={entry['children']}/>
                 })}
             </g>
         );
@@ -122,6 +186,78 @@ var ReactNode = React.createClass({
                     return <text key={value} textAnchor='middle' {... text_tag['attributes']} style={text_tag['style']}>{text_tag['innerHTML']}</text>;
                 })}
             </g>
+        );
+    }
+});
+
+var ReactBools = React.createClass({
+    getInitialState: function() {
+        return {
+            bools_list: []
+        };
+    },
+    
+    componentDidMount: function() {
+        this.setState({bools_list:getNodes('.bool')});
+    },
+    
+    render: function() {
+        return (
+            <g id='bools'>
+                {this.state.bools_list.map(function(entry, value) {
+                    return <ReactBool className='bool' key={entry['id']} attributes={entry['attributes']} styles={entry['style']} children={entry['children']}/>
+                })}
+            </g>
+        );
+    }
+});
+
+var ReactBool = React.createClass({
+    render: function() {
+        //hard-coded className
+        return (
+            <g className={this.props.className} {... this.props.attributes} style={this.props.styles}>
+                <ellipse {... this.props.children[0]['attributes']} style={this.props.children[0]['style']}>
+                </ellipse>
+                {//this.props.node.children is an HTMLCollection, not an array
+                this.props.children.slice(1).map(function(textTag, value) {
+                    //hard-coded textAnchor
+                    return <text key={value} textAnchor='middle' {... textTag['attributes']} style={textTag['style']}>{textTag['innerHTML']}</text>;
+                })}
+            </g>
+        );
+    }
+});
+
+
+var ReactEdges = React.createClass({
+    getInitialState: function() {
+        return {
+            edges_list: []
+        };
+    },
+    
+    componentDidMount: function() {
+        this.setState({edges_list:getNodes('.path')});
+    },
+    
+    render: function() {
+        return (
+            <g id='edges' stroke='black'>
+                {this.state.edges_list.map(function(entry, value) {
+                    return <ReactEdge className='path' key={entry['id']} attributes={entry['attributes']} styles={entry['style']} children={entry['children']}/>
+                })}
+            </g>
+        );
+    }
+});
+
+var ReactEdge = React.createClass({
+    render: function() {
+        //hard-coded className and markerEnd
+        return (
+            <path className={this.props.className} {... this.props.attributes} style={this.props.styles} markerEnd={'url(#arrow)'}>
+            </path>
         );
     }
 });
