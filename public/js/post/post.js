844a1c8b13c5350141b478f5ed3a5537ec89aba9
Christine Murad <christine.murad@mail.utoronto.ca>
2015-11-24 14:45:58 -0500

    specialist-final: Rernamed MultipleCourseCode to CourseCategory

--- public/js/post/post.js
+++ public/js/post/post.js
@@ -87,7 +87,7 @@ var CourseCode = React.createClass({
     }
 })
 
-var MultipleCourseCode = React.createClass({
+var CourseCategory = React.createClass({
     getInitialState: function() {
         return {
             completed: false,
@@ -255,15 +255,15 @@ var SpecialistPost = React.createClass({
                 <CourseYear yearName='First Year' courses={firstYearCourses} />
                 <CourseYear yearName='Second Year' courses={secondYearCourses} />
                 <CourseYear yearName='Later Years' courses={laterYearCourses} />
-                <MultipleCourseCode courseID='spec_400' textBoxNumber={3} courses={courseCategoryArrays[0]} textboxesDisabled={true}
+                <CourseCategory courseID='spec_400' textBoxNumber={3} courses={courseCategoryArrays[0]} textboxesDisabled={true}
                     categoryName='Any 400-level CSC course, BCB410H, BCB420H, BCB430Y, ECE489H (1.5 FCEs)' />
-                <MultipleCourseCode courseID='spec_300' textBoxNumber={3} courses={courseCategoryArrays[1]} textboxesDisabled={true}
+                <CourseCategory courseID='spec_300' textBoxNumber={3} courses={courseCategoryArrays[1]} textboxesDisabled={true}
                     categoryName='Any 300+ level CSC course, BCB410H, BCB420H, BCB430Y, ECE385H, ECE489H (1.5 FCEs)' />
-                <MultipleCourseCode courseID="spec_extra" textBoxNumber={4} courses={courseCategoryArrays[2]} textboxesDisabled={false}
+                <CourseCategory courseID="spec_extra" textBoxNumber={4} courses={courseCategoryArrays[2]} textboxesDisabled={false}
                     categoryName='Any of the following: 300+ level CSC course; MAT: 235/237/257, any 300+ 
                                   except for 329, 390, & 391; STA: 248, 261, any 300+; ECE: 385H/489H; 
                                   BCB: 410H/420H/430Y (2.0 FCEs)' />
-                <MultipleCourseCode courseID="spec_inq" textBoxNumber={1} courses={courseCategoryArrays[3]} textboxesDisabled={true}
+                <CourseCategory courseID="spec_inq" textBoxNumber={1} courses={courseCategoryArrays[3]} textboxesDisabled={true}
                     categoryName='Any from this list: CSC301H, CSC318H, CSC404H, CSC411H, CSC418H, CSC420H, 
                     CSC428H, CSC454H, CSC485H, CSC490H, CSC491H, CSC494H, or PEY (0.5 FCEs) 
                     ** Note: Type "PEY" for Check my POSt to recognize it **' />
