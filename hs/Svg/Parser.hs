56a869a8dd7e9205d0a82687ce194d6624a679e1
david-yz-liu <liudavid@cs.toronto.edu>
2015-12-07 22:18:58 -0500

    Add missing graph files and parsing actions.

--- hs/Svg/Parser.hs
+++ hs/Svg/Parser.hs
@@ -36,17 +36,18 @@ import Config (graphPath)
 import Text.Read (readMaybe)
 
 parsePrebuiltSvgs :: IO ()
-parsePrebuiltSvgs =
-    do
-        performParse "Computer Science" "csc2015.svg"
-        performParse "Statistics" "sta2015.svg"
-        performParse "Biochemistry" "bch2015.svg"
-        performParse "Cell & Systems Biology" "csb2015.svg"
-        performParse "Estonian" "est2015.svg"
-        performParse "Finnish" "fin2015.svg"
-        performParse "Italian" "ita2015.svg"
-        performParse "Linguistics" "lin2015.svg"
-        performParse "Rotman" "rotman2015.svg"
+parsePrebuiltSvgs = do
+    performParse "Computer Science" "csc2015.svg"
+    performParse "Statistics" "sta2015.svg"
+    performParse "Biochemistry" "bch2015.svg"
+    performParse "Cell & Systems Biology" "csb2015.svg"
+    performParse "Estonian" "est2015.svg"
+    performParse "Finnish" "fin2015.svg"
+    performParse "Italian" "ita2015.svg"
+    performParse "Linguistics" "lin2015.svg"
+    performParse "Rotman" "rotman2015.svg"
+    performParse "Economics" "eco2015.svg"
+    performParse "Spanish" "spa2015.svg"
 
 performParse :: String -- ^ The title of the graph.
              -> String -- ^ The filename of the file that will be parsed.
