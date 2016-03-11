368ac36e62e2663dab924a289d8329f4d5234be9
cchens <mail@christinachen.ca>
2016-03-10 20:20:35 -0500

    react-grid: added react_course file

---
+++ public/js/common/objects/react_course.js
@@ -0,0 +1,100 @@
+var Course = React.createClass({
+
+    getDefaultProps: function() {
+        var course = getCourse("CSC108H1");
+
+        return {
+            F: course.fallSession,
+            S: course.springSession,
+            Y: course.yearSession,
+            name: course.name,
+            title: course.title,
+            prereqs: course.prereqs,
+            prereqString: course.prereqString,
+            coreqs: course.coreqs,
+            breadth: course.breadth,
+            prep: course.prep,
+            description: course.description,
+            exclusions: course.exclusions,
+            distribution: course.distribution,
+            manualTutorialEnrolment: course.manualTutorialEnrolment,
+            manualPracticalEnrolment: course.manualPracticalEnrolment,
+            videoUrls: course.videoUrls
+        };
+    },
+
+    getCourse: function(courseName) {
+        'use strict';
+
+        var course;
+        $.ajax({
+            url: 'course',
+            dataType: 'json',
+            data: {name : courseName},
+            async: false,
+            success: function (data) {
+                course = data;
+            },
+            error: function () {
+                throw 'No course file';
+            }
+        });
+
+        return course;
+    },
+
+    render: function() {
+
+        var fallLectures = this.props.F.lectures.map(function (section) {
+            return <Section section={section} />
+        });
+
+        var springLectures = this.props.S.lectures.map(function (section) {
+            return <Section section={section} />
+        });
+
+        var yearLectures = this.props.Y.lectures.map(function (section) {
+            return <Section section={section} />
+        });
+
+        return (
+            <div>
+                <div>{fallLectures}</div>
+                <div>{springLectures}</div>
+                <div>{yearLectures}</div>
+            </div>
+        );
+    }
+
+});
+
+var Section = React.createClass({
+
+    getInitialState: function() {
+        return {
+            clicked: false, satisfied: true
+        };
+    },
+
+    render: function() {
+
+        var section = this.props.section;
+        var id = section.name + '-' + section.section + '-' + section.session;
+
+        return (
+            <div>
+                <li id={id} clicked={this.state.clicked} satisfied={this.state.satisfied}>
+                    <div data-custom-attribute="instructor">{section.instructor}</div>
+                    <div data-custom-attribute="cap">{section.cap}</div>
+                    <div data-custom-attribute="enrol">{section.enrol}</div>
+                    <div data-custom-attribute="wait">{section.wait}</div>
+                </li>
+            </div>
+        );
+    }
+});
+
+
+React.render(
+    <Course />,
+    document.getElementById('course-select-wrapper'));
