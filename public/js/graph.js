94588e3e0d496626271be9a4223aa41c84b57eb2
david-yz-liu <david@cs.toronto.edu>
2016-04-09 12:58:58 -0400

    graph-js-modules: convert sidebar files

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,40 +1,56 @@
 requirejs([
-    'es6!graph/react_graph'],
+    'es6!graph/react_graph',
+    'es6!graph/sidebar/sidebar_divs',
+    'es6!graph/sidebar/focus_descriptions'],
     function (
-        reactNode
+        reactGraph,
+        sidebarDivs,
+        focusInfo
     ) {
 
     $(document).ready(function () {
         'use strict';
-        var graphComponent = reactNode.renderReactGraph();
-
-        // Set sidebar onclick. Eventually move this into its own React component.
-        $('.graph-button').click(function () {
-            var id = $(this).data('id');
-            var name = $(this).text();
-            graphComponent.getGraph(name);
-            changeFocusEnable(id);
-        });
+        var graphComponent = reactGraph.renderReactGraph();
 
         // Set focus button onclicks
         $('.focus').click(function(e) {
             var id = $(this).attr('id');
             var focusDetails = $('#' + id + '-details');
 
-            if (graphComponent.state.highlightedNodes == window[id + 'FocusList']) {
+            if (graphComponent.state.highlightedNodes == focusInfo[id + 'FocusList']) {
                 graphComponent.setState({highlightedNodes: []});
                 focusDetails.animate({height: '2px'}, 'fast');
             } else {
                 $('.details').css('height', '2px');
                 focusDetails.animate({height: '128px'}, 'fast');
-                focusDetails.html(window[id + 'Description']);
-                graphComponent.setState({highlightedNodes: window[id + 'FocusList']});
+                focusDetails.html(focusInfo[id + 'Description']);
+                graphComponent.setState({highlightedNodes: focusInfo[id + 'FocusList']});
             }
         });
 
-        // Enable Reset button
+        // Sidebar initialization.
+        // TODO: move sidebar into its own React component.
         $('#reset').click(function () {
             graphComponent.reset();
-        })
+        });
+
+        $.ajax({
+            url: 'graphs',
+            dataType: 'json',
+            success: function (data) {
+                sidebarDivs.createGraphButtons(data);
+                $('.graph-button').click(function () {
+                    var id = $(this).data('id');
+                    var name = $(this).text();
+                    graphComponent.getGraph(name);
+                    sidebarDivs.changeFocusEnable(id);
+                });
+            },
+            error: function () {
+                throw 'No graphs in database';
+            }
+        });
+
+        sidebarDivs.activateSidebar();
     });
 });
