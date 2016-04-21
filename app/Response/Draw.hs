490f0146094d18e786cb61cfe0b2f283663ef6b9
R0ss <ross.gatih@gmail.com>
2016-04-21 00:38:43 -0400

    Created colour pallet.

--- app/Response/Draw.hs
+++ app/Response/Draw.hs
@@ -7,6 +7,7 @@ import           Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Data.Text as T
+import Control.Monad (forM_, mapM_)
 import Happstack.Server
 import MasterTemplate
 import Scripts
@@ -53,5 +54,8 @@ modePanel = H.div ! A.id "side-panel-wrap" $ do
             ! A.autocomplete "off"
             ! A.type_ "text"
             ! A.size "30"
+    H.table !A.id "colour-table" $ forM_ ([["", "", "", "", ""],
+                                          ["", "", "", "", ""]] :: [[String]])
+                                         (H.tr . mapM_ (H.td . H.toHtml))
     H.div ! A.id "submit-graph-name" ! A.class_ "button" $ "Submit"
     H.div ! A.id "json-data" ! A.class_ "json-data" $ ""
