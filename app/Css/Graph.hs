9c18d62f0103805d25ab4056d273a5fb47590ab1
Kelly Bell <kbell109@gmail.com>
2016-05-25 12:39:51 -0400

    Added CSS for panning buttons and modified location of zoom buttons

--- app/Css/Graph.hs
+++ app/Css/Graph.hs
@@ -230,11 +230,23 @@ graphContainer = do
         height (px 24)
         position absolute
     "#zoom-in-button" ? do
-        top (px 50)
-        right (px 50)
+        top (px 85)
+        right (px 31)
     "#zoom-out-button" ? do
-        top (px 50)
-        right (px 26)
+        top (px 85)
+        right (px 55)
+    "#pan-up-button" ? do
+        top (px 5)
+        right (px 43)
+    "#pan-down-button" ? do
+        top (px 55)
+        right (px 43)
+    "#pan-right-button" ? do
+        top (px 30)
+        right (px 31)
+    "#pan-left-button" ? do
+        top (px 30)
+        right (px 55)
 
 sidebarCSS :: Css
 sidebarCSS = do
