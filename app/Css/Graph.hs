e165c6eba4984406977854bc0461c2a12888e43e
Kelly Bell <kbell109@gmail.com>
2016-06-30 12:15:40 -0400

    Made some changes to the css to make the zoom and pan buttons look a bit better. Switching them to buttons from divs caused them to inherit some less desirable traits.

--- app/Css/Graph.hs
+++ app/Css/Graph.hs
@@ -227,7 +227,9 @@ graphContainer = do
         backgroundColor white
         border solid (px 1) purple10
         "border-radius" -: "6px"
-        fontSize (px 16)
+        "outline" -: "none"
+        fontSize (px 14)
+        fontWeight bold
         fontColor purple10
         cursor pointer
         display inlineBlock
@@ -235,33 +237,37 @@ graphContainer = do
         height (px 25)
         position absolute
         ":hover" & do
-            backgroundColor purple8
+            backgroundColor purple6
             fontColor white
         ":active" & do
             backgroundColor purple10
             fontColor white
+        ":disabled" & do
+            border solid (px 1) grey2
+            backgroundColor white
+            fontColor grey2
     "#zoom-in-button" ? do
         top (px 85)
-        right (px 31)
+        right (px 30)
     "#zoom-out-button" ? do
         top (px 85)
-        right (px 55)
+        right (px 57)
     "#pan-up-button" ? do
-        top (px 5)
+        top (px 4)
         right (px 43)
     "#pan-down-button" ? do
-        top (px 55)
+        top (px 57)
         right (px 43)
     "#pan-right-button" ? do
         top (px 30)
-        right (px 31)
+        right (px 30)
     "#pan-left-button" ? do
         top (px 30)
-        right (px 55)
+        right (px 57)
     "#reset-button" ? do
-        width (px 49)
+        width (px 52)
         top (px 111)
-        right (px 31)
+        right (px 30)
 
 sidebarCSS :: Css
 sidebarCSS = do
