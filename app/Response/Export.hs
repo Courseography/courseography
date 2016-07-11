b64f2fa8a3a56a2867a9da7111cc3c674ca8cb00
Kelly Bell <kbell109@gmail.com>
2016-07-11 10:16:58 -0400

    Changed exportResponse function to serve a pdf file rather than displaying the pdf using html.

--- app/Response/Export.hs
+++ app/Response/Export.hs
@@ -8,14 +8,7 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Happstack.Server
 
+
 exportResponse :: ServerPart Response
 exportResponse =
-    ok $ toResponse $
-        displayPDF
-
-
-{- Insert a large loading icon into the page -}
-displayPDF :: H.Html
-displayPDF =
-    H.div ! A.id "exportDiv" $
-        H.img ! A.id "export-pdf" ! A.src "../CSgraph.pdf"
+    serveDirectory DisableBrowsing ["CSGraph.pdf"] "."
