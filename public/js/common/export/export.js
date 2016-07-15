fa9a225dc47befdaf9217d1a1619a6c258c22dec
david-yz-liu <david@cs.toronto.edu>
2016-07-14 22:18:12 -0400

    fix-export: fix export modal for grid

--- public/js/common/export/export.js
+++ public/js/common/export/export.js
@@ -6,8 +6,7 @@ $(document).ready(function () {
 
 
 /**
- * Creates and returns the Facebook modal content div.
- * @returns {jQuery} The Facebook modal content div.
+ * Creates and displays the Export modal content div.
  */
 function openExportModal() {
     'use strict';
@@ -15,24 +14,4 @@ function openExportModal() {
     var context = $('#courseography-header').attr('context');
     var session = 'fall';
     var img = (context === 'graph') ? getGraphImage() : getGridImage(session);
-    if (context === 'grid') {
-        var contentDiv = $('<div></div>');
-        var topContentDiv = $('<div></div>');
-        var calendarOption = $('<a href="calendar">Download ICS</a>');
-        calendarOption.attr('target', '_blank');
-        topContentDiv.html('<img id="post-image" src="data:image/png;base64,' + img + '" />');
-        contentDiv.attr('id', 'modal-content-container')
-                  .append(calendarOption)
-                  .append(topContentDiv);
-
-        var sessionButton = $('<button type="button" class="btn btn-primary">Switch Sessions</button>');
-        sessionButton.click(function () {
-            session = session === 'fall' ? 'spring' : 'fall';
-            img = getGridImage(session);
-            $('#post-image').attr('src', 'data:image/png;base64,' + img);
-        });
-        contentDiv.append(sessionButton);
-    }
-
-    return contentDiv;
 }
