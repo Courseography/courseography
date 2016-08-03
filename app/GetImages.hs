8888ea0dbb1c6810fab4aee99659746d3fd803cf
bell-kelly <kbell109@gmail.com>
2016-08-03 13:55:31 -0400

    Extracted helper funciton to GetImages that gets the active graph using cookies. Image response now uses that helper function.

--- app/GetImages.hs
+++ app/GetImages.hs
@@ -1,13 +1,26 @@
  module GetImages 
- 	(getGraphImage, getTimetableImage) where
+    (getActiveGraphImage, getTimetableImage) where
 
 import TimetableImageCreator (renderTable)
 import qualified Data.Map as M
 import System.Random
 import Svg.Generator
 import ImageConversion
+import Happstack.Server
+import Data.List.Utils (replace)
 
- -- | Creates an image, and returns the name of the svg used to create the
+-- | If there is an active graph available, an image of that graph is created,
+-- otherwise the Computer Science graph is created as a default. 
+-- Either way, the resulting graph's .svg and .png names are returned. 
+getActiveGraphImage :: Request -> IO (String, String)
+getActiveGraphImage req = do
+    let cookies = M.fromList $ rqCookies req
+        graphName =
+            replace "-" " " $
+                maybe "Computer-Science" cookieValue (M.lookup "active-graph" cookies)
+    getGraphImage graphName (M.map cookieValue cookies)
+
+-- | Creates an image, and returns the name of the svg used to create the
 -- image and the name of the image
 getGraphImage :: String -> M.Map String String -> IO (String, String)
 getGraphImage graphName courseMap = do
