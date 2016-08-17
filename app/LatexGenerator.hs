d61ead66c5381f1e9ca29d9210eb9d3a2310c5f5
bell-kelly <kbell109@gmail.com>
2016-08-17 12:56:10 -0400

    Added a new file (LatexGenerator.hs) that can be used to generate latex files with a variable number of images included.

---
+++ app/LatexGenerator.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module LatexGenerator
+    (generateTex) where
+
+import Text.LaTeX
+import Text.LaTeX.Packages.Graphicx
+
+-- | Create a TEX file named texName that includes all of the images in 
+-- imageNames
+generateTex :: [String] -> String -> IO ()
+generateTex imageNames texName = execLaTeXT (buidTex imageNames) >>= renderFile texName
+
+-- | Combine the preamble and the document text into a single block of latex 
+-- code. The document text contains code to insert all of the images in
+-- imageNames.
+buidTex :: Monad m => [String] -> LaTeXT_ m
+buidTex imageNames = do
+    preamble
+    document (body imageNames)
+
+-- | Defines documentclass and packages used.
+preamble :: Monad m => LaTeXT_ m
+preamble = do
+    documentclass [] article
+    usepackage [] graphicx
+
+-- | Adds an includegraphics command for each image in imageNames. If an empty
+-- list of imageNames was provided, the body will be empty. 
+body :: Monad m => [String] -> LaTeXT_ m
+body [] = do ""
+body (imageName:imageNames) = do
+    center $ includegraphics [IGWidth (CustomMeasure linewidth)] imageName 
+    body imageNames
