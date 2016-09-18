fb795cbc03cb0535c7bb96ec4afa40fbe2750057
Lana El Sanyoura <lana.elsanyoura@mail.utoronto.ca>
2016-09-17 21:27:04 -0400

    Issue 870 Partially Fixed: Reset the scroll bar in info box whenever the user begins to scroll.

--- public/js/common/react_modal.js
+++ public/js/common/react_modal.js
@@ -20,7 +20,7 @@ export var Modal = React.createClass({
     render: function () {
         if (this.state.courseId) {
             return (
-                <div className='modal fade' >
+                <div className='modal fade'>
                     <div className='modal-dialog'>
                         <div className='modal-content'>
                             <div className='modal-header'>
@@ -37,7 +37,7 @@ export var Modal = React.createClass({
                 </div>
             );
         } else {
-            return <div className='modal fade' ></div>;
+            return <div className='modal fade'></div>;
         }
     }
 });
