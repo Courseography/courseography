242ec784ab74b71ed19f82ea1a45d4e0e2a11df7
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-10-20 19:39:24 -0400

    Added showReqs function for ReqBuild.

--- app/Database/ReqBuild.hs
+++ app/Database/ReqBuild.hs
@@ -7,10 +7,19 @@ We will use parsed data to create instances of this type.
 -}
 module ReqBuild
 ( Req(..)
-, addReq
+, showReq
 ) where
 
 import Data.String
-import Database.Course
+import qualified Database.Course as C
 
+-- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
 data Req = J Course | AND [Req]| OR [Req] | FROM (Integer) [Req]
+
+-- | Returns a well formatted String representing the Course(s) relationships.
+showReqs :: Req -> String
+showReqs (J course) = C.name course
+showReqs (AND x:xs) = showReqs x + ", " + showReqs (AND xs)
+showReqs (OR x:xs) = showReqs x + "/ " + showReqs (OR xs)
+showReqs (FROM (a) x:xs) =  show a + "FCE(s) from: (" + showReqs x + ", "
+					     +  showReqs (xs) + ")"
