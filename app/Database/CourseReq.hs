fc18495f13ac07ebe69bad26ef751e077a629a29
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-10-20 18:59:49 -0400

    Added ShowCourseReq function to CourseReq.

--- app/Database/CourseReq.hs
+++ app/Database/CourseReq.hs
@@ -14,5 +14,13 @@ import Data.String
 import Database.Course
 import Database.ReqBuild
 
-data CourseReq = CREQ (Course) [Req] | EXCL (Course) [Req]
-             | PREQ (Course) [Req] deriving (Show)
+data CourseReq = CREQ Course [Req] | EXCL Course [Req]
+               | PREQ Course [Req]
+
+showCourseReq :: CourseReq -> String
+showCourseReq (CREQ course Req) = "Corequisites for" + name course + ":/n"
+							    + showReqs Req + "/n"
+showCourseReq (EXCL course Req) = "Exclusions for" + name course + ":/n"
+							    + showReqs Req + "/n"
+showCourseReq (PREQ course Req) = "Prerequisites for" + name course + ":/n"
+							    + showReqs Req + "/n"
