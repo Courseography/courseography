06d9ba33fed54322c4a4d54861f446714b504615
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-10-20 19:03:06 -0400

    Fixed syntax in CourseReq.

--- app/Database/CourseReq.hs
+++ app/Database/CourseReq.hs
@@ -5,22 +5,22 @@ Module containing data type that represents a Course Requirement.
 
 We will use parsed data to create instances of this type.
 -}
-module Coursereq
+module CourseReq
 ( CourseReq(..)
-, ShowCourseReq()
+, showCourseReq
 ) where
 
 import Data.String
 import Database.Course
 import Database.ReqBuild
 
-data CourseReq = CREQ Course [Req] | EXCL Course [Req]
-               | PREQ Course [Req]
+data CourseReq = CREQ Course Req | EXCL Course Req
+               | PREQ Course Req
 
 showCourseReq :: CourseReq -> String
 showCourseReq (CREQ course Req) = "Corequisites for" + name course + ":/n"
-							    + showReqs Req + "/n"
+							    + showReq Req + "/n"
 showCourseReq (EXCL course Req) = "Exclusions for" + name course + ":/n"
-							    + showReqs Req + "/n"
+							    + showReq Req + "/n"
 showCourseReq (PREQ course Req) = "Prerequisites for" + name course + ":/n"
-							    + showReqs Req + "/n"
+							    + showReq Req + "/n"
