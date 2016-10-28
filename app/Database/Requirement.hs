3adbfe2f9823b225d85d532aabfa283fa69364e7
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-10-28 17:05:42 -0400

    Fix syntax for newline character, use intercalate function to implement show Req.

--- app/Database/Requirement.hs
+++ app/Database/Requirement.hs
@@ -18,15 +18,15 @@ data ProgramReq = PRGREQ String [Req]
 
 -- | Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
-	show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":/n" ++ map show reqs
+	show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ map show reqs
 
 
 data CourseReq = CRSREQ String Req Req Req
 
 instance Show CourseReq where
-	show (CREQ course creq excl preq) = "Corequisites for " ++ course ++ ":/n" ++ show req ++ "/n"
-									  ++ "Exclusions for " ++ course ++ ":/n" ++ show req ++ "/n"
-									  ++ "Prerequisites for " ++ course ++ ":/n" ++ show req ++ "/n"
+	show (CREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+									  ++ "Exclusions for " ++ course ++ ":\n" ++ show req ++ "\n"
+									  ++ "Prerequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
 
 
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
@@ -34,6 +34,6 @@ data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req]
 
 instance Show Req where
 	show (J course) = course
-	show (AND x:xs) = show x ++ ", " ++ show AND xs
-	show (OR x:xs) = show x ++ "/ " ++ show OR xs
-	show (FROM a x:xs) =  show a ++ "FCE(s) from: (" ++ show x ++ ", " ++  show xs ++ ")"
+	show (AND x:xs) = S.intercalate "," $ map show [x, (AND xs)]
+	show (OR x:xs) = S.intercalate "/" $ map show [x, (OR xs)]
+	show (FROM a x) =  show a ++ "FCE(s) from: (" ++ show x ++ ")"
