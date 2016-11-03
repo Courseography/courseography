ce94260842237c3e0b8ea1c4f5de13a5a5cf2c36
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-03 03:43:25 -0400

    Convert indents to spaces.Implement better syntax for show Req.

--- app/Database/Requirement.hs
+++ app/Database/Requirement.hs
@@ -18,22 +18,22 @@ data ProgramReq = PRGREQ String [Req]
 
 -- | Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
-	show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ map show reqs
+    show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ map show reqs
 
 
 data CourseReq = CRSREQ String Req Req Req
 
 instance Show CourseReq where
-	show (CREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
-									  ++ "Exclusions for " ++ course ++ ":\n" ++ show req ++ "\n"
-									  ++ "Prerequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+    show (CREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+                                      ++ "Exclusions for " ++ course ++ ":\n" ++ show req ++ "\n"
+                                      ++ "Prerequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
 
 
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
 data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req]
 
 instance Show Req where
-	show (J course) = course
-	show (AND x:xs) = S.intercalate "," $ map show [x, (AND xs)]
-	show (OR x:xs) = S.intercalate "/" $ map show [x, (OR xs)]
-	show (FROM a x) =  show a ++ "FCE(s) from: (" ++ show x ++ ")"
+    show (J course) = course
+    show (AND reqs) = S.intercalate "," $ map show reqs
+    show (OR reqs) = S.intercalate "/" $ map show reqs
+    show (FROM a x) =  show a ++ "FCE(s) from: (" ++ show x ++ ")"
