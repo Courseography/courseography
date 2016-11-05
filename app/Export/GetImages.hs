f38c8f9dfa5e436f1e31bdaa89ea64cd1d21fe37
Ailsa (Tiantian) Fang <tfang@sysomos.com>
2016-11-05 18:35:14 -0400

    takes a time and returns a list of tuples that correctly index into the 2-D table and some minor clean

--- app/Export/GetImages.hs
+++ app/Export/GetImages.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Export.GetImages
     (getActiveGraphImage, getTimetableImage, randomName, getActiveTimetable) where
 
@@ -6,7 +7,7 @@ import qualified Data.Map as M
 import System.Random
 import Svg.Generator
 import Export.ImageConversion
-import Happstack.Server (Request, rqCookies, cookieValue)
+import Happstack.Server (Request, rqCookies, cookieValue, Cookie)
 import Data.List.Utils (replace)
 import Data.List.Split (splitOn)
 import Database.CourseQueries (getLectureTime, getTutorialTime)
@@ -15,23 +16,21 @@ import Control.Monad.IO.Class  (liftIO)
 import Data.List (partition)
 import Database.Persist.Sqlite (runSqlite, runMigration)
 import Config (databasePath)
-import Data.Fixed
+import Data.Fixed (mod')
 
 -- | If there is an active graph available, an image of that graph is created,
 -- otherwise the Computer Science graph is created as a default.
 -- Either way, the resulting graph's .svg and .png names are returned.
 getActiveGraphImage :: Request -> IO (String, String)
 getActiveGraphImage req = do
-    let cookies = M.fromList $ rqCookies req  --  Map String Cookie
+    let cookies = M.fromList $ rqCookies req
         graphName =
             replace "-" " " $
-                maybe "Computer-Science" cookieValue (M.lookup "active-graph" cookies)  -- if M.lookup "active-graph" cookies is Nothing, then "Computer Science", else get cookieValue of coookie
+                maybe "Computer-Science" cookieValue (M.lookup "active-graph" cookies)
     getGraphImage graphName (M.map cookieValue cookies)
 
 
 -- =================================
-
--- data CourseInfo = CourseInfo {code :: String, section :: String, session :: String, time :: [Time]} deriving (Show)
 
 -- | Get lectures selected by user from selected-lectures cookie. If DNE, assume no lecture seleted
 -- get "CSC148H1-L0301-S_STA257H1-T0105-F_STA257H1-L0101-F_STA303H1-L0101-S_CSC148H1-T5101-F"
@@ -40,7 +39,7 @@ getActiveGraphImage req = do
 getActiveTimetable :: Request -> String -> IO (String, String)
 getActiveTimetable req session = do
     -- get cookie value of "selected-lectures" from browser
-    let cookies = M.fromList $ rqCookies req  --  Map String Cookie
+    let cookies :: M.Map String Cookie = M.fromList $ rqCookies req  --  Map String Cookie
         coursecookie = maybe "" cookieValue $ M.lookup "selected-lectures" cookies
         (lecture, tutorial) = parseCourseCookie coursecookie session  -- ([CourseInfo], [CourseInfo])
     (lecTimes, tutTimes) <- getTimes (lecture, tutorial)
@@ -52,24 +51,16 @@ getActiveTimetable req session = do
 -- | Parse cookie string to two lists of courses, one for lecture, the other for tutorial
 -- "CSC148H1-L5101-S_CSC148H1-T0501-S_STA355H1-L0101-F_CSC108H1-L0102-F"
 -- [("STA355H1","LEC-0101","F"),("CSC108H1","LEC-0102","F")], [])
--- parseCourseCookie :: String -> String -> ([(String, String, String)], [(String, String, String)])
--- parseCourseCookie s session = let lecAndTut = map (splitOn "-") $ splitOn "_" s
---                                   (lecture, tutorial) = partition isLec lecAndTut
---                                   [lectureOfsession, tutorialOfsession] = map (filter (\x -> or ([(x !! 2 !! 0) == (head session), (x !! 2 !! 0) == 'Y']))) [lecture, tutorial]
---                                   lecture' = map (convertLec . list2tuple) lectureOfsession
---                                   tutorial' = map (convertTut . list2tuple) tutorialOfsession
---                               in (lecture', tutorial')
---                               where isLec x = (x !! 1 !! 0) == 'L'
-
 parseCourseCookie :: String -> String -> ([CourseInfo], [CourseInfo])
-parseCourseCookie s session = let lecAndTut = map (splitOn "-") $ splitOn "_" s
-                                  (lecture, tutorial) = partition isLec lecAndTut
-                                  -- get lecture and tutorial in this session
-                                  [lectureOfsession, tutorialOfsession] = map (filter (\x -> or ([(x !! 2 !! 0) == (head session), (x !! 2 !! 0) == 'Y']))) [lecture, tutorial]
-                                  lecture' = map (convertLec . list2tuple) lectureOfsession
-                                  tutorial' = map (convertTut . list2tuple) tutorialOfsession
-                              in (lecture', tutorial')
-                              where isLec x = (x !! 1 !! 0) == 'L'
+parseCourseCookie s session = 
+  let lecAndTut = map (splitOn "-") $ splitOn "_" s
+      (lecture, tutorial) = partition isLec lecAndTut
+      -- get lecture and tutorial in this session
+      [lectureOfsession, tutorialOfsession] = map (filter (\x -> or ([(x !! 2 !! 0) == (head session), (x !! 2 !! 0) == 'Y']))) [lecture, tutorial]
+      lecture' = map (convertLec . list2tuple) lectureOfsession
+      tutorial' = map (convertTut . list2tuple) tutorialOfsession
+  in (lecture', tutorial')
+  where isLec x = (x !! 1 !! 0) == 'L'
 
 list2tuple :: [String] -> (String, String, String)
 list2tuple [a, b, c] = (a, b, c)
@@ -86,64 +77,44 @@ convertTut (code, section, session) = CourseInfo {code = code, section = take 1 
 
 -- | Get data from database for selected courses
 getTimes :: ([CourseInfo], [CourseInfo]) -> IO ([CourseInfo], [CourseInfo])
-getTimes (lecture, tutorial) = runSqlite databasePath $ do
+getTimes (lectures, tutorials) = runSqlite databasePath $ do
     runMigration migrateAll
-    lecTimes <- mapM getLectureTime lecture  -- [(String, String, String)] -> IO ([[Time]])
-    tutTimes <- mapM getTutorialTime tutorial
+    lecTimes <- mapM getLectureTime lectures  -- [(String, String, String)] -> IO ([[Time]])
+    tutTimes <- mapM getTutorialTime tutorials
     return (lecTimes, tutTimes)
 
 
 -- | Generate scheduel in special format based on Times
 getScheduleByTime :: [CourseInfo] -> [CourseInfo] -> [[String]]
-getScheduleByTime lecTimes tutTimes = let allTimes = lecTimes ++ tutTimes
-                                          schedule = replicate 13 $ replicate 5 ""
-                                          schedule' = foldl (\acc x -> addCourseToSchedule x acc) schedule allTimes
-                                      in schedule'
-
--- getScheduleByTime :: [(String, String, String)] -> [(String, String, String)] -> [[Time]] -> [[Time]] -> [[String]]
--- getScheduleByTime lecture tutorial lecTimes tutTimes = let lecture_times = zip lecture lecTimes
---                                                            tutorial_times = zip tutorial tutTimes
---                                                            all_times = lecture_times ++ tutorial_times
---                                                            schedule = replicate 13 $ replicate 5 ""
---                                                            schedule' = foldl (\acc x -> addCourseToSchedule x acc) schedule all_times
---                                                        in schedule'
-
-
+getScheduleByTime lecTimes tutTimes =
+  let allTimes = lecTimes ++ tutTimes
+      schedule = replicate 13 $ replicate 5 ""
+      schedule' = foldl addCourseToSchedule schedule allTimes
+  in schedule'
 
 -- CourseInfo {code = "STA355H1", section = "L0101", session = "F", time = [Time {timeField = [0.0,14.0]},Time {timeField = [0.0,14.5]},Time {timeField = [0.0,15.0]}]}
-addCourseToSchedule :: CourseInfo -> [[String]] -> [[String]]
-addCourseToSchedule courseInfo schedule
+addCourseToSchedule :: [[String]] -> CourseInfo -> [[String]]
+addCourseToSchedule schedule courseInfo
     | (time courseInfo) == [] = schedule
-    | otherwise = let time' = filter (\t-> (mod' (timeField t !! 1) 1) == 0) (time courseInfo)
-            in foldl (\acc x -> addCourseHelper (code courseInfo) (section courseInfo) (session courseInfo) acc x) schedule time'
+    | otherwise = 
+        let time' = filter (\t-> (mod' (timeField t !! 1) 1) == 0) (time courseInfo)
+            timeArray = convertTimeToArray time'
+            in foldl (\acc x -> addCourseHelper (code courseInfo) (section courseInfo) (session courseInfo) acc x) schedule timeArray
 
--- (("STA355H1","L0101","F"),Just [Time {timeField = [0.0,14.0]},Time {timeField = [0.0,14.5]},Time {timeField = [0.0,15.0]},Time {timeField = [0.0,15.5]},Time {timeField = [2.0,14.0]},Time {timeField = [2.0,14.5]}])
--- [["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]]
--- addCourseToSchedule :: ((String, String, String), [Time]) -> [[String]] -> [[String]]
--- addCourseToSchedule (course, courseTimes) schedule = foldl (\acc x -> addCourseHelper course acc x) schedule courseTimes
+-- | Take a list of Time and returns a list of tuples that correctly index into the 2-D table (for generating the image)
+-- Time {timeField = [0.0,14.0]} -> (0, 6)
+convertTimeToArray :: [Time] -> [(Int, Int)]
+convertTimeToArray = map (\x -> (floor $ timeField x !! 0 , floor $ timeField x !! 1 - 8))
 
 
 --  "STA355H1" "L0101" "F"  Time {timeField = [0.0,14.0]}
-addCourseHelper :: String -> String -> String -> [[String]] -> Time -> [[String]]
-addCourseHelper code section session acc x = let [day, time'] = map floor $ timeField x
-                                                 time = time' - 8
-                                                 time_schedule = acc !! time
-                                                 current_schedule = if (null $ time_schedule !! day) then (code++session++" "++section) else (time_schedule !! day ++ ("&"++code++session++" "++section))
-                                                 -- time_schedule' = (take day time_schedule) ++ [code++session++" "++section] ++ (drop (day + 1) time_schedule)
-                                                 time_schedule' = (take day time_schedule) ++ [current_schedule] ++ (drop (day + 1) time_schedule)
-                                                 newacc = (take time acc) ++ [time_schedule'] ++ (drop (time + 1) acc)
-                                             in newacc
-
-
--- ("STA355H1","L0101","F")  Time {timeField = [0.0,14.0]}
--- addCourseHelper :: (String, String, String) -> [[String]] -> Time -> [[String]]
--- addCourseHelper (code, section, session) acc x = let [day, time'] = map floor $ timeField x
---                                                      time = time' - 8
---                                                      time_schedule = acc !! time
---                                                      time_schedule' = (take day time_schedule) ++ [code++session++" "++section] ++ (drop (day + 1) time_schedule)
---                                                      newacc = (take time acc) ++ [time_schedule'] ++ (drop (time + 1) acc)
---                                                  in newacc
-
+addCourseHelper :: String -> String -> String -> [[String]] -> (Int, Int) -> [[String]]
+addCourseHelper code section session acc (day, time) = 
+  let time_schedule = acc !! time
+      current_schedule = if (null $ time_schedule !! day) then (code++session++" "++section) else (time_schedule !! day ++ ("&"++code++session++" "++section))
+      time_schedule' = (take day time_schedule) ++ [current_schedule] ++ (drop (day + 1) time_schedule)
+      newacc = (take time acc) ++ [time_schedule'] ++ (drop (time + 1) acc)
+  in newacc
 
 generateTimetableImg :: [[String]] -> String -> IO(String, String)
 generateTimetableImg schedule session = do
