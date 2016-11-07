f87f44c3af448ac8b706b3ee18c74d94822d6c16
Ailsa (Tiantian) Fang <tfang@sysomos.com>
2016-11-07 03:11:08 -0500

    finx some warning for getimages

--- app/Export/GetImages.hs
+++ app/Export/GetImages.hs
@@ -37,26 +37,25 @@ getActiveGraphImage req = do
 -- return [[Time {timeField = [2.0,18.0]},Time {timeField = [2.0,18.5]},Time {timeField = [2.0,19.0]},Time {timeField = [2.0,19.5]},Time {timeField = [2.0,20.0]},Time {timeField = [2.0,20.5]}],[Time {timeField = [0.0,14.0]},Time {timeField = [0.0,14.5]},Time {timeField = [0.0,15.0]},Time {timeField = [0.0,15.5]},Time {timeField = [2.0,14.0]},Time {timeField = [2.0,14.5]}],Just [Time {timeField = [2.0,10.0]},Time {timeField = [2.0,10.5]},Time {timeField = [0.0,10.0]},Time {timeField = [0.0,10.5]},Time {timeField = [4.0,10.0]},Time {timeField = [4.0,10.5]}], [Time {timeField = [4.0,11.0]},Time {timeField = [4.0,11.5]},Time {timeField = [4.0,12.0]},Time {timeField = [4.0,12.5]}]]
 -- [["8:00","","","","",""],["9:00","","","","",""],["10:00","CSC108 (L)","","CSC108 (L)","","CSC108 (L)"],["11:00","","","","",""],["12:00","","","","",""],["1:00","","","","",""],["2:00","STA355 (L)","","STA355 (L)","",""],["3:00","STA355 (L)","","","",""],["4:00","","","","",""],["5:00","","","","",""],["6:00","","","","",""],["7:00","","","","",""],["8:00","","","","",""]]
 getActiveTimetable :: Request -> String -> IO (String, String)
-getActiveTimetable req session = do
+getActiveTimetable req termSession = do
     -- get cookie value of "selected-lectures" from browser
     let cookies :: M.Map String Cookie = M.fromList $ rqCookies req  --  Map String Cookie
         coursecookie = maybe "" cookieValue $ M.lookup "selected-lectures" cookies
-        (lecture, tutorial) = parseCourseCookie coursecookie session  -- ([CourseInfo], [CourseInfo])
+        (lecture, tutorial) = parseCourseCookie coursecookie termSession  -- ([CourseInfo], [CourseInfo])
     (lecTimes, tutTimes) <- getTimes (lecture, tutorial)
     let schedule' = getScheduleByTime lecTimes tutTimes
-    (svgFilename, imageFilename) <- generateTimetableImg schedule' session
+    (svgFilename, imageFilename) <- generateTimetableImg schedule' termSession
     return (svgFilename, imageFilename)
-
 
 -- | Parse cookie string to two lists of courses, one for lecture, the other for tutorial
 -- "CSC148H1-L5101-S_CSC148H1-T0501-S_STA355H1-L0101-F_CSC108H1-L0102-F"
 -- [("STA355H1","LEC-0101","F"),("CSC108H1","LEC-0102","F")], [])
 parseCourseCookie :: String -> String -> ([CourseInfo], [CourseInfo])
-parseCourseCookie s session = 
+parseCourseCookie s termSession = 
   let lecAndTut = map (splitOn "-") $ splitOn "_" s
       (lecture, tutorial) = partition isLec lecAndTut
       -- get lecture and tutorial in this session
-      [lectureOfsession, tutorialOfsession] = map (filter (\x -> or ([(x !! 2 !! 0) == (head session), (x !! 2 !! 0) == 'Y']))) [lecture, tutorial]
+      [lectureOfsession, tutorialOfsession] = map (filter (\x -> or ([(x !! 2 !! 0) == (head termSession), (x !! 2 !! 0) == 'Y']))) [lecture, tutorial]
       lecture' = map (convertLec . list2tuple) lectureOfsession
       tutorial' = map (convertTut . list2tuple) tutorialOfsession
   in (lecture', tutorial')
@@ -68,19 +67,19 @@ list2tuple _ = undefined
 
 -- ("CSC148H1","L5101","S") -> CourseInfo {code = "CSC148H1", section = "LEC-0501", session = "S", time = []} convert format to match database
 convertLec :: (String, String, String) -> CourseInfo
-convertLec (code, section, session) = CourseInfo {code = code, section = take 1 section ++ "EC-" ++ drop 1 section, session = session, time = [] }
+convertLec (lecCode, lecSection, lecSession) = CourseInfo {code = lecCode, section = take 1 lecSection ++ "EC-" ++ drop 1 lecSection, session = lecSession, time = [] }
 
 -- ("CSC148H1","T0501","S") -> CourseInfo {code = "CSC148H1", section = "TUT-0501", session = "S", time = []} convert format to match database
 convertTut :: (String, String, String) -> CourseInfo
-convertTut (code, section, session) = CourseInfo {code = code, section = take 1 section ++ "UT-" ++ drop 1 section, session = session, time = [] }
+convertTut (tutCode, tutSection, tutSession) = CourseInfo {code = tutCode, section = take 1 tutSection ++ "UT-" ++ drop 1 tutSection, session = tutSession, time = [] }
 
 
 -- | Get data from database for selected courses
 getTimes :: ([CourseInfo], [CourseInfo]) -> IO ([CourseInfo], [CourseInfo])
-getTimes (lectures, tutorials) = runSqlite databasePath $ do
+getTimes (selectedLectures, selectedTutorials) = runSqlite databasePath $ do
     runMigration migrateAll
-    lecTimes <- mapM getLectureTime lectures  -- [(String, String, String)] -> IO ([[Time]])
-    tutTimes <- mapM getTutorialTime tutorials
+    lecTimes <- mapM getLectureTime selectedLectures  -- -> IO ([[Time]])
+    tutTimes <- mapM getTutorialTime selectedTutorials
     return (lecTimes, tutTimes)
 
 
@@ -109,19 +108,19 @@ convertTimeToArray = map (\x -> (floor $ timeField x !! 0 , floor $ timeField x 
 
 --  "STA355H1" "L0101" "F"  Time {timeField = [0.0,14.0]}
 addCourseHelper :: String -> String -> String -> [[String]] -> (Int, Int) -> [[String]]
-addCourseHelper code section session currentSchedule (day, time) = 
-  let time_schedule = currentSchedule !! time
-      current_schedule = if (null $ time_schedule !! day) then (code++session++" "++section) else (time_schedule !! day ++ ("&"++code++session++" "++section))
+addCourseHelper courseCode courseSection courseSession currentSchedule (day, courseTime) = 
+  let time_schedule = currentSchedule !! courseTime
+      current_schedule = if (null $ time_schedule !! day) then (courseCode++courseSession++" "++courseSection) else (time_schedule !! day ++ ("&"++courseCode++courseSession++" "++courseSection))
       time_schedule' = (take day time_schedule) ++ [current_schedule] ++ (drop (day + 1) time_schedule)
-      newSchedule = (take time currentSchedule) ++ [time_schedule'] ++ (drop (time + 1) currentSchedule)
+      newSchedule = (take courseTime currentSchedule) ++ [time_schedule'] ++ (drop (courseTime + 1) currentSchedule)
   in newSchedule
 
 generateTimetableImg :: [[String]] -> String -> IO(String, String)
-generateTimetableImg schedule session = do
+generateTimetableImg schedule courseSession = do
     rand <- randomName
     let svgFilename = rand ++ ".svg"
         imageFilename = rand ++ ".png"
-    renderTableHelper svgFilename (zipWith (:) times schedule) session
+    renderTableHelper svgFilename (zipWith (:) times schedule) courseSession
     createImageFile svgFilename imageFilename
     return (svgFilename, imageFilename)
 
@@ -141,12 +140,12 @@ getGraphImage graphName courseMap = do
 -- | Creates an image, and returns the name of the svg used to create the
 -- image and the name of the image
 getTimetableImage :: String -> String -> IO (String, String)
-getTimetableImage courses session = do
+getTimetableImage courses termSession = do
     -- generate 2 random names
     rand <- randomName
     let svgFilename = rand ++ ".svg"
         imageFilename = rand ++ ".png"
-    renderTable svgFilename courses session
+    renderTable svgFilename courses termSession
     createImageFile svgFilename imageFilename
     return (svgFilename, imageFilename)
 
