343a13161750c11910a88d2388d768aa6484e6ea
Ailsa (Tiantian) Fang <tfang@sysomos.com>
2016-11-13 23:14:52 -0500

    empty space

--- app/Export/PdfGenerator.hs
+++ app/Export/PdfGenerator.hs
@@ -10,7 +10,7 @@ import Data.List.Utils (replace)
 -- the tex file and extra files created by pdflatex
 createPDF :: String -> IO ()
 createPDF texName  = do
-  (_, _, _, pid) <- convertTexToPDF texName 
+  (_, _, _, pid) <- convertTexToPDF texName
   print "Waiting for a process..."
   _ <- waitForProcess pid
   let auxFile = replace ".tex" ".aux" texName
@@ -18,8 +18,8 @@ createPDF texName  = do
   _ <- removeFile (auxFile ++ " " ++ logFile ++ " " ++ texName)
   print "Process Complete"
 
--- | Create a process to use the pdflatex program to create a PDF from a TEX 
--- file (texName). The process is run in nonstop mode and so it will not block 
+-- | Create a process to use the pdflatex program to create a PDF from a TEX
+-- file (texName). The process is run in nonstop mode and so it will not block
 -- if an error occurs. The resulting PDF will have the same filename as texName.
 convertTexToPDF :: String -> IO
                             (Maybe Handle,
@@ -27,7 +27,7 @@ convertTexToPDF :: String -> IO
                              Maybe Handle,
                              ProcessHandle)
 convertTexToPDF texName = createProcess $ CreateProcess
-                      (ShellCommand $ "pdflatex -interaction=nonstopmode " ++ texName) 
+                      (ShellCommand $ "pdflatex -interaction=nonstopmode " ++ texName)
                       Nothing
                       Nothing
                       CreatePipe
