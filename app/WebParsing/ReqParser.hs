3fe427dfe25709832409b8d542bce18819385ccc
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-15 04:29:23 -0500

    Add all approaches. untitled.hs success~\!

--- app/WebParsing/ReqParser.hs
+++ app/WebParsing/ReqParser.hs
@@ -4,8 +4,32 @@ import qualified Text.Parsec as Parsec
 import Text.Parsec ((<?>))
 import Control.Applicative
 import Control.Monad.Identity (Identity)
+import qualified Data.String as S
 parse rule text = Parsec.parse rule "(source)" text
 
+data ProgramReq = PRGREQ String [Req] deriving (Show)
+
+-- | Returns a well formatted String representing a program requirement for specified program.
+-- instance Show ProgramReq where
+--     show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ map show reqs
+
+
+data CourseReq = CRSREQ String Req Req Req deriving (Show)
+
+-- instance Show CourseReq where
+--     show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+--                                       ++ "Exclusions for " ++ course ++ ":\n" ++ show req ++ "\n"
+--                                       ++ "Prerequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+
+
+-- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
+data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req] deriving (Show)
+
+-- instance Show Req where
+--     show (J course) = course
+--     show (AND reqs) = S.intercalate "," $ map show reqs
+--     show (OR reqs) = S.intercalate "/" $ map show reqs
+--     show (FROM a x) =  show a ++ "FCE(s) from: (" ++ show x ++ ")"
 
 -- define separators for "/"  ","  "("  ")"  "from"
 orSeparator :: Parsec.Parsec String () ()
@@ -26,59 +50,99 @@ fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
 
 -- csc263h1, mat157Y1, STA257H1
 -- Parse a single course with spaces before and or after
-courseParser :: Parsec.Parsec String () String
+courseParser :: Parsec.Parsec String () Req
 -- alphanumeric? or is it already just strings, 8 characters.
 -- 3 letters (not case sensitive), 3 digits, 1 letter, 1 digit
 -- take any spaces before and after?
-courseParser = do
+courseParser = (do
     Parsec.spaces
     -- with no spaces, we expect 3 letters, 3 digits, and (h/H/y/Y)1
     code <- Parsec.count 3 Parsec.letter
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return (code++num++sess)
+    return $ J (code++num++sess)) <|> (parParser)
 
 -- more lenient course parser
-courseParser2 :: Parsec.Parsec String () String
--- alphanumeric, 8 characters alphanumeric?
-courseParser2 = do
-    Parsec.spaces
-    course <- Parsec.count 8 Parsec.alphaNum
-    Parsec.spaces
-    return (course)
+-- courseParser2 :: Parsec.Parsec String () String
+-- -- alphanumeric, 8 characters alphanumeric?
+-- courseParser2 = do
+--     Parsec.spaces
+--     course <- Parsec.count 8 Parsec.alphaNum
+--     Parsec.spaces
+--     return (course)
+
+-- courseParser3 :: String -> [String]
+-- courseParser3 course = 
+--     let parsedCourse = parse andParser2 course
+--     in
+--         case parsedCourse of
+--             Right xs -> xs
+--             Left _ -> [""]
 
 -- csc263/csc265, MAT235/Mat237/Mat257
 -- PRECONDITION: '/' infix binary OP => will always have "courses" on both sides
 --                => we will always have "course" followed by (/ "course")s
 -- POSTCONDITION: returns list of "courses" that were s     eparated by '/'
-orParser1 :: Parsec.Parsec String () [String]
+--orParser1 :: Parsec.Parsec String () [String]
 -- look for whitespaces, /, course, whitespaces. Repeat.
 -- more efficiently, separate by optional whitespaces with '/'
-orParser1 = Parsec.many $ do
-    course <- courseParser
-    Parsec.eof <|> orSeparator
-    return course
+--orParser1 = Parsec.many $ do
+  --  course <- courseParser
+    --Parsec.eof <|> orSeparator
+   -- return  course
 
 -- lenient orParser, will simply parse by / and return list of "reqs"
-orParser2 :: Parsec.Parsec String () [String]
+orParser2 :: Parsec.Parsec String () Req
 -- look for whitespaces, /, course, whitespaces. Repeat.
 -- more efficiently, separate by optional whitespaces with '/'
-orParser2 = Parsec.sepBy (Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf ", ()")) (Parsec.char '/')
+orParser2 = do
+    --tmp <- Parsec.sepBy (Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf ", ()")) (Parsec.char '/')
+    tmp <- Parsec.sepBy (courseParser) (Parsec.char '/')
+    return $ OR tmp
+
+-- orParser3 :: String -> [String]
+-- orParser3 req = 
+--     let orParsed = parse orParser2 req
+--     in
+--         case orParsed of
+--             Right xs -> xs
+--             Left _ -> [""]
+
 
 -- mat240,csc263/csc265, MAT235/Mat237/Mat257
 -- PRECONDITION: ',' infix binary OP => will always have "courses" on both sides
 --                => we will always have "course" followed by (, "course")s
 -- POSTCONDITION: returns list of "courses" that were separated by ','
-andParser1 :: Parsec.Parsec String () [String]
-andParser1 = Parsec.many $ do
-    course <- courseParser
-    Parsec.eof <|> andSeparator
-    return course
+-- andParser1 :: Parsec.Parsec String () [String]
+-- andParser1 = Parsec.many $ do
+--     req <- courseParser
+--     Parsec.eof <|> andSeparator
+--     return req
 
 -- lenient orParser, will simply parse by , and return list of "reqs"
-andParser2 :: Parsec.Parsec String () [String]
-andParser2 = Parsec.sepBy (Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf "/ ()")) (Parsec.char ',')
+andorParser2 :: Parsec.Parsec String () Req
+andorParser2 = do
+    --tmp <- Parsec.sepBy (Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf "/ ()")) (Parsec.char ',')
+    tmp <- Parsec.sepBy (orParser2) (Parsec.char ',')
+    return $ AND tmp
+
+parParser :: Parsec.Parsec String () Req
+parParser = do
+    lpSeparator
+    req <- andorParser2
+    rpSeparator
+    return req
+
+-- uses andParser2
+-- andParser3 :: String -> [String]
+-- andParser3 req = 
+--     let andParsed = parse andParser2 req
+--     in
+--         case andParsed of
+--             Right xs -> xs
+--             Left _ -> [""]
+
 
 -- parse by parantheses, treat everything inside paranthesis as one "req"
 --parParser1 :: Parsec.Parsec String () [String]
@@ -86,30 +150,41 @@ andParser2 = Parsec.sepBy (Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf "/ ()"
 -- csc263/csc265, MAT235/Mat237/Mat257
 -- PRECONDITION: '/' ',' infix binary OPs => will always have "courses" on both sides
 -- POSTCONDITION: returns list of list of "courses". Depth 0 by ','' . Depth 1 by '/'.
-andorParser :: Parsec.Parsec String () [Either Parsec.ParseError [String]]
-andorParser = do
-    andParsed <- andParser2
-    let tmp = map (parse orParser2) andParsed
-    return tmp
-
--- WHY NOT TRY CONTROLLING THE ERROR/ RETURN MSG TO CHECK FOR TYPES
--- LOOK INTO EITHER TYPE.. SHOULD BE ABLE TO HAVE
---              A NESTED PARSER
-
--- IDEA: within parParser, store all chunks of Reqs while parsing Req within parantheses until EOF then call
---       andorParser on all paranthesized Reqs. then call andorParser on 
-
-
-
+-- andorParser :: Parsec.Parsec String () [Either Parsec.ParseError [String]]
+-- andorParser = do
+--     andParsed <- andParser2
+--     let andorParsed = map (parse orParser2) andParsed
+--     return andorParsed
 
 -- adding functionality to andorParser to let it parse valid courses as well.
 -- may be unnecessary since we can assume courses have no typo
 -- might be more realistic to NOT expect perfectly formatted course codes due to inconsistencies in html.
---andorParser2 :: Parsec.Parsec String () [Either Parsec.ParseError [String]]
---andorParser2 = do
---    andParsed <- andParser2
---    let tmp = map (parse orParser2) andParsed
---    return map (map (parse courseParser2)) tmp
+-- andorParser2 :: String -> [[String]]
+-- andorParser2 req = map (orParser3) $ andParser3 req
+
+--reqParser :: String -> [[String]]
+
+--lpParser1 :: Parsec.Parsec String () String
+--lpParser1 = do
+--    req <- Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf "/, )")
+--    Parsec.eof <|> Parsec.char '('
+--    return req
+
+--rpParser1 :: Parsec.Parsec String () String
+--rpParser1 = do
+---    req <- Parsec.many1 (Parsec.alphaNum <|> Parsec.oneOf "/, (")
+--    Parsec.eof <|> Parsec.char ')'
+--    return req
+
+
+-- parse everything by parantheses
+-- left or right may be empty, meaning it's not in the middle of a req
+--parParser1 :: Parsec.Parsec String () [[String]]
+--parParser1 = Parsec.many $ do
+--    left <- lpParser1
+--    right <- rpParser1
+--    return [left, right]
+
 
 -- recursion comes in with ()
 -- (mat136, mat136)
---
+++ app/WebParsing/ReqParser_revisedagain.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import qualified Text.Parsec as Parsec
+import Text.Parsec ((<?>))
+import Control.Applicative
+import Control.Monad.Identity (Identity)
+import qualified Data.String as S
+parse rule text = Parsec.parse rule "(source)" text
+
+data ProgramReq = PRGREQ String [Req] deriving (Show)
+
+-- | Returns a well formatted String representing a program requirement for specified program.
+-- instance Show ProgramReq where
+--     show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ map show reqs
+
+
+data CourseReq = CRSREQ String Req Req Req deriving (Show)
+
+-- instance Show CourseReq where
+--     show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+--                                       ++ "Exclusions for " ++ course ++ ":\n" ++ show req ++ "\n"
+--                                       ++ "Prerequisites for " ++ course ++ ":\n" ++ show req ++ "\n"
+
+
+-- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
+data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req] deriving (Show)
+
+-- instance Show Req where
+--     show (J course) = course
+--     show (AND reqs) = S.intercalate "," $ map show reqs
+--     show (OR reqs) = S.intercalate "/" $ map show reqs
+--     show (FROM a x) =  show a ++ "FCE(s) from: (" ++ show x ++ ")"
+
+-- define separators for "/"  ","  "("  ")"  "from"
+orSeparator :: Parsec.Parsec String () ()
+orSeparator = Parsec.spaces >> Parsec.char '/' >> Parsec.spaces
+
+andSeparator :: Parsec.Parsec String () ()
+andSeparator = Parsec.spaces >> Parsec.char ',' >> Parsec.spaces
+
+lpSeparator :: Parsec.Parsec String () ()
+lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
+
+rpSeparator :: Parsec.Parsec String () ()
+rpSeparator = Parsec.spaces >> Parsec.char ')' >> Parsec.spaces
+
+fromSeparator :: Parsec.Parsec String () ()
+fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
+-- potentially have one separator that returns accordingly?
+
+-- parse for single course OR req within parantheses
+courseParser :: Parsec.Parsec String () Req
+-- alphanumeric? or is it already just strings, 8 characters.
+-- 3 letters (not case sensitive), 3 digits, 1 letter, 1 digit
+-- take any spaces before and after?
+courseParser = (do
+    Parsec.spaces
+    -- with no spaces, we expect 3 letters, 3 digits, and (h/H/y/Y)1
+    code <- Parsec.count 3 Parsec.letter
+    num <- Parsec.count 3 Parsec.digit
+    sess <- Parsec.count 2 Parsec.alphaNum
+    Parsec.spaces
+    return $ J (code++num++sess)) <|> (parParser)
+
+courseParser1 :: String -> Req
+courseParser1 req =
+	let tmp = parse courseParser req
+	in case tmp of
+	Right parsed -> parsed
+	Left _ -> J "fucked up fam"
+
+-- parse for reqs separated by / "or"
+orParser :: Parsec.Parsec String () Req
+orParser = do
+    tmp <- Parsec.sepBy (courseParser) (Parsec.char '/')
+    return $ OR tmp
+
+orParser1 :: String -> Req
+orParser1 req =
+    let tmp = parse orParser req
+    in case tmp of
+    Right parsed ->
+	    case parsed of
+		OR [J x] -> J x
+		OR [OR x] -> OR x
+		OR [AND x] -> AND x
+		OR xs -> OR xs
+    Left _ -> J "fucked up fam"
+
+-- parse for reqs separated by , "and"
+andorParser :: Parsec.Parsec String () Req
+andorParser = do
+    tmp <- Parsec.sepBy (orParser) (Parsec.char ',')
+    return $ AND tmp
+
+andorParser1 :: String -> Req
+andorParser1 req =
+    let tmp = parse andorParser req
+    in case tmp of
+    Right parsed ->
+        case parsed of
+        AND [J x] -> J x
+        AND [OR x] -> OR x
+        AND [AND x] -> AND x
+        AND xs -> AND xs
+    Left _ -> J "fucked up fam"
+
+parParser :: Parsec.Parsec String () Req
+parParser = do
+    lpSeparator
+    req <- andorParser
+    rpSeparator
+    return req
+
+parParser1 :: Parsec.Parsec String () String
+parParser1 = do
+    lpSeparator
+    req <- Parsec.many (Parsec.noneOf [')'])
+    return req
+
+parParser2 :: String -> Req
+parParser2 req =
+    let tmp = parse parParser1 req
+    in case tmp of
+    Right parsed -> andorParser1 parsed
+    Left _ -> J "fucked up fam"
+
+-- case check? if highest level is of form AND[OR xs] or OR[AND xs], return OR xs or AND xs, respectively
+-- do this recursively so that we get rid of the bottom level ORs and ANDs??
+
+
+-- TODO: error msg
+---- display
+---- recursive structure
+---- unformatted course
+---- FROM value constructor
