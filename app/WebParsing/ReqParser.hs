1faa7a33ddfe3159a72beefa76d6f18665d1875d
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-16 03:12:21 -0500

    Update old file, resolve bugs for ReqParser.

--- app/WebParsing/ReqParser.hs
+++ app/WebParsing/ReqParser.hs
@@ -1,39 +1,38 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 import qualified Text.Parsec as Parsec
-import Text.Parsec ((<?>))
-import Control.Applicative
+import Text.Parsec ((<|>))
 import Control.Monad.Identity (Identity)
 import qualified Data.String as S
+import qualified Data.List as L
 import Database.Requirement
 parse rule text = Parsec.parse rule "(source)" text
 
+-- define separators for "from"
+fromSeparator :: Parsec.Parsec String () ()
+fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
+
 lpSeparator :: Parsec.Parsec String () ()
-lpSeparator = do
-    Parsec.spaces
-    Parsec.char '('
-    Parsec.spaces
+lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
 
 rpSeparator :: Parsec.Parsec String () ()
-rpSeparator = do
-    Parsec.spaces
-    Parsec.char ')'
-    Parsec.spaces
+rpSeparator = Parsec.spaces >> Parsec.char ')' >> Parsec.spaces
 
+-- helper to find length of list of reqs
 length_list :: [Req] -> Int 
 length_list [] = 0
 length_list (x:xs) =1 + length_list xs
 
 -- parse for single course OR req within parantheses
 courseParser :: Parsec.Parsec String () Req
-courseParser = (do
+courseParser = Parsec.try (do
     Parsec.spaces
     -- with no spaces, we expect 3 letters, 3 digits, and (h/H/y/Y)1
     code <- Parsec.count 3 Parsec.letter
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return $ J (code++num++sess)) <|> (parParser)
+    return $ J (code++num++sess)) <|> (do parParser)
 
 -- parse for reqs separated by / "or"
 orParser :: Parsec.Parsec String () Req
@@ -60,10 +59,10 @@ andorParser = do
 -- parse for reqs within parantheses
 parParser :: Parsec.Parsec String () Req
 parParser = do
-    lpSeparator
-    req <- andorParser
-    rpSeparator
-    return req
+  lpSeparator
+  req <- andorParser
+  rpSeparator
+  return req
 
 -- TODO: error msg
 ---- display
--- app/WebParsing/ReqParserTEST.hs
+++ app/WebParsing/ReqParserTEST.hs
@@ -1,37 +1,38 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 import qualified Text.Parsec as Parsec
-import Text.Parsec ((<?>))
-import Control.Applicative
+import Text.Parsec ((<|>))
 import Control.Monad.Identity (Identity)
-import qualified Data.String as S
 import qualified Data.String as S
 import qualified Data.List as L
 parse rule text = Parsec.parse rule "(source)" text
 
 data ProgramReq = PRGREQ String [Req]
+
+-- | Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
     show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ L.intercalate "\n" (map show reqs)
 
 data CourseReq = CRSREQ String Req Req Req
+
 instance Show CourseReq where
     show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show creq ++ "\n"
                                       ++ "Exclusions for " ++ course ++ ":\n" ++ show excl ++ "\n"
                                       ++ "Prerequisites for " ++ course ++ ":\n" ++ show preq ++ "\n"
 
+-- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
 data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req]
+
 instance Show Req where
     show (J course) = course
     show (AND reqs) = L.intercalate "," $ map show reqs
     show (OR reqs) = L.intercalate "/" $ map show reqs
     show (FROM a reqs) =  show a ++ "FCE(s) from: (" ++ show reqs ++ ")"
 
--- define separators for "/"  ","  "("  ")"  "from"
-orSeparator :: Parsec.Parsec String () ()
-orSeparator = Parsec.spaces >> Parsec.char '/' >> Parsec.spaces
 
-andSeparator :: Parsec.Parsec String () ()
-andSeparator = Parsec.spaces >> Parsec.char ',' >> Parsec.spaces
+-- define separators for "from"
+fromSeparator :: Parsec.Parsec String () ()
+fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
 
 lpSeparator :: Parsec.Parsec String () ()
 lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
@@ -39,24 +40,21 @@ lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
 rpSeparator :: Parsec.Parsec String () ()
 rpSeparator = Parsec.spaces >> Parsec.char ')' >> Parsec.spaces
 
-fromSeparator :: Parsec.Parsec String () ()
-fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
--- potentially have one separator that returns accordingly?
-
+-- helper to find length of list of reqs
 length_list :: [Req] -> Int 
 length_list [] = 0
 length_list (x:xs) =1 + length_list xs
 
 -- parse for single course OR req within parantheses
 courseParser :: Parsec.Parsec String () Req
-courseParser = (do
+courseParser = Parsec.try (do
     Parsec.spaces
     -- with no spaces, we expect 3 letters, 3 digits, and (h/H/y/Y)1
     code <- Parsec.count 3 Parsec.letter
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return $ J (code++num++sess)) <|> (parParser)
+    return $ J (code++num++sess)) <|> (do parParser)
 
 -- parse for reqs separated by / "or"
 orParser :: Parsec.Parsec String () Req
@@ -83,13 +81,9 @@ andorParser = do
 -- parse for reqs within parantheses
 parParser :: Parsec.Parsec String () Req
 parParser = do
-  Parsec.spaces
-  Parsec.char '('
-  Parsec.spaces
+  lpSeparator
   req <- andorParser
-  Parsec.spaces
-  Parsec.char ')'
-  Parsec.spaces
+  rpSeparator
   return req
 
 -- TODO: error msg
