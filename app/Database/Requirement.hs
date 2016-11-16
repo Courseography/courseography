f6764c03c8189aefc5d83afde307e29d2bebe113
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-16 02:15:06 -0500

    Update Requirement.hs

--- app/Database/Requirement.hs
+++ app/Database/Requirement.hs
@@ -5,7 +5,7 @@ Module containing data type that represents a "Requirement".
 
 We will use parsed data to create instances of this type.
 -}
-module Requirement
+module Database.Requirement
 ( ProgramReq(..)
 , CourseReq(..)
 , Req(..)
@@ -14,12 +14,11 @@ module Requirement
 import qualified Data.String as S
 import qualified Data.List as L
 
-data ProgramReq = PRGREQ String Req
+data ProgramReq = PRGREQ String [Req]
 
 -- | Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
-    show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ show reqs
-
+    show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ L.intercalate "\n" (map show reqs)
 
 data CourseReq = CRSREQ String Req Req Req
 
@@ -27,7 +26,6 @@ instance Show CourseReq where
     show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show creq ++ "\n"
                                       ++ "Exclusions for " ++ course ++ ":\n" ++ show excl ++ "\n"
                                       ++ "Prerequisites for " ++ course ++ ":\n" ++ show preq ++ "\n"
-
 
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
 data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req]
