d5bd5c64248b6b33fedd8bbe586280863c2c23f3
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-17 23:47:07 -0500

    Implement and test fromParser. Need to implement int and float parsers.

--- app/WebParsing/ReqParserTEST.hs
+++ app/WebParsing/ReqParserTEST.hs
@@ -9,31 +9,35 @@ parse rule text = Parsec.parse rule "(source)" text
 
 data ProgramReq = PRGREQ String [Req]
 
--- | Returns a well formatted String representing a program requirement for specified program.
+-- Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
-    show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ L.intercalate "\n" (map show reqs)
+    show (PRGREQ program reqs) = "Program Requirements for " ++ program
+                               ++ ":\n" ++ L.intercalate "\n" (map show reqs)
 
 data CourseReq = CRSREQ String Req Req Req
 
 instance Show CourseReq where
-    show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show creq ++ "\n"
-                                      ++ "Exclusions for " ++ course ++ ":\n" ++ show excl ++ "\n"
-                                      ++ "Prerequisites for " ++ course ++ ":\n" ++ show preq ++ "\n"
+    show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n"
+                                        ++ show creq ++ "\n" ++ "Exclusions for "
+                                        ++ course ++ ":\n" ++ show excl ++ "\n"
+                                        ++ "Prerequisites for " ++ course ++ ":\n"
+                                        ++ show preq ++ "\n"
 
 -- define separators for "from"
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
-data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req] | PAR Req
+data Req = J String | AND [Req] | OR [Req] | FROM String Req | PAR Req
 
 instance Show Req where
     show (J course) = course
     show (AND reqs) = L.intercalate "," $ map show reqs
     show (OR reqs) = L.intercalate "/" $ map show reqs
-    show (FROM a reqs) =  show a ++ "FCE(s) from: (" ++ show reqs ++ ")"
+    show (FROM fces reqs) =  fces ++ " FCE(s) from:\n" ++ show reqs
     show (PAR reqs) =  "(" ++ show reqs ++ ")"
 
 -- define separators for "from"
 fromSeparator :: Parsec.Parsec String () ()
-fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
+fromSeparator = Parsec.spaces >> Parsec.try (Parsec.string "FROM" 
+             <|> Parsec.string "from" <|> Parsec.string "From") >> Parsec.spaces
 
 lpSeparator :: Parsec.Parsec String () ()
 lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
@@ -55,7 +59,7 @@ courseParser = Parsec.try (do
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return $ J (code++num++sess)) <|> (do parParser)
+    return $ J (code++num++sess)) <|> (parParser) <|> (fromParser)
 
 -- parse for reqs separated by / "or"
 orParser :: Parsec.Parsec String () Req
@@ -82,13 +86,51 @@ andorParser = do
 -- parse for reqs within parantheses
 parParser :: Parsec.Parsec String () Req
 parParser = do
-  lpSeparator
-  req <- andorParser
-  rpSeparator
-  return $ PAR req
+    lpSeparator
+    req <- andorParser
+    rpSeparator
+    return $ PAR req
+
+-- parse for reqs in "from" format
+fromParser :: Parsec.Parsec String () Req
+fromParser = do 
+    fces <- intParserLenient <|> floatParserLenient
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try fromSeparator))
+    reqs <- andorParser
+    return $ FROM fces reqs
+
+-- "5.0 fces from..." "3 full course from" "2.5 full course from"
+-- Hard-coded for now since all FCEs are of form _._ with anything before
+floatParserLenient :: Parsec.Parsec String () String
+floatParserLenient = do
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try 
+               (Parsec.notFollowedBy (Parsec.noneOf ['0'..'9']))))
+    fces <- Parsec.count 3 Parsec.anyChar
+    return fces
+
+-- floatParser :: Parsec.Parsec String () String
+-- floatParser = do
+--     Parsec.spaces
+--     int <- Parsec.digit
+--     Parsec.char '.'
+--     float <- Parsec.digit
+--     return [int]++[float]
+
+intParserLenient :: Parsec.Parsec String () String
+intParserLenient = do
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try 
+               (Parsec.notFollowedBy (Parsec.noneOf ['0'..'9']))))
+    fces <- Parsec.count 1 Parsec.anyChar
+    return fces
+
+-- intParser :: Parsec.Parsec String () String
+-- intParser = do
+--     Parsec.spaces
+--     fces <- Parsec.digit
+--     return [fces]
+
 
 -- TODO: error msg
----- display
----- recursive structure
----- unformatted course
 ---- FROM value constructor
+---- NEED TO MAKE IMPORTS AND EXPORTS CONSISTENT
+----- MUST TEST WITH REPL
