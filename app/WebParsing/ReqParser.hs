feb5ba753864167ba2c915a3c07460db8be63779
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-17 22:00:37 -0500

    Implement From parser. Consider changing parameter for From value constructor for consistency.

--- app/WebParsing/ReqParser.hs
+++ app/WebParsing/ReqParser.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+module WebParsing.ReqParser
+    (andorParser) where
 
 import qualified Text.Parsec as Parsec
+import Text.Parsec.String
 import Text.Parsec ((<|>))
 import Control.Monad.Identity (Identity)
 import qualified Data.String as S
@@ -10,7 +13,7 @@ parse rule text = Parsec.parse rule "(source)" text
 
 -- define separators for "from"
 fromSeparator :: Parsec.Parsec String () ()
-fromSeparator = Parsec.spaces >> Parsec.oneOf "fromFrom" >> Parsec.spaces
+fromSeparator = Parsec.spaces >> (Parsec.string "FROM" <|> Parsec.string "from" <|> Parsec.string "From") >> Parsec.spaces
 
 lpSeparator :: Parsec.Parsec String () ()
 lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
@@ -32,7 +35,7 @@ courseParser = Parsec.try (do
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return $ J (code++num++sess)) <|> (do parParser)
+    return $ J (code++num++sess)) <|> (parParser) <|> (fromParser)
 
 -- parse for reqs separated by / "or"
 orParser :: Parsec.Parsec String () Req
@@ -62,10 +65,36 @@ parParser = do
   lpSeparator
   req <- andorParser
   rpSeparator
-  return req
+  return $ PAR req
+
+-- "5.0 fces from..." "3 full course from" "2.5 full course from"
+-- find keywords (have a parser for int/float from)
+-- make another parser that calls from parser then calls andorParser
+-- until EOF or newline???
+-- int and float parser
+floatParser :: Parsec.Parsec String () String
+floatParser = do
+  Parsec.spaces
+  fce <- manyTill Parsec.digit (Parsec.char '.')
+  fceDec <- Parsec.oneOf("05")
+  return fce++fceDec
+
+intParser :: Parsec.Parsec String () String
+intParser = do
+  Parsec.spaces
+  fce <- Parsec.many1 Parsec.digit
+  return fce
+-- hard-coded fce parser? int or float of form "_.0" or "_.5"
+-- parse for reqs in "from" format
+fromParser :: Parsec.Parsec String () Req
+fromParser = do 
+  fces <- (floatParser) <|> (intParser)
+  Parsec.manyTill Parsec.anyChar fromSeparator
+  fromSeparator
+  reqs <- andorParser
+  return $ FROM fces req
 
 -- TODO: error msg
----- display
----- recursive structure
----- unformatted course
 ---- FROM value constructor
+---- NEED TO MAKE IMPORTS AND EXPORTS CONSISTENT
+----- MUST TEST WITH REPL
