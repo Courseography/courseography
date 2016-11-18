6e2e8a2bba5af7fff927f49cc33267fc9dfbc7e4
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-18 02:36:19 -0500

    Fix import statement in ReqParser.

--- app/WebParsing/ReqParser.hs
+++ app/WebParsing/ReqParser.hs
@@ -13,7 +13,8 @@ parse rule text = Parsec.parse rule "(source)" text
 
 -- define separators for "from"
 fromSeparator :: Parsec.Parsec String () ()
-fromSeparator = Parsec.spaces >> (Parsec.string "FROM" <|> Parsec.string "from" <|> Parsec.string "From") >> Parsec.spaces
+fromSeparator = Parsec.spaces >> Parsec.try (Parsec.string "FROM" 
+             <|> Parsec.string "from" <|> Parsec.string "From") >> Parsec.spaces
 
 lpSeparator :: Parsec.Parsec String () ()
 lpSeparator = Parsec.spaces >> Parsec.char '(' >> Parsec.spaces
@@ -26,6 +27,36 @@ length_list :: [Req] -> Int
 length_list [] = 0
 length_list (x:xs) =1 + length_list xs
 
+-- "5.0 fces from..." "3 full course from" "2.5 full course from"
+-- Hard-coded for now since all FCEs are of form _._ with anything before
+floatParserLenient :: Parsec.Parsec String () String
+floatParserLenient = do
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try 
+               (Parsec.notFollowedBy (Parsec.noneOf ['0'..'9']))))
+    fces <- floatParser
+    return fces
+
+floatParser :: Parsec.Parsec String () String
+floatParser = do
+    Parsec.spaces
+    int <- Parsec.digit
+    Parsec.char '.'
+    float <- Parsec.digit
+    return [int,float]
+
+intParserLenient :: Parsec.Parsec String () String
+intParserLenient = do
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try 
+               (Parsec.notFollowedBy (Parsec.noneOf ['0'..'9']))))
+    fces <- intParser
+    return fces
+
+intParser :: Parsec.Parsec String () String
+intParser = do
+    Parsec.spaces
+    fces <- Parsec.digit
+    return [fces]
+
 -- parse for single course OR req within parantheses
 courseParser :: Parsec.Parsec String () Req
 courseParser = Parsec.try (do
@@ -35,7 +66,7 @@ courseParser = Parsec.try (do
     num <- Parsec.count 3 Parsec.digit
     sess <- Parsec.count 2 Parsec.alphaNum
     Parsec.spaces
-    return $ J (code++num++sess)) <|> (parParser) <|> (fromParser)
+    return $ J (code++num++sess)) <|> (parParser)
 
 -- parse for reqs separated by / "or"
 orParser :: Parsec.Parsec String () Req
@@ -62,39 +93,15 @@ andorParser = do
 -- parse for reqs within parantheses
 parParser :: Parsec.Parsec String () Req
 parParser = do
-  lpSeparator
-  req <- andorParser
-  rpSeparator
-  return $ PAR req
+    lpSeparator
+    req <- andorParser
+    rpSeparator
+    return $ PAR req
 
--- "5.0 fces from..." "3 full course from" "2.5 full course from"
--- find keywords (have a parser for int/float from)
--- make another parser that calls from parser then calls andorParser
--- until EOF or newline???
--- int and float parser
-floatParser :: Parsec.Parsec String () String
-floatParser = do
-  Parsec.spaces
-  fce <- manyTill Parsec.digit (Parsec.char '.')
-  fceDec <- Parsec.oneOf("05")
-  return fce++fceDec
-
-intParser :: Parsec.Parsec String () String
-intParser = do
-  Parsec.spaces
-  fce <- Parsec.many1 Parsec.digit
-  return fce
--- hard-coded fce parser? int or float of form "_.0" or "_.5"
 -- parse for reqs in "from" format
 fromParser :: Parsec.Parsec String () Req
 fromParser = do 
-  fces <- (floatParser) <|> (intParser)
-  Parsec.manyTill Parsec.anyChar fromSeparator
-  fromSeparator
-  reqs <- andorParser
-  return $ FROM fces req
-
--- TODO: error msg
----- FROM value constructor
----- NEED TO MAKE IMPORTS AND EXPORTS CONSISTENT
------ MUST TEST WITH REPL
+    fces <- intParserLenient <|> floatParserLenient
+    Parsec.try (Parsec.manyTill Parsec.anyChar (Parsec.try fromSeparator))
+    reqs <- andorParser
+    return $ FROM fces reqs
