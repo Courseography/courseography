77e9bb1a9377c08f4467ba7cb972baee1ac14574
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2016-11-18 02:24:58 -0500

    Update Requirement module

--- app/Database/Requirement.hs
+++ app/Database/Requirement.hs
@@ -16,23 +16,20 @@ import qualified Data.List as L
 
 data ProgramReq = PRGREQ String [Req]
 
--- | Returns a well formatted String representing a program requirement for specified program.
+-- Returns a well formatted String representing a program requirement for specified program.
 instance Show ProgramReq where
-    show (PRGREQ program reqs) = "Program Requirements for " ++ program ++ ":\n" ++ L.intercalate "\n" (map show reqs)
+    show (PRGREQ program reqs) = "Program Requirements for " ++ program
+                               ++ ":\n" ++ L.intercalate "\n" (map show reqs)
 
 data CourseReq = CRSREQ String Req Req Req
 
 instance Show CourseReq where
-    show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n" ++ show creq ++ "\n"
-                                      ++ "Exclusions for " ++ course ++ ":\n" ++ show excl ++ "\n"
-                                      ++ "Prerequisites for " ++ course ++ ":\n" ++ show preq ++ "\n"
+    show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n"
+                                        ++ show creq ++ "\n" ++ "Exclusions for "
+                                        ++ course ++ ":\n" ++ show excl ++ "\n"
+                                        ++ "Prerequisites for " ++ course ++ ":\n"
+                                        ++ show preq ++ "\n"
 
+-- define separators for "from"
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
-data Req = J String | AND [Req] | OR [Req] | FROM Integer [Req] | PAR Req
-
-instance Show Req where
-    show (J course) = course
-    show (AND reqs) = L.intercalate "," $ map show reqs
-    show (OR reqs) = L.intercalate "/" $ map show reqs
-    show (FROM a reqs) =  show a ++ "FCE(s) from: (" ++ show reqs ++ ")"
-    show (PAR req) = "(" ++ show req ++ ")"
+data Req = J String | AND [Req] | OR [Req] | FROM String Req | PAR Req
