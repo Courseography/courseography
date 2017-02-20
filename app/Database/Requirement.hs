16a192dc3c6e5cb15442dfdd3640d8505c00b505
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-02-20 10:42:16 -0500

    Derive Show and Eq for ALL data types in Requirement for testing.

--- app/Database/Requirement.hs
+++ app/Database/Requirement.hs
@@ -14,35 +14,35 @@ module Database.Requirement
 import qualified Data.String as S
 import qualified Data.List as L
 
-data ProgramReq = PRGREQ String [Req]
+data ProgramReq = PRGREQ String [Req] deriving (Eq, Show)
 
--- Returns a well formatted String representing a program requirement for specified program.
-instance Show ProgramReq where
-    show (PRGREQ program reqs) = "Program Requirements for " ++ program
-                               ++ ":\n" ++ L.intercalate "\n" (map show reqs)
+-- -- Returns a well formatted String representing a program requirement for specified program.
+-- instance Show ProgramReq where
+--     show (PRGREQ program reqs) = "Program Requirements for " ++ program
+--                                ++ ":\n" ++ L.intercalate "\n" (map show reqs)
 
-data CourseReq = CRSREQ String Req Req Req
+data CourseReq = CRSREQ String Req Req Req deriving (Eq, Show)
 
-instance Show CourseReq where
-    show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n"
-                                        ++ show creq ++ "\n" ++ "Exclusions for "
-                                        ++ course ++ ":\n" ++ show excl ++ "\n"
-                                        ++ "Prerequisites for " ++ course ++ ":\n"
-                                        ++ show preq ++ "\n"
+-- instance Show CourseReq where
+--     show (CRSREQ course creq excl preq) = "Corequisites for " ++ course ++ ":\n"
+--                                         ++ show creq ++ "\n" ++ "Exclusions for "
+--                                         ++ course ++ ":\n" ++ show excl ++ "\n"
+--                                         ++ "Prerequisites for " ++ course ++ ":\n"
+--                                         ++ show preq ++ "\n"
 
 -- for now J seems to be most readable and convenient value constructor for satisfying rec structure.
-data Req = J String | AND [Req] | OR [Req] | FROM String Req | GRADE String Req | RAW String
+data Req = J String | AND [Req] | OR [Req] | FROM String Req | GRADE String Req | RAW String deriving (Eq, Show)
 
-instance Show Req where
-    show (J course) = course
-    show (AND reqs) =
-        case reqs of
-        [x] -> show x
-        otherwise -> "(" ++ (L.intercalate "," $ map show reqs) ++ ")"
-    show (OR reqs) =
-        case reqs of
-        [x] -> show x
-        otherwise -> "(" ++ (L.intercalate "/" $ map show reqs) ++ ")"
-    show (FROM fces reqs) =  fces ++ " FCE(s) from:\n" ++ show reqs
-    show (GRADE grade reqs) =  "(" ++ show reqs ++ " with a minimum grade of " ++ grade ++ "%)"
-    show (RAW text) = "(" ++ text ++ ")"
+-- instance Show Req where
+--     show (J course) = course
+--     show (AND reqs) =
+--         case reqs of
+--         [x] -> show x
+--         otherwise -> "(" ++ (L.intercalate "," $ map show reqs) ++ ")"
+--     show (OR reqs) =
+--         case reqs of
+--         [x] -> show x
+--         otherwise -> "(" ++ (L.intercalate "/" $ map show reqs) ++ ")"
+--     show (FROM fces reqs) =  fces ++ " FCE(s) from:\n" ++ show reqs
+--     show (GRADE grade reqs) =  "(" ++ show reqs ++ " with a minimum grade of " ++ grade ++ "%)"
+--     show (RAW text) = "(" ++ text ++ ")"
