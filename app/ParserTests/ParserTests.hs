ca6c443dc7cb6c7f16b8c6eadeb06ca86c544fb1
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-02-20 15:49:15 -0500

    Create basic tests, implement function to facilitate making test cases.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -9,7 +9,7 @@ Module containing test cases for Requirement Parsers.
 -- For more examples, check out the tests directory.  It contains unit tests
 -- for HUnit.
 
-module ParserTest.ParserTests where
+module ParserTests.ParserTests where
 
 import Test.HUnit
 import Database.Requirement
@@ -22,25 +22,23 @@ import Control.Monad.Identity (Identity)
 -- applies parser to string and creates assertion as in test 2?
 
 test1 :: Test
-test1 = TestCase (assertEqual "for (parseReqs \"csc108h1 or csc148h1\"),"
-                OR [J"csc108h1",J"csc148h1"] (parseReqs "csc108h1 or csc148h1"))
-
-
+test1 = TestCase (assertEqual "for (parseReqs \"csc108h1 or csc148h1\")," (OR [J"csc108h1",J"csc148h1"]) (parseReqs "csc108h1 or csc148h1"))
 
 test2 :: Test
-test2 = TestCase(do courseReq <- courseParser " csc148h1 "
-                    assertEqual "for (courseParser \" csc148h1 \"),"
-                    J "csc148h1" courseReq)
+test2 = TestCase(let courseReq = Parsec.parse courseParser "" " csc148h1 "
+                    in assertEqual "for (courseParser \" csc148h1 \")," (Right $ J "csc148h1") courseReq)
 
 test3 :: Test
-test3 = TestCase(do courseReq <- courseParser " csc148h1 "
-                    assertEqual "for (courseParser \" csc148h1 \"),"
-                    J "csc148h1" courseReq)
+test3 = TestCase(let courseReq = Parsec.parse orParser "" " csc148h1 "
+                    in assertEqual "for (orParser \" csc148h1 \")," (Right $ OR[J "csc148h1"]) courseReq)
 
-test100 :: Test
-test2 = TestCase (do courseReq <-  singleParser " csc148h1 "
-                     assertEqual "for (singleParser \" csc148h1 \","
-                     J "csc148h1" courseReq
+test4 :: Test
+test4 = TestCase(let courseReq = Parsec.parse andParser "" " csc148h1,csc165h1 "
+                    in assertEqual "for (andParser \" csc148h1,csc165h1 \")," (Right $ AND[J "csc148h1",J "csc165h1"]) courseReq)
+
+createTest :: Parser Req -> String -> Req -> Test
+createTest parser input expected = TestCase (let courseReq =  Parsec.parse parser "" input
+	in assertEqual ("for (" ++ input ++ "),") (Right expected) courseReq)
 
 tests :: Test
 tests = TestList [TestLabel "parseReqs" test1, TestLabel "testsingleParser" test2]
