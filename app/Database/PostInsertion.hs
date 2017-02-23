ec2b8648e33a68cfc62cf7fb5d0b54cd10b3ad0d
FrankJohnLee <frankjohnlee@live.com>
2017-02-23 17:28:15 -0500

    Changed numbers at the end to _.

--- app/Database/PostInsertion.hs
+++ app/Database/PostInsertion.hs
@@ -11,9 +11,9 @@ import Database.Tables hiding (postCode, description, postCategoryName)
 
 -- | Insert a new post into the database
 insertPost :: T.Text -> T.Text -> T.Text -> T.Text -> IO ()
-insertPost departmentName1 postName0 postCode description =
+insertPost departmentName1 postName_ postCode description =
     runSqlite databasePath $ do
-        insert_ $ Post postName0 departmentName1 postCode description  :: SqlPersistM ()
+        insert_ $ Post postName_ departmentName_ postCode description  :: SqlPersistM ()
 
 -- | Insert a new post category into the database
 insertPostCategory :: T.Text -> T.Text -> IO ()
