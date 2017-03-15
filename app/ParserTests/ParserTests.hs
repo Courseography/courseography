3cba730eb149a5b185d99c815aca7bb83fb2d16c
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-03-15 19:22:15 -0400

    Refactor module for cabal testing. Export reqTestSuite.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -9,7 +9,8 @@ Module containing test cases for Requirement Parsers.
 -- For more examples, check out the tests directory.  It contains unit tests
 -- for HUnit.
 
-module ParserTest.ParserTests where
+module ParserTests.ParserTests
+(  reqTestSuite  ) where
 
 import Test.HUnit
 import Database.Requirement
@@ -19,25 +20,8 @@ import WebParsing.ReqParser
 -- applies parser to string and creates assertion as in test 2?
 
 test1 :: Test
-test1 = TestCase (assertEqual "for (parseReqs \"csc108h1 or csc148h1\"),"
-                OR [J"csc108h1",J"csc148h1"] (parseReqs "csc108h1 or csc148h1"))
+test1 = TestCase (assertEqual "for (parseReqs \"csc108h1 or csc148h1\")," (OR [J"csc108h1",J"csc148h1"]) (parseReqs "csc108h1 or csc148h1"))
 
 
-
-test2 :: Test
-test2 = TestCase(do courseReq <- courseParser " csc148h1 "
-                    assertEqual "for (courseParser \" csc148h1 \"),"
-                    J "csc148h1" courseReq)
-
-test3 :: Test
-test3 = TestCase(do courseReq <- courseParser " csc148h1 "
-                    assertEqual "for (courseParser \" csc148h1 \"),"
-                    J "csc148h1" courseReq)
-
-test100 :: Test
-test2 = TestCase (do courseReq <-  singleParser " csc148h1 "
-                     assertEqual "for (singleParser \" csc148h1 \","
-                     J "csc148h1" courseReq
-
-tests :: Test
-tests = TestList [TestLabel "parseReqs" test1, TestLabel "testsingleParser" test2]
+reqTestSuite :: Test
+reqTestSuite = TestList [TestLabel "parseReqs" test1]
