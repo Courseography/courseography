6817a5c4083f1761fb53a33c5d5032cf11b734bb
Ailsa (Tiantian) Fang <tfang@sysomos.com>
2017-04-16 16:39:03 -0400

    change back white space

--- app/Database/CourseInsertion.hs
+++ app/Database/CourseInsertion.hs
@@ -37,10 +37,10 @@ saveGraphJSON jsonStr nameStr = do
     where
         insertGraph :: T.Text -> [Text] -> [Shape] -> [Path] -> SqlPersistM ()
         insertGraph nameStr_ texts shapes paths = do
-          gId <- insert $ Graph nameStr_ 256 256
-          insertMany_ $ map (\text -> text {textGraph = gId}) texts
-          insertMany_ $ map (\shape -> shape {shapeGraph = gId}) shapes
-          insertMany_ $ map (\path -> path {pathGraph = gId}) paths
+            gId <- insert $ Graph nameStr_ 256 256
+            insertMany_ $ map (\text -> text {textGraph = gId}) texts
+            insertMany_ $ map (\shape -> shape {shapeGraph = gId}) shapes
+            insertMany_ $ map (\path -> path {pathGraph = gId}) paths
 
 --contains' :: PersistEntity m => T.Text -> SqlPersistM m
 --contains field query = Filter field (Left $ T.concat ["%", query, "%"]) (BackendSpecificFilter "LIKE")
