0a81d628c2e2d3da8c95ba93a1406e004faf539c
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-18 12:30:07 -0400

    Add basic test cases.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -19,8 +19,19 @@ createTest :: (Eq a, Show a) => Parser a -> (String, a) -> Test
 createTest parser (input, expected) = let courseReq =  Parsec.parse parser "" input
                                       in TestCase $ (assertEqual ("for (" ++ input ++ "),") (Right expected) courseReq)
 
+
 test1 :: Test
 test1 = TestCase (assertEqual "TEST PASS" ("True") ("True"))
 
+-- Basic inputs found on artsci website; only andParser.
+basicInputs :: [(String, Req)]
+basicInputs = [("CSC120H1/CSC148H1", OR [J "CSC120H1", J "CSC148H1"]),
+			   ("CSC120H1, CSC121H1, CSC148H1", AND [J "CSC120H1", J "CSC121H1", J "CSC148H1"]),
+			   ("CSC148H1/(CSC108H1/CSC120H1, MAT137Y1/MAT157Y1)", OR [J "CSC148H1", AND [OR [J "CSC108H1", J "CSC120H1"], OR [J "MAT137Y1", J "MAT157Y1"]]]),
+			   ("STA247H1/STA255H1/STA257H1/PSY201H1/ECO227Y1, (MAT135H1, MAT136H1)/MAT137Y1/MAT157Y1", AND [OR [J "STA247H1", J "STA255H1", J "STA257H1", J "PSY201H1", J "ECO227Y1"], OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1", J "MAT157Y1"]])]
+
+basicTests :: Test
+basicTests = TestLabel "basic categoryParser" $ TestList $ (map (createTest categoryParser) basicInputs)
+
 reqTestSuite :: Test
-reqTestSuite = TestList [TestLabel "parseReqs" test1]
+reqTestSuite = TestList [TestLabel "basicTests" basicTests]
