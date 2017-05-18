294dca6b47e17b2c12fd8330a0f8f0045d46e544
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-05-18 11:12:22 -0400

    Add imports. Implement helper to facilite creating test cases.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -8,7 +8,16 @@ Module containing test cases for Requirement Parsers.
 module ParserTests.ParserTests
 ( reqTestSuite ) where
 
-import Test.HUnit
+import qualified Text.Parsec as Parsec
+import Text.Parsec.String (Parser)
+import Database.Requirement
+import WebParsing.ReqParser
+import Test.HUnit ( assertEqual, Test(..) )
+
+-- Function to facilitate test case creation given a string, Req tuple
+createTest :: (Eq a, Show a) => Parser a -> (String, a) -> Test
+createTest parser (input, expected) = let courseReq =  Parsec.parse parser "" input
+                                      in TestCase $ (assertEqual ("for (" ++ input ++ "),") (Right expected) courseReq)
 
 test1 :: Test
 test1 = TestCase (assertEqual "TEST PASS" ("True") ("True"))
