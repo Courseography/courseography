9c82f1b1cf87b49fdb3fda241b5cf6f5d2234ff2
minfanzhang <zhang.minfan@mail.utoronto.ca>
2017-06-07 15:58:50 -0500

    Add files via upload

---
+++ app/Main.hs
@@ -0,0 +1,53 @@
+{-|
+Description: Entry point for Courseography.
+
+This module implements a 'main' method which takes a command-line
+argument and executes the corresponding IO action.
+
+If no argument is provided, the default action is 'server', which
+starts the server.
+-}
+module Main where
+
+import System.IO (stderr, hPutStrLn)
+import System.Environment (getArgs)
+import Data.Maybe (fromMaybe)
+import Data.List (intercalate)
+import qualified Data.Map.Strict as Map
+
+-- internal dependencies
+import Server (runServer)
+import Database.Database (setupDatabase)
+import Svg.Parser (parsePrebuiltSvgs)
+import Css.Compiler (compileCSS)
+import Util.Documentation (generateDocs)
+
+-- dynamicgraph_test
+import DynamicGraphs.WriteRunDot(doDots)
+import DynamicGraphs.GraphGenerator(ex3)
+
+-- | A map of command-line arguments to their corresponding IO actions.
+taskMap :: Map.Map String (IO ())
+taskMap = Map.fromList [
+    ("server", runServer),
+    ("database", setupDatabase),
+    ("graphs", parsePrebuiltSvgs),
+    ("css", compileCSS),
+    ("docs", generateDocs),
+    ("minfangraph", doDots [ ("ex3" , ex3) ])]
+
+-- | Courseography entry point.
+main :: IO ()
+main = do
+    args <- getArgs
+    let taskName = if null args then "server" else head args
+    fromMaybe putUsage (Map.lookup taskName taskMap)
+
+
+-- | Print usage message to user (when main gets an incorrect argument).
+putUsage :: IO ()
+putUsage = hPutStrLn stderr usageMsg
+    where
+        taskNames = Map.keys taskMap
+        usageMsg = "Unrecognized argument. Available arguments:\n" ++
+                   intercalate "\n" taskNames
