3c62a9913490368efa0d1294ac81601e57419b89
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-07-26 14:42:23 -0400

    Update test cases with new structure of Req.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -49,9 +49,9 @@ parInputs = [
 
 fromParInputs :: [(String, Req)]
 fromParInputs = [
-      ("1.0 FCE from the following: (CSC148H1)", FROM "1.0" $ J "CSC148H1")
-    , ("2.0 FCEs from CSC165H1/CSC148H1", FROM "2.0" $ OR [J "CSC165H1", J "CSC148H1"])
-    , ("2 FCEs from: MAT135H1, MAT136H1/ MAT137Y1", FROM "2" $ AND [J "MAT135H1",OR [J "MAT136H1",J "MAT137Y1"]])
+      ("1.0 FCE from the following: (CSC148H1)", FCES "1.0" $ J "CSC148H1")
+    , ("2.0 FCEs from CSC165H1/CSC148H1", FCES "2.0" $ OR [J "CSC165H1", J "CSC148H1"])
+    , ("2 FCEs from: MAT135H1, MAT136H1/ MAT137Y1", FCES "2" $ AND [J "MAT135H1",OR [J "MAT136H1",J "MAT137Y1"]])
     ]
 
 
@@ -77,7 +77,7 @@ gradeAftInputs = [
 
 artSciInputs :: [(String, Req)]
 artSciInputs = [
-      ("1.0 full course or its equivalent from HIS230H1/ HIS231H1/ NEW220H1/ NEW221H1/ NEW225H1/ NEW226H1", FROM "1.0" (OR [J "HIS230H1",J "HIS231H1",J "NEW220H1",J "NEW221H1",J "NEW225H1",J "NEW226H1"]))
+      ("1.0 full course or its equivalent from HIS230H1/ HIS231H1/ NEW220H1/ NEW221H1/ NEW225H1/ NEW226H1", FCES "1.0" (OR [J "HIS230H1",J "HIS231H1",J "NEW220H1",J "NEW221H1",J "NEW225H1",J "NEW226H1"]))
     , ("5 FCEs of any of the following: LIN232H1; LIN241H1; JLP315H1; LIN331H1; LIN341H1; JLP374H1", J "")
     , ("(ECO101H1, ECO102H1)/ECO100Y1, RSM100H1/ MGT100H1/ RSM100Y1", AND [OR [AND [J "ECO101H1",J "ECO102H1"],J "ECO100Y1"],OR [J "RSM100H1",J "MGT100H1",J "RSM100Y1"]])
     , ("CAS310H1 and CAS320H1", AND [J "CAS310H1",J "CAS320H1"])
@@ -86,9 +86,9 @@ artSciInputs = [
 {-|
 "FALSE POSITIVE?"
 
-Right (AND [FROM "5" (GRADE "1" (J "LIN232H1")),GRADE "1" (J "JLP315H1"),GRADE "1" (J "LIN341H1")]
+Right (AND [FCES "5" (GRADE "1" (J "LIN232H1")),GRADE "1" (J "JLP315H1"),GRADE "1" (J "LIN341H1")]
     vs.
-Right (FROM "5" [J "LIN232H1", J "LIN241H1", J "JLP315H1", J "LIN331H1", J "LIN341H1", J "JLP374H1"])
+Right (FCES "5" [J "LIN232H1", J "LIN241H1", J "JLP315H1", J "LIN331H1", J "LIN341H1", J "JLP374H1"])
 
 How do we want to handle semicolons?
 
