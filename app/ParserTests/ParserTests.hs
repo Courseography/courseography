b2b113eea10719432820e8d498857a24a0475f32
RyanDJLee <ryandj.lee@mail.utoronto.ca>
2017-08-29 21:44:24 -0400

    Update test cases according to new J constructor.

--- app/ParserTests/ParserTests.hs
+++ app/ParserTests/ParserTests.hs
@@ -20,65 +20,68 @@ createTest parser label input = TestLabel label $ TestList $ map (\(x, y) ->
                                 TestCase $ assertEqual ("for (" ++ x ++ "),")
                                 (Right y) (Parsec.parse parser "" x)) input
 
+-- categoryParser Tests
 orInputs :: [(String, Req)]
 orInputs = [
-      ("CSC120H1/CSC148H1", OR [J "CSC120H1", J "CSC148H1"])
-    , ("CSC108H1/CSC120H1/CSC148H1", OR [J "CSC108H1", J "CSC120H1", J "CSC148H1"])
+      ("CSC120H1/CSC148H1", OR [J "CSC120H1" "", J "CSC148H1" ""])
+    , ("CSC108H1/CSC120H1/CSC148H1", OR [J "CSC108H1" "", J "CSC120H1" "", J "CSC148H1" ""])
     ]
 
 andInputs :: [(String, Req)]
 andInputs = [
-      ("CSC165H1, CSC236H1", AND [J "CSC165H1", J "CSC236H1"])
-    , ("CSC120H1, CSC121H1, CSC148H1", AND [J "CSC120H1", J "CSC121H1", J "CSC148H1"])
+      ("CSC165H1, CSC236H1", AND [J "CSC165H1" "", J "CSC236H1" ""])
+    , ("CSC120H1, CSC121H1, CSC148H1", AND [J "CSC120H1" "", J "CSC121H1" "", J "CSC148H1" ""])
     ]
 
 andorInputs :: [(String, Req)]
 andorInputs = [
-      ("CSC148H1/CSC207H1, CSC165H1/CSC236H1", AND [OR [J "CSC148H1", J "CSC207H1"], OR [J "CSC165H1", J "CSC236H1"]])
+      ("CSC148H1/CSC207H1, CSC165H1/CSC236H1", AND [OR [J "CSC148H1" "", J "CSC207H1" ""], OR [J "CSC165H1" "", J "CSC236H1" ""]])
     ]
 
 parInputs :: [(String, Req)]
 parInputs = [
-      ("(CSC148H1)", J "CSC148H1")
-    , ("CSC108H1, (CSC165H1/CSC148H1)", AND [J "CSC108H1", OR [J "CSC165H1", J "CSC148H1"]])
-    , ("(MAT135H1, MAT136H1)/ MAT137Y1", OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1"])
-    , ("CSC148H1/(CSC108H1/CSC120H1, MAT137Y1/MAT157Y1)", OR [J "CSC148H1", AND [OR [J "CSC108H1", J "CSC120H1"], OR [J "MAT137Y1", J "MAT157Y1"]]])
-    , ("STA247H1/STA255H1/STA257H1/PSY201H1/ECO227Y1, (MAT135H1, MAT136H1)/MAT137Y1/MAT157Y1", AND [OR [J "STA247H1", J "STA255H1", J "STA257H1", J "PSY201H1", J "ECO227Y1"], OR [AND [J "MAT135H1", J "MAT136H1"], J "MAT137Y1", J "MAT157Y1"]])
+      ("(CSC148H1)", J "CSC148H1" "")
+    , ("CSC108H1, (CSC165H1/CSC148H1)", AND [J "CSC108H1" "", OR [J "CSC165H1" "", J "CSC148H1" ""]])
+    , ("(MAT135H1, MAT136H1)/ MAT137Y1", OR [AND [J "MAT135H1" "", J "MAT136H1" ""], J "MAT137Y1" ""])
+    , ("CSC148H1/(CSC108H1/CSC120H1, MAT137Y1/MAT157Y1)", OR [J "CSC148H1" "", AND [OR [J "CSC108H1" "", J "CSC120H1" ""], OR [J "MAT137Y1" "", J "MAT157Y1" ""]]])
+    , ("STA247H1/STA255H1/STA257H1/PSY201H1/ECO227Y1, (MAT135H1, MAT136H1)/MAT137Y1/MAT157Y1", AND [OR [J "STA247H1" "", J "STA255H1" "", J "STA257H1" "", J "PSY201H1" "", J "ECO227Y1" ""], OR [AND [J "MAT135H1" "", J "MAT136H1" ""], J "MAT137Y1" "", J "MAT157Y1" ""]])
     ]
 
 fromParInputs :: [(String, Req)]
 fromParInputs = [
-      ("1.0 FCE from the following: (CSC148H1)", FCES "1.0" $ J "CSC148H1")
-    , ("2.0 FCEs from CSC165H1/CSC148H1", FCES "2.0" $ OR [J "CSC165H1", J "CSC148H1"])
-    , ("2 FCEs from: MAT135H1, MAT136H1/ MAT137Y1", FCES "2" $ AND [J "MAT135H1",OR [J "MAT136H1",J "MAT137Y1"]])
+      ("1.0 FCE from the following: (CSC148H1)", FCES "1.0" $ J "CSC148H1" "")
+    , ("2.0 FCEs from CSC165H1/CSC148H1", FCES "2.0" $ OR [J "CSC165H1" "", J "CSC148H1" ""])
+    , ("2 FCEs from: MAT135H1, MAT136H1/ MAT137Y1", FCES "2" $ AND [J "MAT135H1" "",OR [J "MAT136H1" "",J "MAT137Y1" ""]])
     ]
 
+
+-- TODO: No more "before" reqs in new artsci calendar except first test case.
 gradeBefInputs :: [(String, Req)]
 gradeBefInputs = [
-      ("minimum mark of A- in CSC236H1", GRADE "A-" $ J "CSC236H1") 
-    , ("minimum grade of 75% CSC236H1", GRADE "75" $ J "CSC236H1")
-    , ("minimum of 75% CSC236H1", GRADE "75" $ J "CSC236H1")
-    , ("minimum (75%) CSC236H1", GRADE "75" $ J "CSC236H1")
+      ("minimum mark of A- in CSC236H1", GRADE "A-" $ J "CSC236H1" "") 
+    , ("minimum grade of 75% CSC236H1", GRADE "75" $ J "CSC236H1" "")
+    , ("minimum of 75% CSC236H1", GRADE "75" $ J "CSC236H1" "")
+    , ("minimum (75%) CSC236H1", GRADE "75" $ J "CSC236H1" "")
     ]
 
 gradeAftInputs :: [(String, Req)]
 gradeAftInputs = [
-      ("CSC236H1 75%", GRADE "75" $ J "CSC236H1")
-    , ("CSC236H1 (75%)", GRADE "75" $ J "CSC236H1")
-    , ("CSC236H1(75%)", GRADE "75" $ J "CSC236H1")
-    , ("CSC263H1 (C+)", GRADE "C+" $ J "CSC263H1")
-    , ("CSC263H1 B-", GRADE "B-" $ J "CSC263H1") 
-    , ("CSC263H1 with a minimum grade of 60%", GRADE "60" $ J "CSC263H1") 
-    , ("CSC263H1 with a minimum mark of B-", GRADE "B-" $ J "CSC263H1")
+      ("CSC236H1 75%", GRADE "75" $ J "CSC236H1" "")
+    , ("CSC236H1 (75%)", GRADE "75" $ J "CSC236H1" "")
+    , ("CSC236H1(75%)", GRADE "75" $ J "CSC236H1" "")
+    , ("CSC263H1 (C+)", GRADE "C+" $ J "CSC263H1" "")
+    , ("CSC263H1 B-", GRADE "B-" $ J "CSC263H1" "") 
+    , ("CSC263H1 with a minimum grade of 60%", GRADE "60" $ J "CSC263H1" "") 
+    , ("CSC263H1 with a minimum mark of B-", GRADE "B-" $ J "CSC263H1" "")
     ]
 
 artSciInputs :: [(String, Req)]
 artSciInputs = [
-      ("1.0 full course or its equivalent from HIS230H1/ HIS231H1/ NEW220H1/ NEW221H1/ NEW225H1/ NEW226H1", FCES "1.0" (OR [J "HIS230H1",J "HIS231H1",J "NEW220H1",J "NEW221H1",J "NEW225H1",J "NEW226H1"]))
-    , ("5 FCEs of any of the following: LIN232H1; LIN241H1; JLP315H1; LIN331H1; LIN341H1; JLP374H1", FCES "5" (AND [J "LIN232H1",J "LIN241H1",J "JLP315H1",J "LIN331H1",J "LIN341H1",J "JLP374H1"]))
-    , ("(ECO101H1, ECO102H1)/ECO100Y1, RSM100H1/ MGT100H1/ RSM100Y1", AND [OR [AND [J "ECO101H1",J "ECO102H1"],J "ECO100Y1"],OR [J "RSM100H1",J "MGT100H1",J "RSM100Y1"]])
-    , ("CAS310H1 and CAS320H1", AND [J "CAS310H1",J "CAS320H1"])
+      ("2.0 FCEs: BIO220H1 (ecology and evolutionary biology); ENV234H1, ENV334H1 (environmental biology)", J "" "")
+    , ("0.5 FCE in statistics from: EEB225H1 (recommended)/ STA220H1/ STA257H1/  STA288H1/ GGR270H1/ PSY201H1", J "" "")
     ]
+
+
 
 orTests :: Test
 orTests = createTest categoryParser "Basic or Requirement" orInputs
@@ -107,3 +110,4 @@ artSciTests = createTest categoryParser "Arts and Science requirements from Chri
 -- functions for running tests in REPL
 reqTestSuite :: Test
 reqTestSuite = TestLabel "ReqParser tests" $ TestList [orTests, andTests, andorTests, parTests, fromParTests, gradeBefTests, gradeAftTests, artSciTests]
+ TestLabel "ReqParser tests" $ TestList [orTests, andTests, andorTests, parTests, fromParTests, gradeBefTests, gradeAftTests, artSciTests]
