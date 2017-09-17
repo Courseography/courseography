1ab14ca93a5412a85dffcb6e83c0e52530336ddf
Minfan Zhang <zhang.minfan@mail.utoronto.ca>
2017-09-17 01:12:37 -0500

    solved all problems except for the short form of type constructor

--- app/DynamicGraphs/WriteRunDot.hs
+++ app/DynamicGraphs/WriteRunDot.hs
@@ -12,8 +12,7 @@ doDots cases = do
 createImage :: PrintDotRepr dg n => (FilePath, dg n) -> IO FilePath
 createImage (n, g) = createImageInDir (normalise "graphs/gen") n Png g
 
--- (question)did not find fuction addExtension under Data.GraphViz
--- here runGraphvizCommand Dot creates the final graph given the input DotGraph object g and connects it
+-- Here runGraphvizCommand Dot creates the final graph given the input DotGraph object g and connects it
 -- with the file path(by comining directory d and filename n) to make the final graph in the required directory.
 createImageInDir :: PrintDotRepr dg n => FilePath -> FilePath -> GraphvizOutput -> dg n -> IO FilePath
 createImageInDir d n o g = Data.GraphViz.addExtension (runGraphvizCommand Dot g) o (combine d n)
