8ca618b7e5739970e67952ae5cb121bc0c6afa89
Minfan Zhang <zhang.minfan@mail.utoronto.ca>
2017-10-04 22:10:23 -0500

    10.4 update, solving problems except for extenability.

--- app/DynamicGraphs/GraphGenerator.hs
+++ app/DynamicGraphs/GraphGenerator.hs
@@ -21,7 +21,7 @@ type StmtsWithCounter = ([DotStatement Text], Int)
 -- Serves as a sort of "interface" for the whole part "dynamic graph"
 sampleGraph :: DotGraph Text
 sampleGraph = reqsToGraph [
-    ("CSC148", J "CSC108")
+    ("CSC148H1", RAW "some literal requirements")
     ]
 
 -- ** Main algorithm for converting requirements into a graph
@@ -40,29 +40,72 @@ reqsToGraph reqs =
 reqToStmts :: StmtsWithCounter -> (Text, Req) -> StmtsWithCounter
 reqToStmts (stmtslst, counter) (name, NONE) = let stmtslst0 = let stmtslst1 = stmtslst 
                                                                   counter1 = counter
-                                                              in  stmtslst1 ++ [DN $ DotNode (name `mappend` (pack (show (counter1)))) []]
+                                                              in  stmtslst1 ++ [DN $ DotNode (mappendTextWithCounter name counter1) []]
                                                   counter0 =  let counter2 = counter
-                                                              in  (counter2) + 1
+                                                              in  (counter2 + 1)
                                               in  (stmtslst0, counter0)
+
 reqToStmts (stmtslst, counter) (name, J string1) = let (stmtslst0, _) = foldUpReqLst [(name, NONE), (str1, NONE)] 0
                                                    in ([DE $ DotEdge str1 name []] ++ stmtslst0, counter + 3)
   where str1 = pack string1
 
+reqToStmts (stmtslst, counter) (name, AND reqs1) = ([DN $ DotNode (mappendTextWithCounter "and" (counter_sub + 1000)) [], 
+                                                    DE $ DotEdge (mappendTextWithCounter "and" (counter_sub + 1000)) 
+                                                    (mappendTextWithCounter name counter) []] ++ 
+                                                    statements_sub, counter_sub + 1)
+  where createSingleSubStmt = createSubnodeCorrespondingEdge (mappendTextWithCounter "and" (counter_sub + 1000))
+        (statements_sub, counter_sub) = foldl(\acc x -> createSingleSubStmt acc x) (reqToStmts (stmtslst, counter) (name, NONE)) (decompJString reqs1)
+
+reqToStmts (stmtslst, counter) (name, OR reqs1) = ([DN $ DotNode (mappendTextWithCounter "or" (counter_sub + 1000)) [], 
+                                                    DE $ DotEdge (mappendTextWithCounter "or" (counter_sub + 1000)) 
+                                                    (mappendTextWithCounter name counter) []] ++ 
+                                                    statements_sub, counter_sub + 1)
+  where createSingleSubStmt = createSubnodeCorrespondingEdge (mappendTextWithCounter "or" (counter_sub + 1000))
+        (statements_sub, counter_sub) = foldl(\acc x -> createSingleSubStmt acc x) (reqToStmts (stmtslst, counter) (name, NONE)) (decompJString reqs1)
+
+reqToStmts (stmtslst, counter) (name, FROM string1 (J string2)) = ([DN $ DotNode (mappendTextWithCounter (pack string1) (counter_sub + 1000)) [], 
+                                                                    DE $ DotEdge (mappendTextWithCounter (pack string1) (counter_sub + 1000)) 
+                                                                    (mappendTextWithCounter name counter) []] ++ 
+                                                                    statements_sub, counter_sub + 1)
+  where createSingleSubStmt = createSubnodeCorrespondingEdge (mappendTextWithCounter (pack string1) (counter_sub + 1000))
+        (statements_sub, counter_sub) = foldl(\acc x -> createSingleSubStmt acc x) (reqToStmts (stmtslst, counter) (name, NONE)) [((pack string2), NONE)]
+
+reqToStmts (stmtslst, counter) (name, GRADE string1 (J string2)) = ([DN $ DotNode (mappendTextWithCounter (pack string1) (counter_sub + 1000)) [], 
+                                                                    DE $ DotEdge (mappendTextWithCounter (pack string1) (counter_sub + 1000)) 
+                                                                    (mappendTextWithCounter name counter) []] ++ 
+                                                                    statements_sub, counter_sub + 1)
+  where createSingleSubStmt = createSubnodeCorrespondingEdge (mappendTextWithCounter (pack string1) (counter_sub + 1000))
+        (statements_sub, counter_sub) = foldl(\acc x -> createSingleSubStmt acc x) (reqToStmts (stmtslst, counter) (name, NONE)) [((pack string2), NONE)]
+
+reqToStmts (stmtslst, counter) (name, RAW string1) = ([DN $ DotNode (mappendTextWithCounter (pack string1) counter) [], 
+                                                       DE $ DotEdge (mappendTextWithCounter (pack string1) counter) 
+                                                       (mappendTextWithCounter name counter) []], counter + 1)
+
 foldUpReqLst :: [(Text, Req)] -> Int -> StmtsWithCounter
 foldUpReqLst reqlst count = foldl(\acc x -> reqToStmts acc x) ([], count) reqlst
 
+createSubnodeCorrespondingEdge :: Text -> StmtsWithCounter -> (Text, Req) -> StmtsWithCounter
+createSubnodeCorrespondingEdge parentnode (stmtslst, counter) (name, NONE) = let stmtslst1 = stmtslst ++ [DE $ DotEdge (mappendTextWithCounter name counter) 
+                                                                                                          parentnode []]
+                                                                             in reqToStmts (stmtslst1, counter) (name, NONE)
+
+mappendTextWithCounter :: Text -> Int -> Text
+mappendTextWithCounter text1 counter = text1 `mappend` "_counter_" `mappend` (pack (show (counter))) 
+
 -- Now this only wotks for Req lists of J String. Failed if using [Req] as input and pack x in foldl.
-decompJString :: [String] -> [(Text, Req)]
+decompJString :: [Req] -> [(Text, Req)]
 decompJString [] = []
-decompJString xs = foldl(\acc x -> (pack x, NONE):acc) [] xs
+decompJString ((J x):xs) = (pack x, NONE):(decompJString xs)
 
-createAndEdge :: [Text] -> [DotStatement Text]
-createAndEdge [] = []
-createAndEdge xs = foldl(\acc x -> [DE $ DotEdge x "and" []] ++ acc) [] xs
+createAndEdge :: Int -> Int -> [Text] -> [DotStatement Text]
+createAndEdge counter_node counter_and [] = []
+createAndEdge counter_node counter_and xs = foldl(\acc x -> [DE $ DotEdge (x `mappend` (pack (show (counter_node)))) 
+                                                             ("and" `mappend` (pack (show (counter_and)))) []] ++ acc) [] xs
 
-createOrEdge :: [Text] -> [DotStatement Text]
-createOrEdge [] = []
-createOrEdge xs = foldl(\acc x -> [DE $ DotEdge x "or" []] ++ acc) [] xs
+createOrEdge :: Int -> Int -> [Text] -> [DotStatement Text]
+createOrEdge counter_node counter_or [] = []
+createOrEdge counter_node counter_or xs = foldl(\acc x -> [DE $ DotEdge (x `mappend` (pack (show (counter_node)))) 
+                                                            ("or" `mappend` (pack (show (counter_or)))) []] ++ acc) [] xs
 
 dragNodeIdInReq :: [Req] -> [Text]
 dragNodeIdInReq [] = []
