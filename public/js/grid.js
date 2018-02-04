8fbe03e3cf0760db9707f8fe0c62796fbd119876
Sid Gupta <sid.gupta@mail.utoronto.ca>
2018-02-04 09:36:36 -0500

    Added comments to my local copy when studying the code

--- public/js/grid.js
+++ public/js/grid.js
@@ -2,13 +2,15 @@ requirejs([
     'grid/generate_grid',
     'grid/timetable_util',
     'common/utilities/trap_scroll',
-    'es6!common/export/export'
+    'es6!common/export/export',
+    'es6!grid/react_util'
     ],
     function(
         generate_grid,
         timetable_util,
         trap_scroll,
-        exp
+        exp,
+        react_util
     ) {
 
     $(document).ready(function () {
@@ -26,5 +28,7 @@ requirejs([
         timetable_util.getVeryLargeCourseArray();
         trap_scroll.trap_scroll();
         setTdHover();
+
+        react_util.initGrid();
     });
 });
---
+++ public/js/grid/react_util.js
@@ -0,0 +1,127 @@
+var CoursePanel = React.createClass({
+    render: function() {
+        return (
+            <ul className="trapScroll-enabled" id="course-select">
+                <li id="clear-all">
+                    <h3>Clear All</h3>
+                </li>
+            </ul>
+        );
+    }
+});
+
+var SearchPanel = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <div id="filter-container">
+                    <form onsubmit="return false;">
+                        <input id="course-filter" className="form-control" placeholder="Enter a course!" autoComplete="off" type="text" />
+                    </form>
+                </div>
+                <div id="search-container">
+                    <CourseList />
+                </div>
+            </div>
+        );
+    }
+});
+
+var CourseList = React.createClass({
+    getInitialState: function() {
+        return {
+            courses: [], courseFilter: ''
+        };
+    },
+
+    componentDidMount: function() {
+        $.ajax({
+            url: 'all-courses',
+            dataType: 'text',
+            success: function (data) {
+                this.enableSearch();
+                var courses = data.split('\n').map(function (course) {
+                    return course.substring(0, 8);
+                });
+                this.setState({courses: courses});
+            }.bind(this)
+        });
+    },
+
+    enableSearch: function() {
+        'use strict';
+
+        $('#course-filter').keyup(function() {
+            this.setState({courseFilter: $('#course-filter').val().toUpperCase()});
+        }.bind(this));
+    },
+
+    render: function() {
+
+        var state = this.state;
+
+        if (state.courseFilter !== '') {
+            var searchList = state.courses.filter(function (course) {
+                return course.indexOf(state.courseFilter) > -1;
+            }).map(function (course) {
+                return <CourseEntry course={course} key={course} />
+            });
+        }
+
+        return (
+            <div id="search-list">
+                <ul>{searchList}</ul>
+            </div>
+        );
+    }
+});
+
+var CourseEntry = React.createClass({
+
+    getInitialState: function() {
+        return {
+            star: false
+        };
+    },
+
+    toggleStar: function() {
+        this.setState({star: !this.state.star});
+    },
+
+    render: function() {
+
+        var classes = '';
+
+        if (this.state.star) {
+            classes += 'starred-course';
+        }
+
+        return (
+            <li id={this.props.course + '-search'} className={classes} onClick={this.toggleStar}>{this.props.course}</li>
+        );
+    }
+});
+
+
+var InfoPanel = React.createClass({
+    render: function() {
+        return (
+            <div id="info" className="row"></div>
+        );
+    }
+});
+
+
+export function initGrid() {
+  // ReactDOM.render(
+  //     <CoursePanel />,
+  //     document.getElementById('course-select-wrapper'));
+
+  ReactDOM.render(
+      <SearchPanel />,
+      document.getElementById('search-layout'));
+
+  // ReactDOM.render(
+  //     <InfoPanel />,
+  //     document.getElementsByClassName('col-md-8 col-xs-12 col-md-pull-2')[0]);
+}
