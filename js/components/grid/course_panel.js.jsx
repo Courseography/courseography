75e0c6c703ca525fc366f6b3dc2fc32285738b7e
betty <betty-wang@live.ca>
2018-07-07 17:03:43 -0400

    fix code format, remove multi-line ternary ifs and correct '==' equality checks to '==='

--- js/components/grid/course_panel.js.jsx
+++ js/components/grid/course_panel.js.jsx
@@ -39,7 +39,12 @@ class Course extends React.Component {
     )
     .then(response => response.json()) //datatype
     .then(data => {
-        let course = {courseCode: "", F: [], S:[], Y:[]}
+        let course = {
+          courseCode: "",
+          F: [],
+          S:[],
+          Y:[]
+        };
         course.courseCode = data.name;
         course.F = course.F.concat(this.parseLectures(data.fallSession.lectures),
                                     this.parseLectures(data.fallSession.tutorials));
@@ -54,43 +59,45 @@ class Course extends React.Component {
   parseLectures(lectures) {
     // Remove duplicated lecture sections
     let allLectures = lectures.filter((lecture, index, lectures) => {
-      return (lectures.map(lect => lect.section).indexOf(lecture.section) == index)
+      return (lectures.map(lect => lect.section).indexOf(lecture.section) === index)
     });
     let parsedLectures = [];
 
     let days = {0: 'M', 1: 'T', 2: 'W', 3: 'R', 4: 'F'};
     // Loop through the lecture sections to get each section's session code and lecture times
-    for (let i=0; i < allLectures.length; i++) {
+    allLectures.forEach( lectureInfo => {
       // Check to make sure its not a restricted section. Restricted sections have enrollment
       // restricted for a particular group of students, but happens at the same time and place as a regular
       // lecture/tutorial section.
-      if (allLectures[i].section.charAt(3) !== '2' && allLectures[i].times !== 'Online Web Version') {
-        let lecture = {courseName: allLectures[i].code.substring(0, 6) + " (" + allLectures[i].section.substring(0,1) + ")",
-                       lectureCode: allLectures[i].section.substring(0, 1) + allLectures[i].section.substring(3),
-                       times: {}};
-        let allTimes = allLectures[i].times;
+      if (lectureInfo.section.charAt(3) !== '2' && lectureInfo.times !== 'Online Web Version') {
+        let lecture = {
+          courseName: lectureInfo.code.substring(0, 6) + " (" + lectureInfo.section.substring(0,1) + ")",
+          lectureCode: lectureInfo.section.substring(0, 1) + lectureInfo.section.substring(3),
+          times: {}
+        };
+        let allTimes = lectureInfo.times;
         // The times in the original data are arrays of form [day, startTime], where day is an integer
         // between 0 and 4 that corresponds to days Monday to Friday. Eg: [0, 14] is a Monday lecture that
         // starts are 2PM. Every half hour is an individual array.
         // Loop through each individual time array and add the start time to the corresponding day.
-        for (let j=0; j < allTimes.length; j++) {
-          let day = days[(allTimes[j].timeField[0])];
+        allTimes.forEach( time => {
+          let day = days[(time.timeField[0])];
           if (!lecture.times[day]) {
-            lecture.times[day] = [allTimes[j].timeField[1]];
+            lecture.times[day] = [time.timeField[1]];
           }
           else {
-            lecture.times[day].push(allTimes[j].timeField[1]);
+            lecture.times[day].push(time.timeField[1]);
           }
-        }
+        });
         // Loop through each day of the week and remove the half hour increments.
         // Only keep start time and end time in the array.
         for (let weekday in lecture.times) {
           let times = []
           if (lecture.times.hasOwnProperty(weekday)) {
             times.push(lecture.times[weekday][0]);
-            for (let hour=1; hour < lecture.times[weekday].length; hour++) {
+            for (let hour = 1; hour < lecture.times[weekday].length; hour++) {
               if (hour+1 >= lecture.times[weekday].length ||
-                lecture.times[weekday][hour+1] != lecture.times[weekday][hour] + 0.5) {
+                lecture.times[weekday][hour+1] !== lecture.times[weekday][hour] + 0.5) {
                 times.push(lecture.times[weekday][hour] + 0.5);
               }
             }
@@ -99,8 +106,8 @@ class Course extends React.Component {
         }
         parsedLectures.push(lecture);
       }
-    }
-    parsedLectures.sort(function (lec1, lec2) {return lec1.lectureCode > lec2.lectureCode});
+    });
+    parsedLectures.sort((lec1, lec2) => {return lec1.lectureCode > lec2.lectureCode});
     return parsedLectures;
   }
 
@@ -154,14 +161,14 @@ class Course extends React.Component {
 
 class SectionList extends React.Component {
   render() {
-    const lectureSections = this.props.lectures.map(
-      lecture => <LectureSection key={this.props.courseCode + lecture.lectureCode + this.props.section}
-                                  session={this.props.session}
-                                  courseCode={this.props.courseCode}
-                                  lecture={lecture}
-                                  addSelectedLecture={this.props.addSelectedLecture}
-                                  selectedLectures={this.props.selectedLectures}
-                                  removeSelectedLecture={this.props.removeSelectedLecture}/>)
+    const lectureSections = this.props.lectures.map(lecture =>
+      <LectureSection key={this.props.courseCode + lecture.lectureCode + this.props.section}
+                      session={this.props.session}
+                      courseCode={this.props.courseCode}
+                      lecture={lecture}
+                      addSelectedLecture={this.props.addSelectedLecture}
+                      selectedLectures={this.props.selectedLectures}
+                      removeSelectedLecture={this.props.removeSelectedLecture}/>);
     return(
       <ul className={"sectionList-" + this.props.session} id="lecture-list">
         {lectureSections}
@@ -179,16 +186,17 @@ class LectureSection extends React.Component {
   // Check whether the course is already in the selectCourses list.
   // Remove the course if it is, or add the course if it is not.
   selectLecture() {
-    let index = this.props.selectedLectures.map(lecture => lecture.course).indexOf(this.props.lecture.courseName);
-
-    while (index != -1 && (this.props.selectedLectures[index].lectureCode !== this.props.lecture.lectureCode ||
-          this.props.session !== this.props.selectedLectures[index].session)) {
-      index = this.props.selectedLectures.map(lecture => lecture.course).indexOf(this.props.lecture.courseName, index + 1);
+    let sameLecture = this.props.selectedLectures.filter((lecture) => {
+      return lecture.course === this.props.lecture.courseName && lecture.session === this.props.session &&
+        lecture.lectureCode === this.props.lecture.lectureCode
+    });
+    // If sameLecture is not an empty array, then this lecture is already selected and should be removed
+    if (sameLecture.length > 0) {
+      this.props.removeSelectedLecture(this.props.lecture.courseName, this.props.session);
+    } else {
+      this.props.addSelectedLecture(this.props.lecture.courseName, this.props.session,
+                                    this.props.lecture.lectureCode, this.props.lecture.times);
     }
-    // If index != -1, then the while loop stopped because the lectureCode and session both matched
-    (index != -1) ? this.props.removeSelectedLecture(this.props.lecture.courseName, this.props.session) :
-                    this.props.addSelectedLecture(this.props.lecture.courseName, this.props.session,
-                                                      this.props.lecture.lectureCode, this.props.lecture.times);
   }
 
   render() {
