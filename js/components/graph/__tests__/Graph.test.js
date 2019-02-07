252216e41115d699e480485fe0c038ad3c1820aa
Fullchee Zhang <fullchee@gmail.com>
2019-02-06 22:04:04 -0500

    Add expected values to jest tests

--- js/components/graph/__tests__/Graph.test.js
+++ js/components/graph/__tests__/Graph.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import graphData from "./graphData";
+import graphData from "../__mocks__/graphData";
 import Graph from '../Graph'
 
 let graph;
@@ -19,13 +19,21 @@ beforeAll(() => {
 describe('Graph Node', () => {
     it('Hovering over a course node should make a info box appear', () => {
         const csc165 = graph.find('#csc165240');
+
+        csc165.simulate('mouseout');
+
         expect(graph.exists("g#infoBox")).toBe(false);
-        expect(csc165.hasClass("takeable"));
+        expect(csc165.hasClass("takeable")).toBe(true);
         
         csc165.simulate('mouseover');
-        expect(csc165.hasClass("missing"));
+        expect(csc165.hasClass("missing")).toBe(true);
+
         expect(graph.exists("g#infoBox")).toBe(true);
-        expect(graph.exists("g#tooltip-group")).toBe(true);
+        expect(graph.exists("g.tooltip-group")).toBe(true);
+
+        csc165.simulate('mouseout');
+        expect(csc165.hasClass("takeable"))
+        expect(graph.exists("g#infoBox")).toBe(false);
 
     });
     it('Pressing on the info box should create a new pop up', () => {
@@ -36,27 +44,28 @@ describe('Graph Node', () => {
         const hybridCSC165 = graph.find("g#h68"); // hardcoded
         const csc165 = graph.find('g#csc165240');
         const csc165To236 = graph.find('path[d="M497.651848,69.09890799999998 497.651848,130.885308 "]')  // extremely hard-coded
-        expect(csc165.hasClass("takeable"));
-        expect(csc165To236.hasClass("inactive"));
-        expect(hybridCSC165.hasClass("inactive"));
+
+        expect(csc165.hasClass("takeable")).toBe(true);
+        expect(csc165To236.hasClass("inactive")).toBe(true);
+        expect(hybridCSC165.hasClass("inactive")).toBe(true);
 
         csc165.simulate('click');
 
         // after
-        expect(csc165.hasClass("active"));
-        expect(csc165To236.hasClass("takeable"));
-        expect(hybridCSC165.hasClass("active"));
+        expect(csc165.hasClass("active")).toBe(true);
+        expect(csc165To236.hasClass("takeable")).toBe(true);
+        expect(hybridCSC165.hasClass("active")).toBe(true);
 
         // cleanup
         csc165.simulate('click');
-        expect(csc165.hasClass("takeable"));
-        expect(csc165To236.hasClass("inactive"));
-        expect(hybridCSC165.hasClass("inactive"));
+        expect(csc165.hasClass("takeable")).toBe(true);
+        expect(csc165To236.hasClass("inactive")).toBe(true);
+        expect(hybridCSC165.hasClass("inactive")).toBe(true);
     });
 
     it("courses without the met pre-requisites should be inactive and not takeable", () => {
         const csc148 = graph.find('g#csc148');
-        expect(csc148.hasClass("inactive"));
+        expect(csc148.hasClass("inactive")).toBe(true);
     });
 
     it("Clicking and hovering over Boolean and Hybrid nodes should do nothing", () => {
