2091c27c05f1199e7206e257e4fc5b70d8bb2d24
Fullchee Zhang <fullchee@gmail.com>
2019-02-09 18:50:49 -0500

    Import smaller estonian graph

---
+++ js/components/graph/__mocks__/estonianData.js
@@ -0,0 +1,519 @@
+export default {
+  height: 349.00021,
+  width: 276.50015,
+  nodesJSON: [
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3143",
+          text: "EST100",
+          pos: [47.76304999999999, 86.80416500000001],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.442750000000004, 67.59026],
+      fill: "#99cc00",
+      id_: "est100"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3153",
+          text: "EST101",
+          pos: [47.76304999999999, 133.034163],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.442750000000004, 113.82026300000001],
+      fill: "#99cc00",
+      id_: "est101"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3163",
+          text: "EST200",
+          pos: [47.76304999999999, 179.264063],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.442750000000004, 160.050163],
+      fill: "#99cc00",
+      id_: "est200"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3173",
+          text: "EST201",
+          pos: [47.76304999999999, 224.95387300000002],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.442750000000004, 205.74006300000002],
+      fill: "#99cc00",
+      id_: "est201"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3183",
+          text: "EST300",
+          pos: [48.09155, 270.643753],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.771249999999995, 251.42985299999998],
+      fill: "#99cc00",
+      id_: "est300"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3193",
+          text: "EST400",
+          pos: [48.09155, 316.333573],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [32.771249999999995, 297.11967300000003],
+      fill: "#99cc00",
+      id_: "est400"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3203",
+          text: "EST420",
+          pos: [184.76316000000003, 270.643753],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [169.44274000000001, 251.42985299999998],
+      fill: "#99ccff",
+      id_: "est420"
+    },
+    {
+      graph: 5,
+      height: 29,
+      type_: "Node",
+      text: [
+        {
+          graph: 5,
+          rId: "text3213",
+          text: "EST421",
+          pos: [184.76316000000003, 316.333573],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 74,
+      stroke: "",
+      pos: [169.44274000000001, 297.11967300000003],
+      fill: "#99ccff",
+      id_: "est421"
+    }
+  ],
+  labelsJSON: [
+    {
+      graph: 5,
+      rId: "tspan4216-0",
+      text: "",
+      pos: [31.720894, 26.21325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4218-9",
+      text: "Estonian",
+      pos: [31.720894, 26.21325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4220-4",
+      text: "",
+      pos: [31.720894, 44.96325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4222-8",
+      text: "Language",
+      pos: [31.720894, 44.96325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4206-2",
+      text: "",
+      pos: [158.354382, 215.09519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4208-4",
+      text: "Independent",
+      pos: [158.354382, 215.09519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4210-5",
+      text: "",
+      pos: [158.354382, 233.84519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4212-5",
+      text: "Study",
+      pos: [158.354382, 233.84519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3143",
+      text: "EST100",
+      pos: [47.76304999999999, 86.80416500000001],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3153",
+      text: "EST101",
+      pos: [47.76304999999999, 133.034163],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3163",
+      text: "EST200",
+      pos: [47.76304999999999, 179.264063],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3173",
+      text: "EST201",
+      pos: [47.76304999999999, 224.95387300000002],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3183",
+      text: "EST300",
+      pos: [48.09155, 270.643753],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3193",
+      text: "EST400",
+      pos: [48.09155, 316.333573],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3203",
+      text: "EST420",
+      pos: [184.76316000000003, 270.643753],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "text3213",
+      text: "EST421",
+      pos: [184.76316000000003, 316.333573],
+      fill: "",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4216-0",
+      text: "",
+      pos: [31.720894, 26.21325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4218-9",
+      text: "Estonian",
+      pos: [31.720894, 26.21325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4220-4",
+      text: "",
+      pos: [31.720894, 44.96325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4222-8",
+      text: "Language",
+      pos: [31.720894, 44.96325999999999],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4206-2",
+      text: "",
+      pos: [158.354382, 215.09519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4208-4",
+      text: "Independent",
+      pos: [158.354382, 215.09519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4210-5",
+      text: "",
+      pos: [158.354382, 233.84519999999998],
+      fill: "#000000",
+      align: "begin"
+    },
+    {
+      graph: 5,
+      rId: "tspan4212-5",
+      text: "Study",
+      pos: [158.354382, 233.84519999999998],
+      fill: "#000000",
+      align: "begin"
+    }
+  ],
+  regionsJSON: [
+    {
+      graph: 5,
+      points: [
+        [-8.558000000000732e-3, 0.529769999999985],
+        [146.991913, 0.5302000000000362],
+        [146.991443, 348.52977],
+        [-8.558000000000732e-3, 348.52977]
+      ],
+      isRegion: true,
+      stroke: "",
+      fill: "#bbff56",
+      id_: "p314",
+      source: "",
+      target: ""
+    },
+    {
+      graph: 5,
+      points: [
+        [146.991912, 0.529769999999985],
+        [276.491442, 0.529769999999985],
+        [276.491442, 348.52977],
+        [146.991912, 348.52977]
+      ],
+      isRegion: true,
+      stroke: "",
+      fill: "#bbffff",
+      id_: "p315",
+      source: "",
+      target: ""
+    },
+    {
+      graph: 5,
+      points: [[69.44275, 96.58226300000001], [69.44275, 105.84606299999999]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p316",
+      source: "est100",
+      target: "est101"
+    },
+    {
+      graph: 5,
+      points: [[69.44275, 142.812263], [69.44275, 152.075963]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p317",
+      source: "est101",
+      target: "est200"
+    },
+    {
+      graph: 5,
+      points: [[69.44275, 189.051363], [69.44275, 197.71176300000002]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p318",
+      source: "est200",
+      target: "est201"
+    },
+    {
+      graph: 5,
+      points: [[69.54705, 234.741163], [69.60924999999999, 243.40176300000002]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p319",
+      source: "est201",
+      target: "est300"
+    },
+    {
+      graph: 5,
+      points: [
+        [106.77534999999999, 265.929863],
+        [161.40814999999998, 265.929863]
+      ],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p320",
+      source: "est300",
+      target: "est420"
+    },
+    {
+      graph: 5,
+      points: [[106.37495, 299.382963], [162.23405000000002, 280.709063]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p321",
+      source: "est400",
+      target: "est420"
+    },
+    {
+      graph: 5,
+      points: [
+        [-8.558000000000732e-3, 0.529769999999985],
+        [146.991913, 0.5302000000000362],
+        [146.991443, 348.52977],
+        [-8.558000000000732e-3, 348.52977]
+      ],
+      isRegion: true,
+      stroke: "",
+      fill: "#bbff56",
+      id_: "p314",
+      source: "",
+      target: ""
+    },
+    {
+      graph: 5,
+      points: [
+        [146.991912, 0.529769999999985],
+        [276.491442, 0.529769999999985],
+        [276.491442, 348.52977],
+        [146.991912, 348.52977]
+      ],
+      isRegion: true,
+      stroke: "",
+      fill: "#bbffff",
+      id_: "p315",
+      source: "",
+      target: ""
+    }
+  ],
+  hybridsJSON: [
+    {
+      graph: 1,
+      height: 14.7368002,
+      type_: "BoolNode",
+      text: [
+        {
+          graph: 1,
+          rId: "text512",
+          text: "and",
+          pos: [1012.903868, 246.655709],
+          fill: "",
+          align: "begin"
+        }
+      ],
+      width: 19.7600002,
+      stroke: "",
+      pos: [1020.015148, 243.51310800000002],
+      fill: "",
+      id_: "bool1"
+    }
+  ],
+  edgesJSON: [
+    {
+      graph: 1,
+      points: [[497.651848, 69.09890799999998], [497.651848, 130.885308]],
+      isRegion: false,
+      stroke: "",
+      fill: "none",
+      id_: "p1",
+      source: "csc165240",
+      target: "csc236240"
+    }
+  ]
+};
