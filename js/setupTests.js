04d1879c27b2292296e995ea706c204fc6091ade
Fullchee Zhang <fullchee@gmail.com>
2019-02-20 15:04:59 -0500

    Make divs separate in setupTests.js

--- js/setupTests.js
+++ js/setupTests.js
@@ -6,9 +6,9 @@ document.body.innerHTML = `
 <nav>
     <li>
         <a id="nav-export">Export</a>
-        <div id="fcecount"></div>
     </li>
 </nav>
-<div id="react-graph" class="react-graph"></div>`;
+<div id="react-graph" class="react-graph"></div>
+<div id="fcecount"></div>`;
 
 global.fetch = jest.fn(() => new Promise(resolve => resolve({ok: true, json: jest.fn(() => new Promise(resolve => resolve({texts: [], shapes: [], paths: [], width: 100, height: 100})))})));
