5c9149ea27d7ca41d51998d098b31b2d967cbf23
Fullchee Zhang <fullchee@gmail.com>
2019-02-28 18:43:27 -0500

    Stuck on setting up localStorage

---
+++ js/components/graph/__tests__/Graph.test.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { mount } from "enzyme";
+import Graph from "../Graph";
+
+let graph;
+
+beforeAll(() => {
+    const graphProps = {
+        edit: false,
+        initialDrawMode: "draw-node",
+        initialOnDraw: false,
+        start_blank: false
+    };
+    graph = mount(<Graph {...graphProps} />);
+});
+
+describe("Graph Node", () => {
+    it.skip("Hovering over a course node should make a info box appear", () => {
+        const csc165 = graph.find("#csc165240");
+
+        csc165.simulate("mouseout");
+
+        expect(graph.exists("g#infoBox")).toBe(false);
+        expect(csc165.hasClass("takeable")).toBe(true);
+
+        csc165.simulate("mouseover");
+        expect(csc165.hasClass("missing")).toBe(true);
+
+        expect(graph.exists("g#infoBox")).toBe(true);
+        expect(graph.exists("g.tooltip-group")).toBe(true);
+
+        csc165.simulate("mouseout");
+        expect(csc165.hasClass("takeable"));
+        expect(graph.exists("g#infoBox")).toBe(false);
+    });
+    it.skip("clicking a course node: triggers changes in the path and children nodes (including hybrid nodes)", () => {
+        // before
+        const hybridCSC165 = graph.find("g#h68"); // hardcoded
+        const csc165 = graph.find("#csc165240");
+        const csc165To236 = graph.find(
+            'path[d="M497.651848,69.09890799999998 497.651848,130.885308 "]'
+        ); // extremely hard-coded
+
+        expect(csc165.hasClass("takeable")).toBe(true);
+        expect(csc165To236.hasClass("inactive")).toBe(true);
+        expect(hybridCSC165.hasClass("inactive")).toBe(true);
+
+        csc165.simulate("click");
+
+        // after
+        expect(csc165.hasClass("active")).toBe(true);
+        expect(csc165To236.hasClass("takeable")).toBe(true);
+        expect(hybridCSC165.hasClass("active")).toBe(true);
+
+        // cleanup
+        csc165.simulate("click");
+        expect(csc165.hasClass("takeable")).toBe(true);
+        expect(csc165To236.hasClass("inactive")).toBe(true);
+        expect(hybridCSC165.hasClass("inactive")).toBe(true);
+    });
+
+    it("Pressing a course node should increase the FCE count by 0.5 if it's a half-year course", () => {
+
+    });
+    it("Pressing a course node should increase the FCE count by 1.0 if it's a full-year course", () => {
+
+    });
+});
--- js/setupTests.js
+++ js/setupTests.js
@@ -1,5 +1,24 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
+import nock from 'nock';
+import cscData from './components/graph/__mocks__/cscData';
 
-document.body.innerHTML = '<div id="react-graph" class="react-graph">...</div>'
+global.fetch = require("node-fetch");
+
+// when running with Jest, it asks for
+// http://localhost/get-json-data?graphName=Computer+Science
+nock('http://localhost/')// TODO: double check the url
+    .get('/get-json-data?graphName=Computer+Science')
+    .reply(200, () => {
+        return cscData;
+    });
+
+document.body.innerHTML = `
+<nav>
+    <li>
+        <a id="nav-export">Export</a>
+    </li>
+</nav>
+<div id="react-graph" class="react-graph"></div>
+<div id="fcecount"></div>`;
