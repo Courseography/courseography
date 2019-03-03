c1adeb479567a876c13460ecea307aeba351c4bc
Fullchee Zhang <fullchee@gmail.com>
2019-03-03 14:55:07 -0500

    Remove unneeded graph.update() call

--- js/components/graph/__tests__/Graph.test.js
+++ js/components/graph/__tests__/Graph.test.js
@@ -14,14 +14,12 @@ beforeAll(() => {
 
     // TODO: mount has the ajax call
     graph = mount(<Graph {...graphProps} />);
-    graph.update(); // force update, wait for the async fetch
 });
 
 describe("Graph Node", () => {
     it("Hovering over a course node should make a info box appear", () => {
-        graph.update();  // not ideal, basically stalling, may not work on a faster computer
-        // produces unexpected behaviour
-
+        graph.update();
+        // graph now has data
     });
     it("clicking a course node: triggers changes in the path and children nodes (including hybrid nodes)", () => {
 
