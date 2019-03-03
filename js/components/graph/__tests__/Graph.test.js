1aaf8e7f126ad0a132bd4198a37d0658b3169b77
Fullchee Zhang <fullchee@gmail.com>
2019-03-03 15:35:52 -0500

    Put graph.update() in beforeAll instead of the first test

--- js/components/graph/__tests__/Graph.test.js
+++ js/components/graph/__tests__/Graph.test.js
@@ -4,7 +4,7 @@ import Graph from "../Graph";
 
 let graph;
 
-beforeAll(() => {
+beforeAll((done) => {
     const graphProps = {
         edit: false,
         initialDrawMode: "draw-node",
@@ -14,12 +14,31 @@ beforeAll(() => {
 
     // TODO: mount has the ajax call
     graph = mount(<Graph {...graphProps} />);
+
+    // slight delay before calling update()
+    setTimeout(() => {
+        graph.update();
+
+        expect(graph.exists("Node"));
+        expect(graph.exists("Edge"));
+        done();
+    }, 100);
 });
 
 describe("Graph Node", () => {
     it("Hovering over a course node should make a info box appear", () => {
-        graph.update();
-        // graph now has data
+        const csc165 = graph.find("g#csc165240");
+        // add csc165 to the localStorage
+        csc165.simulate("mouseover");
+        csc165.simulate("mouseout");
+        
+        expect(graph.exists("g#infoBox")).toBe(false);
+
+        // TODO: why does this return false?
+        // expect(csc165.hasClass("takeable")).toBe(true);
+
+        // csc165.simulate("mouseover");
+        // console.log(graph.exists("g#infoBox"));
     });
     it("clicking a course node: triggers changes in the path and children nodes (including hybrid nodes)", () => {
 
