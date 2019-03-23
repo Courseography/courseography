c9a16a00af541c4bdcbe7a70e6bfe83a0b89cfdf
Fullchee Zhang <fullchee@gmail.com>
2019-03-23 19:38:25 -0400

    Add comment that focuses are hard-coded

--- js/components/graph/__tests__/Graph.test.js
+++ js/components/graph/__tests__/Graph.test.js
@@ -1,15 +1,30 @@
-import { cleanup} from "react-testing-library";
+import { cleanup, fireEvent } from "react-testing-library";
+import setupGraph from './setupGraph';
  
 afterEach(cleanup);
 
 // FCEs are only displayed in the sidebar component and inside the state
 describe("Graph", () => {
-    it("Pressing a course node should increase the FCE count by 0.5 if it's a half-year course", () => {
-        // TODO: we could use enzyme's render() and see if clicking a node increases or decreases the FCECount in graph.state
+    it("A new graph should start with 0 FCEs", async () => {
+        await setupGraph();
+        expect(document.getElementById('fcecount').textContent).toBe('FCE Count: 0');
+    })
+    it("Pressing a course node should increase the FCE count by 0.5 if it's a half-year course", async () => {
+        const graph = await setupGraph();
+        const aaa100 = graph.getByText("AAA100");
+        const aaa201 = graph.getByText("AAA201");
+
+        fireEvent.click(aaa100);
+        expect(document.getElementById('fcecount').textContent).toBe('FCE Count: 0.5');
+        fireEvent.click(aaa201);
+        expect(document.getElementById('fcecount').textContent).toBe('FCE Count: 1');
+        fireEvent.click(aaa100);
+        expect(document.getElementById('fcecount').textContent).toBe('FCE Count: 0.5');
+        fireEvent.click(aaa201);
+        expect(document.getElementById('fcecount').textContent).toBe('FCE Count: 0');
     });
     // TODO: March 11, 2019: there's no way to encode a 1.0 FCE and a 0.5 FCE course in the Graph
     // it("Pressing a course node should increase the FCE count by 1.0 if it's a full-year course", () => {});
-
-    it('should highlighted all the class nodes of a focus if the focus is selected', () => {});
-    it('zooming increases the size of the nodes and zooming out decreases the size of the nodes', () => {});
+    // Focuses are hard-coded in courseography/js/components/graph/sidebar/focus_descriptions
+    // it('should highlighted all the class nodes of a focus if the focus is selected', () => {});
 });
