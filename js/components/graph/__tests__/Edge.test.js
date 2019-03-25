ffe532d32d176b30d8cb19d0107182d046c8009a
Fullchee Zhang <fullchee@gmail.com>
2019-03-25 18:52:59 -0400

    Temp commit

--- js/components/graph/__tests__/Edge.test.js
+++ js/components/graph/__tests__/Edge.test.js
@@ -7,6 +7,17 @@ import { cleanup, fireEvent } from "react-testing-library";
 afterEach(cleanup);
 
 describe("Edge", () => {
+
+  function getPath(graph, name) {
+    const mapping = {
+      '102-201': 0
+      // TODO: get all of the paths here
+    };
+    if (mapping[name] === undefined) {
+      throw new Error(`Path "${name}" not found!`);
+    }
+    return graph.container.getElementsByTagName("path")[mapping[name]];
+  }
   it("should match shallow snapshot", () => {
     const edgeProps = {
       className: "path",
@@ -21,23 +32,42 @@ describe("Edge", () => {
     expect(component).toMatchSnapshot();
   });
 
-  it("with unmet pre-req should be inactive", async () => {
-    const graph = await setupGraph();
-    // console.log(prettyDOM(graph));
+  it("should be initialized with the class 'inactive'", async () => {
+    await setupGraph();
+    const paths = document.getElementsByTagName("path");
+    for (let i = 0; i < paths.length; i += 1) {
+      expect(paths[i].classList.contains("inactive")).toBe(true);
+      expect(paths[i].classList.contains("path")).toBe(true);
+    }
+  });
 
+  it("with met pre-req should be active", async () => {
+    const graph = await setupGraph();
+    const aaa102 = graph.getByText("AAA102");
+    expect(getPath(graph, '102-201').classList.contains("inactive")).toBe(true);
+
+    fireEvent.click(aaa102);
+    jest.runOnlyPendingTimers();
+    console.log(getPath(graph, '102-201'))
+    // expect(getPath(graph, '102-201').classList.contains("takeable")).toBe(true);
+    // console.log(getPath(graph, '102-201').classList)
+    // expect(getPath(graph, '102-201').classList.contains("active")).toBe(true);
   });
 
   it("with unmet re-req with selected child class should still be inactive", () => {});
 
   it("with met pre-reqs should be takeable", async () => {
     const graph = await setupGraph();
-    const aaa101 = graph.getByText("AAA101").parentNode;
+    const aaa201 = graph.getByText("AAA201").parentNode;
     // TODO: how to select an edge with RTL?
-    // const edge101 = graph.getBy???
+    // const edge102 = graph.getBy???
 
-    fireEvent.click(aaa101);
-    // TODO: expect(edge101.classList.contains('takeable')).toBeTruthy();
+    fireEvent.click(aaa201);
+    // TODO: expect(edge201.classList.contains('takeable')).toBeTruthy();
+    console.log(aaa201.classList)
   });
 
-  it("with met pre-req and a selected child class should be active", () => {});
+  it("with met pre-req and a selected child class should be active", () => {
+
+  });
 });
