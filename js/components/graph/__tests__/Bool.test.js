e9d9419e54c7c924d11c6d13abc70fcee4b4eda6
Fullchee Zhang <fullchee@gmail.com>
2019-03-29 15:36:27 -0400

    Add snapshot for OR bool

--- js/components/graph/__tests__/Bool.test.js
+++ js/components/graph/__tests__/Bool.test.js
@@ -98,6 +98,40 @@ describe("AND Bool", () => {
 });
 
 describe("OR Bool", () => {
+  it("should match shallow snapshot", () => {
+    const boolProps = {
+      JSON: {
+        fill: "",
+        graph: 1,
+        height: 14.7368002,
+        id: "bool1",
+        pos: [1020.015148, 243.51310800000002],
+        stroke: "",
+        text: [
+          {
+            align: "begin",
+            fill: "",
+            graph: 1,
+            pos: [1012.903868, 246.655709],
+            rID: "text512",
+            text: "or"
+          }
+        ],
+        type_: "BoolNode",
+        width: 19.7600002
+      },
+      childs: ["csc369", "csc358", "csc458", "csc385", "csc367"],
+      className: "bool",
+      inEdges: ["p39", "p40"],
+      logicalType: "or",
+      outEdges: ["p41", "p42", "p43", "p59", "p82"],
+      parents: ["csc209", "csc258"],
+      svg: {}
+    };
+    const wrapper = shallow(<Bool {...boolProps} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it("should not do anything when you hover or click on it", async () => {
     const graph = await setupGraph();
     const orBool = graph.getByText("or").parentNode;
