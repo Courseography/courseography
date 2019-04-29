dabb95bad391abeb56ce38aff77dc686b14cd989
Fullchee Zhang <Fullchee@users.noreply.github.com>
2019-04-29 14:33:35 -0400

    Bool tests (#1063)

--- js/setupTests.js
+++ js/setupTests.js
@@ -2,9 +2,12 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });  // enzyme
 import testData from './components/graph/__mocks__/testData';
+import aaa100CourseInfo from './components/graph/__mocks__/aaa100-course-info';
 import fetchMock from 'fetch-mock';
 
 fetchMock.get('http://localhost/get-json-data?graphName=Computer+Science', testData);
+fetchMock.get('http://localhost/course?name=aaa100H1', aaa100CourseInfo);
+fetchMock.get('/course?name=aaa100H1', aaa100CourseInfo);
 
 document.body.innerHTML = `
 <nav>
