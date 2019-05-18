820193b8da29feebf3272e19b92fcf7c6f123c81
Fullchee Zhang <fullchee@gmail.com>
2019-05-17 20:06:45 -0400

    Update Node snapshot and temporarily remove hybrid node snapshot

--- js/components/graph/__tests__/Node.test.js
+++ js/components/graph/__tests__/Node.test.js
@@ -7,55 +7,6 @@ import setupGraph from "./setupGraph";
 afterEach(cleanup);
 
 describe("Hybrid Node", () => {
-  it("node", () => {
-    const nodeProps = {
-      JSON: {
-        fill: "#888888",
-        graph: 1,
-        height: 24,
-        id_: "h46",
-        pos: [18.168848, 471.586698],
-        stroke: "",
-        text: [
-          {
-            align: "begin",
-            fill: "",
-            graph: 1,
-            pos: [20.753048200000002, 481.389408],
-            rId: "text454",
-            text: "CSC318/418/",
-            type_: "Hybrid",
-            width: 65.207497
-          },
-          {
-            align: "begin",
-            fill: "",
-            graph: 1,
-            pos: [32.698848, 493.640408],
-            rId: "text456",
-            text: "301/384",
-            length: 2,
-            type_: "Hybrid",
-            width: 65.207497
-          }
-        ],
-        type_: "Hybrid",
-        width: 65.207497
-      },
-      childs: ["csc404"],
-      className: "hybrid",
-      hybrid: true,
-      inEdges: [],
-      logicalType: "AND",
-      outEdges: ["p32"],
-      parents: [],
-      svg: {
-        onKeyDown: jest.fn()
-      }
-    };
-    const wrapper = shallow(<Node {...nodeProps} />);
-    expect(wrapper).toMatchSnapshot();
-  });
 });
 
 describe("Course Node", () => {
