b8aee419afee05aa9e6199989747505455f644ee
Fullchee Zhang <fullchee@gmail.com>
2019-06-23 12:51:49 -0400

    Move getCourse function to js folder and remove unused code

--- js/components/common/utils.js
+++ js/components/common/utils.js
@@ -9,3 +9,20 @@ export function refLookUp(targetNode, svg) {
     return svg.nodes.current[targetNode] ||
         svg.bools.current[targetNode];
 }
+
+/**
+ * Retrieves a course from file.
+ * @param {string} courseName The course code. This + '.txt' is the name of the file.
+ * @returns {Promise} Promise object representing the JSON object containing course information.
+ */
+export function getCourse(courseName) {
+    'use strict';
+
+    return fetch(
+        'course?name=' + courseName,
+        )
+        .then(response => response.json())
+        .catch(error => {
+            throw(error);
+        });
+}
