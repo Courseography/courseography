c06acf2b034543cc696bbe50e9c5f555c47191b0
Fullchee Zhang <fullchee@gmail.com>
2019-07-15 21:22:38 -0400

    Update react-testing-library

--- js/components/graph/__tests__/Node.test.js
+++ js/components/graph/__tests__/Node.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { fireEvent } from "react-testing-library";
+import { fireEvent } from "@testing-library/react";
 import Node from "../Node";
 import TestGraph from "./TestGraph";
 
