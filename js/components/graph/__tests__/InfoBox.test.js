9f4d07bb02173d6c97a06294de3456f14bd41d6a
Fullchee Zhang <Fullchee@users.noreply.github.com>
2019-08-02 08:40:41 -0400

    Infobox tests (#1104)

--- js/components/graph/__tests__/InfoBox.test.js
+++ js/components/graph/__tests__/InfoBox.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
-
 import InfoBox from "../InfoBox";
+import TestGraph from "./TestGraph";
+import { fireEvent } from "@testing-library/react";
 
 describe("InfoBox", () => {
   it("should match shallow snapshot", () => {
@@ -12,5 +13,45 @@ describe("InfoBox", () => {
     };
     const component = shallow(<InfoBox {...infoBoxProps} />);
     expect(component).toMatchSnapshot();
+  });
+
+  it("should appear when hovering over a course", async () => {
+    const graph = await TestGraph.build();
+    const aaa100 = graph.getByTestId("aaa100");
+    
+    expect(graph.textExists("Info")).toBe(false);
+    fireEvent.mouseOver(aaa100);
+    const infoBox = graph.getNodeByText("Info");
+    expect(infoBox.classList.contains("tooltip-group")).toBe(true);
+  });
+  
+  it("should disappear a second after the the cursor isn't hovered over the course", async done => {
+    const graph = await TestGraph.build();
+    const aaa100 = graph.getByTestId("aaa100");
+
+    fireEvent.mouseOver(aaa100);
+    const infoBox = graph.getNodeByText("Info");
+    expect(infoBox.classList.contains("tooltip-group")).toBe(true);
+
+    fireEvent.mouseOut(aaa100);
+
+    setTimeout(() => {
+      expect(graph.textExists("Info")).toBe(false);
+      done();
+    }, 1000);
+  });
+  it("Pressing on the info box should create a new pop up", async done => {
+    const graph = await TestGraph.build();
+    const aaa100 = graph.getByTestId("aaa100");
+    fireEvent.mouseOver(aaa100);
+    const infoBox = graph.getNodeByText("Info");
+    fireEvent.click(infoBox);
+
+    // wait for fake fetch to finish
+    setTimeout(() => {
+      // expect description in the modal box to appear
+      expect(graph.textExists(/AAA Thinking/)).toBe(true);
+      done();
+    }, 1000);
   });
 });
