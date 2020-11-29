1821d9ad8f01a0f017e34272573ebfa9186268f2
Alexander Shih <alex.alex.shih@gmail.com>
2020-11-29 10:19:25 -0500

    sidebar: Convert graph sidebar into a React component (#1143)
    Co-authored-by: stefurac <cassandra.stefura@mail.utoronto.ca>

--- app/Response/Graph.hs
+++ app/Response/Graph.hs
@@ -15,43 +15,7 @@ graphResponse =
                 []
                 (do
                     header "graph"
-                    H.div ! A.id "container" $ do
-                        H.div ! A.id "react-graph" ! A.class_ "react-graph" $ ""
-                        sideBar
+                    H.div ! A.id "container" $ ""
                     disclaimer
                 )
                 graphScripts
-
-sideBar :: H.Html
-sideBar = do
-    H.div ! A.id "sidebar" $ do
-        H.div ! A.id "fce" $ do
-            H.div ! A.id "fcecount" $ "FCE Count: 0.0"
-            H.button ! A.id "reset" $ "Reset Graph"
-        H.nav ! A.id "sidebar-nav" $ H.ul $ do
-            H.li ! A.id "graphs-nav" $
-                H.a ! A.href "" $ "Graphs"
-            H.li ! A.id "focuses-nav" $
-                H.a ! A.href "" $ "Focuses"
-        H.div ! A.id "focuses" $ do
-            H.p ! A.id "sci" ! A.class_ "focus" $ "Scientific Computing"
-            H.div ! A.id "sci-details" ! A.class_ "details" $ ""
-            H.p ! A.id "AI" ! A.class_ "focus" $ "Artificial Intelligence"
-            H.div ! A.id "AI-details" ! A.class_ "details" $ ""
-            H.p ! A.id "NLP" ! A.class_ "focus" $ "Natural Language Processing"
-            H.div ! A.id "NLP-details" ! A.class_ "details" $ ""
-            H.p ! A.id "vision" ! A.class_ "focus" $ "Computer Vision"
-            H.div ! A.id "vision-details" ! A.class_ "details" $ ""
-            H.p ! A.id "systems" ! A.class_ "focus" $ "Computer Systems"
-            H.div ! A.id "systems-details" ! A.class_ "details" $ ""
-            H.p ! A.id "game" ! A.class_ "focus" $ "Video Games"
-            H.div ! A.id "game-details" ! A.class_ "details" $ ""
-            H.p ! A.id "HCI" ! A.class_ "focus" $ "Human Computer Interaction"
-            H.div ! A.id "HCI-details" ! A.class_ "details" $ ""
-            H.p ! A.id "theory" ! A.class_ "focus" $ "Theory of Computation"
-            H.div ! A.id "theory-details" ! A.class_ "details" $ ""
-            H.p ! A.id "web" ! A.class_ "focus" $ "Web Technologies"
-            H.div ! A.id "web-details" ! A.class_ "details" $ ""
-        H.div ! A.id "graphs" $ ""
-    H.div ! A.id "sidebar-button" $
-        H.img ! A.id "sidebar-icon" ! A.src "static/res/ico/sidebar.png"
---
+++ js/components/graph/__tests__/Sidebar.test.js
@@ -0,0 +1,76 @@
+import { fireEvent, waitForDomChange } from "@testing-library/react";
+import TestSidebar from "./TestSidebar";
+import TestContainer from "./TestContainer";
+
+describe("Sidebar", () => {
+  it("should match shallow snapshot", async () => {
+    const sidebar = await TestSidebar.build();
+    expect(sidebar).toMatchSnapshot();
+  });
+
+  it("Pressing on the sidebar button should open it", async () => {
+    const sidebar = await TestSidebar.build();
+    expect(sidebar.getByTestId("test-sidebar").classList.contains("opened")).toBe(false);
+    fireEvent.click(sidebar.getByTestId("test-sidebar-button"));
+    expect(sidebar.getByTestId("test-sidebar").classList.contains("opened")).toBe(true);
+  });
+  
+  it("Focus buttons should be hidden when graphs are active", async() => {
+    const sidebar = await TestSidebar.build();
+    fireEvent.click(sidebar.getByTestId("test-sidebar-button"));
+    expect(sidebar.getByTestId("test-graphs-nav").classList.contains("active")).toBe(true);
+    expect(sidebar.getByTestId("test-graph-buttons").classList.contains("hidden")).toBe(false);
+    expect(sidebar.getByTestId("test-focuses-nav").classList.contains("active")).toBe(false);
+    expect(sidebar.getByTestId("test-focus-buttons").classList.contains("hidden")).toBe(true);
+  });
+
+  it("Graph buttons should be hidden when focuses are active", async() => {
+    const sidebar = await TestSidebar.build();
+    fireEvent.click(sidebar.getByTestId("test-sidebar-button"));
+    fireEvent.click(sidebar.getByTestId("test-focuses-nav"));
+    expect(sidebar.getByTestId("test-graphs-nav").classList.contains("active")).toBe(false);
+    expect(sidebar.getByTestId("test-graph-buttons").classList.contains("hidden")).toBe(true);
+    expect(sidebar.getByTestId("test-focuses-nav").classList.contains("active")).toBe(true);
+    expect(sidebar.getByTestId("test-focus-buttons").classList.contains("hidden")).toBe(false);
+  });
+
+  it("Focuses should be disabled for non computer science graphs", async() => {
+    const container = await TestContainer.build();
+    fireEvent.click(container.getByTestId("test-sidebar-button"));
+    expect(container.getByTestId("test-focuses-nav").classList.contains("disabled")).toBe(false);
+    fireEvent.click(container.getByTestId("test-graph-0"));
+    expect(container.getByTestId("test-focuses-nav").classList.contains("disabled")).toBe(true);
+  });
+
+  it("Clicking a graph button should increase the FCE Count", async () => {
+    const container = await TestContainer.build();
+    expect(container.getByText("FCE Count: 0.0")).toBeDefined();
+    fireEvent.click(container.getByTestId("aaa100"));
+    expect(container.getByText("FCE Count: 0.5")).toBeDefined();
+    fireEvent.click(container.getByTestId("aaa101"));
+    expect(container.getByText("FCE Count: 1.0")).toBeDefined();
+    fireEvent.click(container.getByTestId("aaa101"));
+    expect(container.getByText("FCE Count: 0.5")).toBeDefined();
+  });
+
+  it("Clicking the reset button should reset the FCE Count to 0.0", async () => {
+    const container = await TestContainer.build();
+    expect(container.getByText("FCE Count: 0.0")).toBeDefined();
+    fireEvent.click(container.getByTestId("aaa100"));
+    expect(container.getByText("FCE Count: 0.5")).toBeDefined();
+    fireEvent.click(container.getByTestId("test-reset"));
+    expect(container.getByText("FCE Count: 0.0")).toBeDefined();
+  });
+
+  it("Clicking on a graph button should change the graph", async () => {
+    const container = await TestContainer.build();
+    fireEvent.click(container.getByTestId("test-sidebar-button"));
+    expect(container.getByText("AAA100")).toBeDefined();
+    fireEvent.click(container.getByTestId("test-graph-0"));
+    // Need to wait for props update in Graph component
+    await waitForDomChange(() => {
+      expect(container.getByText("BBB100")).toBeDefined();
+      expect(container.getByText("AAA100")).toBeNull();
+    })
+  });
+});
