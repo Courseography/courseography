9829c0b71e96be43b5992f6146ddb681e8901141
David Liu <david@cs.toronto.edu>
2021-05-01 02:06:41 +0000

    setup: Upgrade to lts-17.10 and Yarn 2. (#1184)
    Also update setup instructions and CircleCI configuration.

--- scripts/setup_windows.bat
+++
@@ -1,8 +0,0 @@
-:: This script must be run as an administrator.
-
-:: First install Node.js and Yarn.
-choco install nodejs.install -y
-choco install yarn -y
-
-:: Next, install JavaScript dependencies through Yarn.
-yarn install
