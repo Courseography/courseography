a795a2c9b72c4c90a4203e4a04cf622c89300ae3
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-06-09 09:37:11 -0400

    graph: Move hybrids parents & children data to connections state (#1211)

--- js/components/graph/Graph.js
+++ js/components/graph/Graph.js
@@ -152,6 +152,12 @@ export default class Graph extends React.Component {
           outEdgesObj[node.id_] = [];
         });
 
+        hybridsList.forEach(hybrid => {
+          childrenObj[hybrid.id_] = [];
+          outEdgesObj[hybrid.id_] = [];
+          populateHybridRelatives(hybrid, nodesList, parentsObj, childrenObj);
+        })
+
         edgesList.forEach(edge => {
           if (edge.target in parentsObj) {
             parentsObj[edge.target].push(edge.source);
@@ -740,6 +746,174 @@ export default class Graph extends React.Component {
       </div>
     );
   }
+}
+
+/** Helper function for parsing hybrid node's text
+ *
+ * @param {string} s
+ * @returns {Array}
+ */
+function parseAnd(s) {
+  "use strict";
+
+  var curr = s;
+  var andList = [];
+  while (curr.length > 0) {
+    if (
+      curr.charAt(0) === "," ||
+      curr.charAt(0) === ";" ||
+      curr.charAt(0) === " "
+    ) {
+      curr = curr.substr(1);
+    } else {
+      var result = parseOr(curr);
+      if (curr === result[1]) {
+        console.error("Parsing failed for " + s + "  with curr = " + curr);
+        break;
+      } else {
+        curr = result[1];
+        andList.push(result[0]);
+      }
+    }
+  }
+  return [andList, curr];
+}
+
+/**
+ * Helper function for parsing hybrid node's text
+ * @param {string} s
+ * @returns {Array}
+ */
+function parseOr(s) {
+  "use strict";
+
+  var curr = s;
+  var orList = [];
+  var tmp;
+  var result;
+  var coursePrefix;
+  while (curr.length > 0 && curr.charAt(0) !== "," && curr.charAt(0) !== ";") {
+    if (curr.charAt(0) === "(") {
+      tmp = curr.substr(1, curr.indexOf(")"));
+      if (coursePrefix === undefined && tmp.length >= 6) {
+        coursePrefix = tmp.substr(0, 3).toUpperCase();
+      }
+      result = parseCourse(tmp, coursePrefix);
+
+      orList.append(result[0]);
+      curr = curr.substr(curr.indexOf(")") + 1);
+    } else if (curr.charAt(0) === " " || curr.charAt(0) === "/") {
+      curr = curr.substr(1);
+    } else {
+      if (coursePrefix === undefined && curr.length >= 6) {
+        coursePrefix = curr.substr(0, 3).toUpperCase();
+      }
+      result = parseCourse(curr, coursePrefix);
+      if (curr === result[1]) {
+        console.error("Parsing failed for " + s + " with curr = " + curr);
+        break;
+      }
+      curr = result[1];
+      orList.push(result[0]);
+    }
+  }
+
+  if (orList.length === 1) {
+    orList = orList[0];
+  }
+
+  return [orList, curr];
+}
+
+/**
+ * Helper function for parsing hybrid node's text
+ * @param {string} s
+ * @param {string} prefix
+ * @returns {Array}
+ */
+function parseCourse(s, prefix) {
+  "use strict";
+
+  var start = s.search(/[,/]/);
+
+  if (start === 3) {
+    return [prefix + s.substr(0, start), s.substr(start)];
+  } else if (start > 0) {
+    return [s.substr(0, start).toUpperCase(), s.substr(start)];
+  }
+
+  if (s.length === 3) {
+    return [prefix + s, ""];
+  }
+
+  return [s, ""];
+}
+
+
+/** Helper function that adds parents of hybridNode to the parents object, and adds hybrid nodes as children of the Nodes they represent
+ *
+ * @param {Node} hybridNode
+ * @param {Array} nodesJSON
+ * @param {Object} parents
+ * @param {Object} childrenObj
+ */
+function populateHybridRelatives(hybridNode, nodesJSON, parents, childrenObj){
+
+  // parse prereqs based on text
+  var hybridText = "";
+  hybridNode.text.forEach(textTag => (hybridText += textTag.text));
+  var nodeParents = [];
+  // First search for entire string (see Stats graph)
+  var prereqNode = findRelationship(hybridText, nodesJSON);
+  if (prereqNode !== undefined) {
+    nodeParents.push(prereqNode.id_);
+    childrenObj[prereqNode.id_].push(hybridNode.id_);
+  } else {
+    // Parse text first
+    var prereqs = parseAnd(hybridText)[0];
+    prereqs.forEach(course => {
+      if (typeof course === "string") {
+        prereqNode = findRelationship(course, nodesJSON);
+        if (prereqNode !== undefined) {
+          nodeParents.push(prereqNode.id_);
+          childrenObj[prereqNode.id_].push(hybridNode.id_);
+        } else {
+          console.error("Could not find prereq for ", hybridText);
+        }
+      } else if (typeof course === "object") {
+        var orPrereq = [];
+        course.forEach(c => {
+          var prereqNode = findRelationship(c, nodesJSON);
+          if (prereqNode !== undefined) {
+            orPrereq.push(prereqNode.id_);
+            childrenObj[prereqNode.id_].push(hybridNode.id_);
+          } else {
+            console.error("Could not find prereq for ", hybridText);
+          }
+        });
+        if (orPrereq.length > 0) {
+          nodeParents.push(orPrereq);
+        }
+      }
+    });
+  }
+  parents[hybridNode.id_] = nodeParents;
+}
+
+/**
+ * Helper for hybrid computation. Finds the node with the same course label as the hybrid.
+ * @param  {string} course
+ * @param {Array} nodesJSON
+ * @return {Node}
+ */
+var findRelationship = (course, nodesJSON) => {
+  var nodes = nodesJSON;
+  var node = nodes.find(
+    n =>
+      n.type_ === "Node" &&
+      n.text.some(textTag => textTag.text.includes(course))
+  );
+  return node;
 }
 
 Graph.propTypes = {
--- js/components/graph/NodeGroup.js
+++ js/components/graph/NodeGroup.js
@@ -48,57 +48,9 @@ export default class NodeGroup extends React.Component {
   render() {
     var svg = this.props.svg;
     var highlightedNodes = this.props.highlightedNodes;
-    var hybridRelationships = [];
     return (
       <g id="nodes">
         {this.props.hybridsJSON.map(entry => {
-          var childs = [];
-          var outEdges = [];
-          this.props.edgesJSON.map(element => {
-            // Note: hybrids shouldn't have any in edges
-            if (entry.id_ === element.source) {
-              childs.push(element.target);
-              outEdges.push(element.id_);
-            }
-          });
-          // parse prereqs based on text
-          var hybridText = "";
-          entry.text.forEach(textTag => (hybridText += textTag.text));
-          var parents = [];
-          // First search for entire string (see Stats graph)
-          var prereqNode = this.findRelationship(hybridText);
-          if (prereqNode !== undefined) {
-            parents.push(prereqNode.id_);
-            hybridRelationships.push([prereqNode.id_, entry.id_]);
-          } else {
-            // Parse text first
-            var prereqs = parseAnd(hybridText)[0];
-            prereqs.forEach(course => {
-              if (typeof course === "string") {
-                prereqNode = this.findRelationship(course);
-                if (prereqNode !== undefined) {
-                  parents.push(prereqNode.id_);
-                  hybridRelationships.push([prereqNode.id_, entry.id_]);
-                } else {
-                  console.error("Could not find prereq for ", hybridText);
-                }
-              } else if (typeof course === "object") {
-                var orPrereq = [];
-                course.forEach(c => {
-                  var prereqNode = this.findRelationship(c);
-                  if (prereqNode !== undefined) {
-                    orPrereq.push(prereqNode.id_);
-                    hybridRelationships.push([prereqNode.id_, entry.id_]);
-                  } else {
-                    console.error("Could not find prereq for ", hybridText);
-                  }
-                });
-                if (orPrereq.length > 0) {
-                  parents.push(orPrereq);
-                }
-              }
-            });
-          }
           return (
             <Node
               JSON={entry}
@@ -106,10 +58,10 @@ export default class NodeGroup extends React.Component {
               key={entry.id_}
               hybrid={true}
               ref={this.setRefEntry(entry)}
-              parents={parents}
-              childs={childs}
+              parents={this.props.connections.parents[entry.id_]}
+              childs={this.props.connections.children[entry.id_]}
               inEdges={[]}
-              outEdges={outEdges}
+              outEdges={this.props.connections.outEdges[entry.id_]}
               svg={svg}
               logicalType={"AND"}
             />
@@ -117,11 +69,6 @@ export default class NodeGroup extends React.Component {
         })}
         {this.props.nodesJSON.map(entry => {
           var highlighted = highlightedNodes.indexOf(entry.id_) >= 0;
-          hybridRelationships.forEach(element => {
-            if (element[0] === entry.id_) {
-              this.props.connections.children[entry.id_].push(element[1]);
-            }
-          });
           return (
             <Node
               JSON={entry}
@@ -148,106 +95,7 @@ export default class NodeGroup extends React.Component {
   }
 }
 
-/**
- *
- * @param {string} s
- * @returns {Array}
- */
-function parseAnd(s) {
-  "use strict";
 
-  var curr = s;
-  var andList = [];
-  while (curr.length > 0) {
-    if (
-      curr.charAt(0) === "," ||
-      curr.charAt(0) === ";" ||
-      curr.charAt(0) === " "
-    ) {
-      curr = curr.substr(1);
-    } else {
-      var result = parseOr(curr);
-      if (curr === result[1]) {
-        console.error("Parsing failed for " + s + "  with curr = " + curr);
-        break;
-      } else {
-        curr = result[1];
-        andList.push(result[0]);
-      }
-    }
-  }
-  return [andList, curr];
-}
-
-/**
- *
- * @param {string} s
- * @returns {Array}
- */
-function parseOr(s) {
-  "use strict";
-
-  var curr = s;
-  var orList = [];
-  var tmp;
-  var result;
-  var coursePrefix;
-  while (curr.length > 0 && curr.charAt(0) !== "," && curr.charAt(0) !== ";") {
-    if (curr.charAt(0) === "(") {
-      tmp = curr.substr(1, curr.indexOf(")"));
-      if (coursePrefix === undefined && tmp.length >= 6) {
-        coursePrefix = tmp.substr(0, 3).toUpperCase();
-      }
-      result = parseCourse(tmp, coursePrefix);
-
-      orList.append(result[0]);
-      curr = curr.substr(curr.indexOf(")") + 1);
-    } else if (curr.charAt(0) === " " || curr.charAt(0) === "/") {
-      curr = curr.substr(1);
-    } else {
-      if (coursePrefix === undefined && curr.length >= 6) {
-        coursePrefix = curr.substr(0, 3).toUpperCase();
-      }
-      result = parseCourse(curr, coursePrefix);
-      if (curr === result[1]) {
-        console.error("Parsing failed for " + s + " with curr = " + curr);
-        break;
-      }
-      curr = result[1];
-      orList.push(result[0]);
-    }
-  }
-
-  if (orList.length === 1) {
-    orList = orList[0];
-  }
-
-  return [orList, curr];
-}
-
-/**
- *
- * @param {string} s
- * @param {string} prefix
- * @returns {Array}
- */
-function parseCourse(s, prefix) {
-  "use strict";
-
-  var start = s.search(/[,/]/);
-
-  if (start === 3) {
-    return [prefix + s.substr(0, start), s.substr(start)];
-  } else if (start > 0) {
-    return [s.substr(0, start).toUpperCase(), s.substr(start)];
-  }
-
-  if (s.length === 3) {
-    return [prefix + s, ""];
-  }
-
-  return [s, ""];
-}
 
 NodeGroup.propTypes = {
   edgesJSON: PropTypes.array,
