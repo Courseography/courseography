296d4986104f6e88b8ffec38cad830d4bd1a5e3b
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-07-13 17:03:39 -0400

    now disappears when not hovered

--- js/components/graph/GraphDropdown.js
+++ js/components/graph/GraphDropdown.js
@@ -3,29 +3,22 @@ import PropTypes from "prop-types";
 
 export default class GraphDropdown extends React.Component{
 
-	createDropdownColumn(columnHeader, column){
-
-		return (
-			<div>
-				{<h4>{columnHeader}</h4>}
-					<ul>
-					{column.map((graph, i) => {
-						return <li
-										key={i}
-										className="graph-dropdown-item"
-										onClick={() => this.props.updateGraph(graph.title)}
-										data-testid={"test-graph-" + i}
-										>
-											{graph.title}
-										</li>
-					})}
-
-					</ul>
-			</div>
-		);
+	generateDropdownItems(column){
+		return column.map((graph, i) => {
+			let titleStart = graph.title.lastIndexOf(")");
+			let graphDisplayTitle = (titleStart === -1) ? graph.title : graph.title.slice(titleStart + 2);
+			return <li
+							key={i}
+							className="graph-dropdown-item"
+							onClick={() => this.props.updateGraph(graph.title)}
+							data-testid={"test-graph-" + i}
+							>
+								{graphDisplayTitle}
+							</li>
+		})
 	}
 
-	createDropdown(dropdownClassName, onMouseMove, onMouseleave, updateGraph, leftOffset){
+	createDropdown(dropdownClassName, leftOffset){
 		let officialColumn = [];
 		let businessColumn = [];
 		let sciencesColumn = [];
@@ -55,42 +48,49 @@ export default class GraphDropdown extends React.Component{
 		}
 
 		this.props.graphs.forEach((graph) => {
-			console.log(graph.title);
 			graphToColumnMapping[graph.title].push(graph)
 		});
-		console.log({officialColumn});
-		console.log({businessColumn});
-		console.log({sciencesColumn});
-		console.log({socialStudiesColumn});
-		console.log({languagesColumn});
-		return (
-		<div className={dropdownClassName} style={{left:leftOffset}}>
-			<div className="official-section">
-					<h2> Official Graphs </h2>
-					{this.createDropdownColumn("Placeholder", officialColumn)}
-				</div>
-			<div className="unofficial-section">
-					<h2> Unofficial Graphs</h2>
-					<div className="unofficial-columns">
-						{this.createDropdownColumn("Business", businessColumn)}
-						{this.createDropdownColumn("Sciences", sciencesColumn)}
-						{this.createDropdownColumn("Social Studies", socialStudiesColumn)}
-						{this.createDropdownColumn("Languages", languagesColumn)}
-					</div>
-			</div>
 
-		</div>
+		return (
+		<ul
+			className={dropdownClassName}
+			style={{left:leftOffset}}
+			onMouseMove={this.props.onMouseMove}
+			onMouseLeave={this.props.onMouseLeave}
+			data-testid={"test-graph-dropdown"}
+		>
+			<div>
+				<h5>Official Graphs</h5>
+				{this.generateDropdownItems(officialColumn)}
+			</div>
+			<div>
+				<h5>(Unofficial) Sciences</h5>
+				{this.generateDropdownItems(sciencesColumn)}
+			</div>
+			<div>
+				<h5>(Unofficial) Business</h5>
+				{this.generateDropdownItems(businessColumn)}
+			</div>
+			<div>
+				<h5>(Unofficial) Social Studies</h5>
+				{this.generateDropdownItems(socialStudiesColumn)}
+			</div>
+			<div>
+				<h5>(Unofficial) Languages</h5>
+				{this.generateDropdownItems(languagesColumn)}
+			</div>
+		</ul>
+
 		);
 	}
 
 	render() {
-		let halfComponentWidth = 328;
+		let halfComponentWidth = 478;
 		let graphTabLeft = 790;
 		if (document.querySelector["a[href='/graph'"]){
 			graphTabLeft = document.querySelector("a[href='/graph'").getBoundingClientRect().left;
 		}
-		// var className = this.props.showGraphDropdown ? "graph-dropdown-display": "graph-dropdown-hidden";
-		var className = "graph-dropdown-display";
+		var className = this.props.showGraphDropdown ? "graph-dropdown-display": "graph-dropdown-hidden";
 
 		return this.createDropdown(className, this.props.onMouseMove, this.props.onMouseLeave, this.props.updateGraph, graphTabLeft - halfComponentWidth);
 	}
