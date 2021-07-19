33015d9e28e88bea760f68675c6f6f5ab93f59bc
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-07-18 21:57:01 -0400

    graph: Create Graph selector dropdown component (#1226)

--- js/components/graph/__tests__/TestGraph.js
+++ js/components/graph/__tests__/TestGraph.js
@@ -26,6 +26,8 @@ export default class TestGraph {
       graphName: "Computer Science",
       setFCECount: () => {},
       incrementFCECount: () => {},
+      graphs: [],
+      updateGraph: () => {}
     };
 
     const rtlGraph = render(<Graph {...graphProps} />);
