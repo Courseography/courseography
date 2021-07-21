ec30c2f1e1f72ecf7bc4c73c70ba259687a58166
EclipseIsDead <sidmcheath@gmail.com>
2021-07-21 19:55:38 -0400

    Finished tests

--- js/components/graph/__tests__/Sidebar.test.js
+++ js/components/graph/__tests__/Sidebar.test.js
@@ -1,14 +1,11 @@
 import { fireEvent } from "@testing-library/react";
-import React from 'react';
-import Sidebar from "../Sidebar";
-import renderer from 'react-test-renderer';
 import TestSidebar from "./TestSidebar";
 import TestContainer from "./TestContainer";
 
 describe("Sidebar", () => {
-  it("should match shallow snapshot", () => {
-    const tree = renderer.create(<Sidebar />).toJSON();
-    expect(tree).toMatchSnapshot();
+  it("should match shallow snapshot", async () => {
+    const sidebar = await TestSidebar.build();
+    expect(sidebar).toMatchSnapshot();
   });
 
   it("Pressing on the sidebar button should open it", async () => {
@@ -33,16 +30,6 @@ describe("Sidebar", () => {
     const container = await TestContainer.build();
     fireEvent.click(container.getByTestId("aaa100"));
     const sidebar = await TestSidebar.build();
-    expect(sidebar.getByText(sidebar.getByTestId("test aaa100"))).toBeDefined();
-  });
-
-  it("Clicking the reset button should reset the FCE Count to 0.0", async () => {
-    const container = await TestContainer.build();
-    expect(container.getByText("FCE Count: 0.0")).toBeDefined();
-    fireEvent.click(container.getByTestId("aaa100"));
-    expect(container.getByText("FCE Count: 0.5")).toBeDefined();
-    const sidebar = await TestSidebar.build();
-    fireEvent.click(sidebar.getAllByTestId("test-reset-sidebar"));
-    expect(container.getByText("FCE Count: 0.0")).toBeDefined();
+    expect(sidebar.getByTestId("test aaa100")).toBeDefined();
   });
 });
