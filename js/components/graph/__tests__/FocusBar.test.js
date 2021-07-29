e558994e38f966003ba199444010dda04079abec
Leen Al Lababidi <leenlab2@gmail.com>
2021-07-29 23:03:25 +0400

    Added more tests for focuse tabs

--- js/components/graph/__tests__/FocusBar.test.js
+++ js/components/graph/__tests__/FocusBar.test.js
@@ -3,22 +3,22 @@ import TestFocusBar from "./TestFocusBar";
 import TestContainer from "./TestContainer";
 
 describe("FocusBar", () => {
-    it("should match shallow snapshot", async () => {
-        const focusBar = await TestFocusBar.build();
-        expect(focusBar).toMatchSnapshot();
-    });
+  it("should match shallow snapshot", async () => {
+    const focusBar = await TestFocusBar.build();
+    expect(focusBar).toMatchSnapshot();
+  });
 
-    it("Focusbar should be disabled for non-computer science graphs", async () => {
-        const container = await TestContainer.build();
-        expect(container.getByRole("button", {name: /FOCUSES ⪢/i})).toBeInTheDocument;
-        fireEvent.click(container.getByTestId("test-graph-1"));
-        expect(container.queryByRole("button", {name: /FOCUSES ⪢/i})).toBeNull;
-    });
+  it("Focusbar should be disabled for non-computer science graphs", async () => {
+    const container = await TestContainer.build();
+    expect(container.getByRole("button", {name: /FOCUSES ⪢/i})).toBeInTheDocument;
+    fireEvent.click(container.getByTestId("test-graph-1"));
+    expect(container.queryByRole("button", {name: /FOCUSES ⪢/i})).toBeNull;
+  });
 
-    it("Pressing the focusbar button should open it", async () => {
-        const focusBar = await TestFocusBar.build();
-        expect(focusBar.queryByRole("button", {name: /Scientific Computing/i})).toBeNull;
-        fireEvent.click(focusBar.getByRole("button", {name: /FOCUSES ⪢/i}));
-        expect(focusBar.getByRole("button", {name: /Scientific Computing/i})).toBeInTheDocument;
-    });
+  it("Pressing the focusbar button should open it", async () => {
+    const focusBar = await TestFocusBar.build();
+    expect(focusBar.queryByRole("button", {name: /Scientific Computing/i})).toBeNull;
+    fireEvent.click(focusBar.getByRole("button", {name: /FOCUSES ⪢/i}));
+    expect(focusBar.getByRole("button", {name: /Scientific Computing/i})).toBeInTheDocument;
+  });
 });
--- js/components/graph/__tests__/FocusTab.test.js
+++ js/components/graph/__tests__/FocusTab.test.js
@@ -1,9 +1,29 @@
-// import FocusTab from "../FocusTab";
+import { fireEvent } from "@testing-library/react";
+import TestContainer from "./TestContainer";
+import TestFocusBar from "./TestFocusBar";
 
 describe("FocusTab", () => {
-    it("should match shallow snapshot", () => {});
-    it("When focuses are clicked, focuses should be active", () => {});
-    it("When focuses are clicked, the graph should be highlighted", () => {});
-    it("When focuses are clicked, an information button should appear", () => {});
-    it("Clicking on the information button should open a popup", () => {});
+  it("When focuses are clicked, focuses should be active", async () => {
+    const focusBar = await TestFocusBar.build();
+    fireEvent.click(focusBar.getByRole("button", {name: /FOCUSES ⪢/i}));
+    expect(focusBar.getByRole("button", {name: /scientific computing/i}).classList.contains("active-focus")).toBe(false);
+    fireEvent.click(focusBar.getByRole("button", {name: /scientific computing/i}));
+    expect(focusBar.getByRole("button", {name: /scientific computing/i}).classList.contains("active-focus")).toBe(true);
+  });
+
+  it("When focuses are clicked, the graph should be highlighted", async () => {
+    const container = await TestContainer.build();
+    expect(container.getByTestId("react-graph").classList.contains("highlight-nodes")).toBe(false);
+    fireEvent.click(container.getByRole("button", {name: /FOCUSES ⪢/i}));
+    fireEvent.click(container.getByRole("button", {name: /scientific computing/i}));
+    expect(container.getByTestId("react-graph").classList.contains("highlight-nodes")).toBe(true);
+  });
+
+  it("When focuses are clicked, an information button should appear", async () => {
+    const container = await TestContainer.build();
+    expect(container.queryByRole("button", {name: "i"})).toBeNull;
+    fireEvent.click(container.getByRole("button", {name: /FOCUSES ⪢/i}));
+    fireEvent.click(container.getByRole("button", {name: /scientific computing/i}));
+    expect(container.queryByRole("button", {name: "i"})).toBeInTheDocument;
+  });
 });
