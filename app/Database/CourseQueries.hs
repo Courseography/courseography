18122dec8f7783ff8a0b7c9679140dcba9af9dc7
ChingChang9 <chingtheprogrammer@gmail.com>
2022-08-13 17:21:26 -0400

    doc: update 304-related comments

--- app/Database/CourseQueries.hs
+++ app/Database/CourseQueries.hs
@@ -71,14 +71,10 @@ queryCourse str = do
     return $ createJSONResponse courseJSON
 
 -- | Takes a http request with a post code and sends a JSON response containing the post data
--- | if the post data has been modified since the timestamp in the request,
--- | or a 304 "Not Modified" response otherwise
 retrievePost :: T.Text -> ServerPart Response
 retrievePost = liftIO . queryPost
 
 -- | Queries the database for the post data then returns a JSON response of it
--- | if the post data has been modified since the timestamp in the request,
--- | or a 304 "Not Modified" response otherwise
 queryPost :: T.Text -> IO Response
 queryPost code = do
     postMaybe <- returnPost code
