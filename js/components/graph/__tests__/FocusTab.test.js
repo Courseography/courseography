6647bc99a04a3d984bdd1c0abddab129e98186f7
leowrites <75044178+leowrites@users.noreply.github.com>
2022-09-18 14:26:06 -0400

    add FocusTab tests

--- js/components/graph/__tests__/FocusTab.test.js
+++ js/components/graph/__tests__/FocusTab.test.js
@@ -35,4 +35,43 @@ describe("FocusTab", () => {
     expect(container.queryByRole("button", { name: /Focus Description/i }))
       .toBeInTheDocument
   })
+  it("When focuses are clicked twice, the information button should appear and disappear", async () => {
+    const container = await TestContainer.build()
+    // information button should not be in the doucment
+    expect(container.queryByRole("button", { name: /Focus Description/ })).toBeNull
+    // click on the focus button
+    fireEvent.click(container.getByRole("button", { name: /FOCUSES ⪢/i }))
+    // click on a focus
+    fireEvent.click(container.getByRole("button", { name: /scientific computing/i }))
+    // information button should appear
+    expect(container.queryByRole("button", { name: /Focus Description/i }))
+      .toBeInTheDocument
+    // click on the focus again
+    fireEvent.click(container.getByRole("button", { name: /scientific computing/i }))
+    // information button should disappear
+    expect(container.queryByRole("button", { name: /Focus Description/i })).not
+      .toBeInTheDocument
+  })
+  it("When focuses are clicked twice, the graph should be highlighted and then not highlighted", async () => {
+    const container = await TestContainer.build()
+    expect(
+      // test id is found in Graph.js which gives reference to the main graph
+      container.getByTestId("react-graph").classList.contains("highlight-nodes")
+    ).toBe(false)
+    fireEvent.click(container.getByRole("button", { name: /FOCUSES ⪢/i }))
+    fireEvent.click(container.getByRole("button", { name: /scientific computing/i }))
+    await waitFor(() =>
+      expect(
+        // check the graph contains class name "highlighted-nodes"
+        container.getByTestId("react-graph").classList.contains("highlight-nodes")
+      ).toBe(true)
+    )
+    // click again to remove the highlights
+    fireEvent.click(container.getByRole("button", { name: /scientific computing/i }))
+    await waitFor(() => {
+      expect(
+        container.getByTestId("react-graph").classList.contains("highlight-nodes")
+      ).toBe(false)
+    })
+  })
 })
