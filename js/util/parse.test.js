89bda9e01eb478ce43cd54278890704ea0a5e731
leowrites <75044178+leowrites@users.noreply.github.com>
2022-09-24 17:53:50 -0400

    added tests for parseOr, parseAnd, and parseCourse

---
+++ js/util/parse.test.js
@@ -0,0 +1,103 @@
+// Tests for parsing algorithms from js/util/util.js
+import { parseAnd, parseOr, parseCourse } from "./util"
+
+const recursiveChecker = (actual, expected) => {
+  if (typeof actual === "string") {
+    expect(actual).toMatch(expected)
+  } else if (Array.isArray(actual)) {
+    actual.forEach((arr, i) => {
+      recursiveChecker(arr, expected[i])
+    })
+  }
+}
+
+describe("parseAnd", () => {
+  test("parseAnd correctly parses courses when a comma separates two couress", () => {
+    const input = "CSC111, STA247"
+    const actual = parseAnd(input)
+    const expected = [["CSC111", "STA247"], ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseAnd correctly parses courses separated by both comma and slash", () => {
+    const input = "CSC111, MAT135/136/137"
+    const actual = parseAnd(input)
+    const expected = [["CSC111", ["MAT135", "MAT136", "MAT137"]], ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseAnd correctly parses courses separated by multiple commas and slash", () => {
+    const input = "CSC111, STA247, Calc1, MAT135/136, CSC145/CSC165, CSC108/199"
+    const actual = parseAnd(input)
+    const expected = [
+      [
+        "CSC111",
+        "STA247",
+        "CALC1",
+        ["MAT135", "MAT136"],
+        ["CSC145", "CSC165"],
+        ["CSC108", "CSC199"],
+      ],
+      "",
+    ]
+    recursiveChecker(actual, expected)
+  })
+})
+
+describe("parseOr", () => {
+  test("parseOr correctly calls returns all parsed courses", () => {
+    const input = "CSC111/207/209/258"
+    const actual = parseOr(input)
+    const expected = [["CSC111", "CSC207", "CSC209", "CSC258"], ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseOr correctly returns parsed course when a string of a single course is wrapped in paranthesis", () => {
+    const input = "(CSC207)"
+    const actual = parseOr(input)
+    const expected = ["CSC207", ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseOr correctly returns parsed course when the string only contains one course", () => {
+    const input = "CSC207"
+    const actual = parseOr(input)
+    const expected = ["CSC207", ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseOr correctly returns parsed course when a comma separates two courses and breaks after parsing the first course", () => {
+    const input = "CSC207,209"
+    const actual = parseOr(input)
+    const expected = ["CSC207", ",209"]
+    recursiveChecker(actual, expected)
+  })
+})
+
+describe("parseCourse", () => {
+  test("parseCourse correctly returns an array with a string starting with a prefix", () => {
+    const input = "CSC111/207/209/258"
+    const actual = parseCourse(input, "CSC")
+    const expected = ["CSC111", "/207/209/258"]
+    recursiveChecker(actual, expected)
+  })
+  test("parseCourse correctly returns an array with a string starting without a prefix", () => {
+    const input = "207/209/258"
+    const actual = parseCourse(input, "CSC")
+    const expected = ["CSC207", "/209/258"]
+    recursiveChecker(actual, expected)
+  })
+  test("parseCourse correctly returns an array with a string containing one course", () => {
+    const input = "207"
+    const actual = parseCourse(input, "CSC")
+    const expected = ["CSC207", ""]
+    recursiveChecker(actual, expected)
+  })
+  test("parseCourse correctly returns an array with a string with comma as separator", () => {
+    const input = "CSC207,209,236"
+    const actual = parseCourse(input, "CSC")
+    const expected = ["CSC207", ",209,236"]
+    recursiveChecker(actual, expected)
+  })
+  test("parseCourse returns an array containing two empty strings when the input s is empty", () => {
+    const input = ""
+    const actual = parseCourse(input, "CSC")
+    const expected = ["", ""]
+    recursiveChecker(actual, expected)
+  })
+})
