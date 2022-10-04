23c9aa74f25de54459bf1d06987cc3e02a249e8c
leowrites <75044178+leowrites@users.noreply.github.com>
2022-10-04 13:36:46 -0400

    fixed typo

--- js/components/graph/__tests__/populateHybridRelatives.test.js
+++ js/components/graph/__tests__/populateHybridRelatives.test.js
@@ -144,7 +144,7 @@ describe("populateHybridRelatives", () => {
           type_: "Node",
         },
         {
-          id_: "mat135136137157cacl1",
+          id_: "mat135136137157calc1",
           text: [
             {
               text: "MAT(135,136)/137/157",
@@ -159,13 +159,13 @@ describe("populateHybridRelatives", () => {
       parents = {
         csc111: [],
         csc165: [],
-        mat135136137157cacl1: [],
+        mat135136137157calc1: [],
         h62: [],
       }
       childrenObj = {
         csc111: [],
         csc165: [],
-        mat135136137157cacl1: [],
+        mat135136137157calc1: [],
       }
     })
     test("Test populateHybridRelatives should mutate parents and childrenObj \
@@ -174,13 +174,13 @@ describe("populateHybridRelatives", () => {
       const expectedParents = {
         csc111: [],
         csc165: [],
-        mat135136137157cacl1: [],
-        h62: [["csc111", "csc165", "mat135136137157cacl1"]],
+        mat135136137157calc1: [],
+        h62: [["csc111", "csc165", "mat135136137157calc1"]],
       }
       const expectedChildren = {
         csc111: ["h62"],
         csc165: ["h62"],
-        mat135136137157cacl1: ["h62"],
+        mat135136137157calc1: ["h62"],
       }
       expect(parents).toEqual(expectedParents)
       expect(childrenObj).toEqual(expectedChildren)
@@ -192,13 +192,13 @@ describe("populateHybridRelatives", () => {
       const expectedParents = {
         csc111: [],
         csc165: [],
-        mat135136137157cacl1: [],
+        mat135136137157calc1: [],
         h62: [["csc111", "csc165"]],
       }
       const expectedChildren = {
         csc111: ["h62"],
         csc165: ["h62"],
-        mat135136137157cacl1: [],
+        mat135136137157calc1: [],
       }
       populateHybridRelatives(hybridNode, nodesJSON, parents, childrenObj)
       expect(console.error).toHaveBeenCalledTimes(1)
