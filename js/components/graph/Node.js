c666a700373fb8a51c97c9c659ecaf653a7926a7
nazzaninghazzi <nazzaninghazzi@yahoo.com>
2022-11-07 11:40:51 -0500

    fixed proptypes

--- js/components/graph/Node.js
+++ js/components/graph/Node.js
@@ -295,6 +295,7 @@ Node.propTypes = {
   onMouseEnter: PropTypes.func,
   onMouseLeave: PropTypes.func,
   outEdges: PropTypes.array,
+  nodeMethods: PropTypes.object,
   updateNodeStatus: PropTypes.func,
   updateNodeSelected: PropTypes.func,
   status: PropTypes.string,
