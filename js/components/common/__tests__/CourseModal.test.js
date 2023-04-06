d6d0e85256c95da02356697ecfde5d34063035a8
nazzaninghazzi <nazzaninghazzi@yahoo.com>
2023-04-06 19:44:07 -0400

    add tests for convertToLink

---
+++ js/components/common/__tests__/CourseModal.test.js
@@ -0,0 +1,68 @@
+import React from "react"
+import {CourseModal} from "../react_modal.js"
+
+/**
+ * Test for convertToLink.
+ */
+describe("ConvertToLink", () => {
+    let actual
+    let content
+    let expected
+    const wrapper = new CourseModal({})
+
+    describe("The content has no course names", () => {
+        beforeEach(() => {
+            content = "The application of logic and proof techniques."
+        })
+        test("The content is returned as all strings", () => {
+            actual = wrapper.convertToLink(content)
+            expected = ["The", " ", "application", " ", "of", " ", "logic",
+            " ", "and", " ", "proof", " ", "techniques.", " "]
+            expect(actual).toEqual(expected)
+            
+        })
+    })
+
+    describe("The content has one course name", () => {
+        beforeEach(() => {
+            content = "Certain topics briefly mentioned in CSC165H1 may be covered."
+        })
+        test("The content is returned as one link tag and several strings", () => {
+            actual = wrapper.convertToLink(content)
+            expected = ["Certain", " ", "topics", " ", "briefly", " ", "mentioned",
+            " ", "in", " ",
+            <a
+            key="5"
+            className="course-selection"
+            onClick={() => this.clickCourseLink(word)}
+          >
+            CSC165H1
+          </a>,
+               "", " ", "may",  " ",
+            "be", " ", "covered.", " "]
+            expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected))
+        })
+    })
+
+    describe("The content has one course name followed by a symbol, such as ')' ", () => {
+        beforeEach(() => {
+            content = "(60% or higher in CSC111H1)"
+        })
+        test("The content is returned as one link tag and several strings \
+         (including the symbol after the course name", () => {
+            actual = wrapper.convertToLink(content)
+            expected = ["(60%", " ", "or", " ", "higher", " ", "in",  " ",
+            <a
+            key="4"
+            className="course-selection"
+            onClick={() => this.clickCourseLink(word)}
+          >
+            CSC111H1
+          </a>, 
+            ")",  " "]
+            expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected))
+       })
+    })
+})
+    
+        
