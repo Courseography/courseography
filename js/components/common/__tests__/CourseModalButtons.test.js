174fe7f3fdca4b59a37a4b6f23589c99c1f33ecc
MaryamTaj <tajm48822@gmail.com>
2023-06-12 12:28:24 -0400

    Adding tests to check buttons rendering when course link is clicked

--- js/components/common/__tests__/CourseModalButtons.test.js
+++ js/components/common/__tests__/CourseModalButtons.test.js
@@ -5,63 +5,27 @@ import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 describe("CourseModal", () => {
-  it("disables forward button when course link is clicked", () => {
+  it("does not render buttons when course modal is opened", () => {
     render(<CourseModal showCourseModal={true} onClose={() => {}} />)
 
     const backButton = screen.queryByRole("button", { name: /</i })
+    const forwardButton = screen.queryByRole("button", { name: />/i })
 
     expect(backButton).not.toBeInTheDocument()
+    expect(forwardButton).not.toBeInTheDocument()
     screen.debug()
   })
 
-  //    it("clicking on back button calls the infoModalBackClick callback handler", () => {
-  //        const infoModalBackClick = jest.fn()
-  //
-  //        render(
-  //              <CourseModal showCourseModal={true} onClose={() => {}} />
-  //            )
-  //
-  //        const backButton = screen.findByRole('button', { name: /</i })
-  //        userEvent.click(backButton)
-  //
-  //        expect(infoModalBackClick).toHaveBeenCalledTimes(1)
-  //    })
-  //
-  //    it("clicking on forward button calls the infoModalForwardClick callback handler", () => {
-  //        const infoModalForwardClick = jest.fn()
-  //
-  //        render(
-  //              <CourseModal showCourseModal={true} onClose={() => {}} />
-  //            )
-  //
-  //        const forwardButton = screen.getByRole('button', { name: />/i })
-  //        userEvent.click(forwardButton)
-  //
-  //        expect(infoModalForwardClick).toHaveBeenCalledTimes(1)
-  //    })
+  it("renders buttons when course link is clicked", () => {
+    render(<CourseModal showCourseModal={true} onClose={() => {}} />)
 
-  //  it("disables back button when there are no previously selected courses", () => {
-  //    render(
-  //      <CourseModal showCourseModal={true} onClose={() => {}} currVisitedIndex={0} />
-  //    )
-  //
-  //    const backButton = screen.getByRole("button", { name: /</i })
-  //
-  //    expect(backButton).toBeDisabled()
-  //  })
-  //
-  //  it("disables forward button when there are no visited courses remaining to go through", () => {
-  //    render(
-  //      <CourseModal
-  //        showCourseModal={true}
-  //        onClose={() => {}}
-  //        currVisitedCourses={["CSC111, CSC110"]}
-  //        currVisitedIndex={1}
-  //      />
-  //    )
-  //
-  //    const forwardButton = screen.getByRole("button", { name: />/i })
-  //
-  //    expect(forwardButton).toBeDisabled()
-  //  })
+    const link = screen.getByRole("link", { class: "course-selection" })
+    userEvent.click(link)
+
+    const backButton = screen.queryByRole("button", { name: /</i })
+    const forwardButton = screen.queryByRole("button", { name: />/i })
+
+    expect(backButton).toBeInTheDocument()
+    expect(forwardButton).toBeInTheDocument()
+  })
 })
