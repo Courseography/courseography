d5c30fa03f9ff0c590a7f70ea24894132aa5eaec
MaryamTaj <tajm48822@gmail.com>
2023-06-20 15:34:16 -0400

    Adding tests to check if buttons are disabled/enabled

--- js/components/common/__tests__/CourseModalButtons.test.js
+++ js/components/common/__tests__/CourseModalButtons.test.js
@@ -47,4 +47,60 @@ describe("CourseModal", () => {
 
     screen.debug(modal)
   })
+
+  it("back button is enabled when course link is clicked", async () => {
+    // create a graph
+    await TestGraph.build()
+
+    // find the course node aa100
+    const node = document.querySelector('[data-testid="aaa100"]')
+    fireEvent.mouseOver(node)
+
+    // find the infobox beside it, and click on it
+    const infobox = document.getElementById("aaa100-tooltip-rect")
+    fireEvent.click(infobox)
+
+    // find the modal
+    await waitFor(() => {
+      const modal = document.querySelector(".ReactModalPortal")
+      return modal !== null
+    })
+    const modal = document.querySelector(".ReactModalPortal")
+
+    // find the link
+    const link = screen.getByText("BBB100H1")
+    fireEvent.click(link)
+
+    // find the back button
+    const back_button = screen.getByText("<")
+    expect(back_button.disabled).toBe(false)
+  })
+
+  it("forward button is disabled when course link is clicked", async () => {
+    // create a graph
+    await TestGraph.build()
+
+    // find the course node aa100
+    const node = document.querySelector('[data-testid="aaa100"]')
+    fireEvent.mouseOver(node)
+
+    // find the infobox beside it, and click on it
+    const infobox = document.getElementById("aaa100-tooltip-rect")
+    fireEvent.click(infobox)
+
+    // find the modal
+    await waitFor(() => {
+      const modal = document.querySelector(".ReactModalPortal")
+      return modal !== null
+    })
+    const modal = document.querySelector(".ReactModalPortal")
+
+    // find the link
+    const link = screen.getByText("BBB100H1")
+    fireEvent.click(link)
+
+    // find the back button
+    const forward_button = screen.getByText(">")
+    expect(forward_button.disabled).toBe(true)
+  })
 })
