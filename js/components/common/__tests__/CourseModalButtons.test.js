042d3209299a810c8c30f9499947f23ed3c167f1
Jaeyong Lee <lee.j1533@gmail.com>
2023-06-23 11:25:43 -0400

    add test for expected modal back button behaviour

--- js/components/common/__tests__/CourseModalButtons.test.js
+++ js/components/common/__tests__/CourseModalButtons.test.js
@@ -5,11 +5,11 @@ describe("CourseModal", () => {
   beforeEach(async () => {
     await TestGraph.build()
 
-    // find the course node aa100
+    // find the course node AAA100
     const node = document.querySelector('[data-testid="aaa100"]')
     fireEvent.mouseOver(node)
 
-    // find the infobox beside it, and click on it
+    // find and click info box beside it
     const infobox = document.getElementById("aaa100-tooltip-rect")
     fireEvent.click(infobox)
   })
@@ -19,25 +19,54 @@ describe("CourseModal", () => {
     expect(document.querySelector(".info-modal-button")).toBeNull
   })
 
-  it("renders buttons when course link is clicked", async () => {
-    // find the modal
+  it("renders buttons when a course link is clicked", async () => {
     await waitFor(() => {
       const modal = document.querySelector(".ReactModalPortal")
       return modal !== null
     })
 
-    // find the link
-    const link = screen.getByText("BBB100H1")
-    fireEvent.click(link)
+    const courseLink = screen.getByText("BBB100H1")
+    fireEvent.click(courseLink)
 
-    // find the buttons
+    // check that two buttons have been rendered
     const buttons = document.querySelectorAll(".info-modal-button")
     expect(buttons.length == 2).toBe(true)
 
     // check that forward button is disabled, back button is enabled
-    const forward_button = screen.getByText(">")
-    const back_button = screen.getByText("<")
-    expect(forward_button.disabled).toBe(true)
-    expect(back_button.disabled).toBe(false)
+    const forwardButton = screen.getByText(">")
+    const backButton = screen.getByText("<")
+    expect(forwardButton.disabled).toBe(true)
+    expect(backButton.disabled).toBe(false)
+  })
+
+  it("takes user back to the previously viewed course when back button is clicked", async () => {
+    await waitFor(() => {
+      const modal = document.querySelector(".ReactModalPortal")
+      return modal !== null
+    })
+
+    const courseLink = screen.getByText("BBB100H1")
+    fireEvent.click(courseLink)
+
+    // wait for modal to update to bbb100 information
+    await waitFor(() => {
+      const updatedModal = document.querySelector(".ReactModalPortal")
+      return updatedModal
+    })
+
+    let modalHeader = document.querySelector(".modal-header")
+    expect(modalHeader.textContent).toContain("BBB100H1 Introduction to BBB Thinking")
+
+    const backButton = screen.getByText("<")
+    fireEvent.click(backButton)
+
+    // wait for modal to update back to aaa100 information
+    await waitFor(() => {
+      const updatedModal = document.querySelector(".ReactModalPortal")
+      return updatedModal
+    })
+
+    modalHeader = document.querySelector(".modal-header")
+    expect(modalHeader.textContent).toContain("AAA100 Introduction to AAA Thinking")
   })
 })
