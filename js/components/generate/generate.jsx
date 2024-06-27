6f1d8d35de5d0f5d725b6669cef7bee2061b4364
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-06-26 23:34:23 -0400

    fixed jsx tranform config issue

---
+++ js/components/generate/generate.jsx
@@ -0,0 +1,6 @@
+// import React from "react"
+// import {jsx as _jsx} from 'react/jsx-runtime';
+import ReactDOM from "react-dom"
+import GenerateForm from "./GenerateForm.js"
+
+ReactDOM.render(<GenerateForm />, document.getElementById("generateRoot"))
