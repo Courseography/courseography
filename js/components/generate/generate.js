7ac98bc750e909c3ec6e8167d6159c9ab3188fe2
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-06-26 23:56:09 -0400

    removed obsolete file and removed comments

--- js/components/generate/generate.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import GenerateForm from "./GenerateForm.js"
-
-ReactDOM.render(<GenerateForm />, document.getElementById("generateRoot"))
--- js/components/generate/generate.jsx
+++ js/components/generate/generate.jsx
@@ -1,5 +1,3 @@
-// import React from "react"
-// import {jsx as _jsx} from 'react/jsx-runtime';
 import ReactDOM from "react-dom"
 import GenerateForm from "./GenerateForm.js"
 
