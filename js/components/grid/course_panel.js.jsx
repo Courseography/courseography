ed92013de16987a546b24e293b905f927e489afe
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-06 15:13:42 -0500

    removed React import

--- js/components/grid/course_panel.js.jsx
+++ js/components/grid/course_panel.js.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react"
+import { useCallback, useState, useEffect } from "react"
 import { CourseModal } from "../common/react_modal.js.jsx"
 import { getCourse } from "../common/utils"
 
