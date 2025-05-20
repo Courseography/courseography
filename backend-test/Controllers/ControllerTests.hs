6dec96fb692a6d8402f377d8321c4861120b958f
yoonieaj <yoonie.jang@mail.utoronto.ca>
2025-05-20 12:40:49 -0400

    Remove whitespace

--- backend-test/Controllers/ControllerTests.hs
+++ backend-test/Controllers/ControllerTests.hs
@@ -8,10 +8,9 @@ Module that contains the test suites for all the controllers.
 module Controllers.ControllerTests
 (  controllerTests  ) where
 
-import Test.HUnit (Test (..))
-
 import Controllers.CourseControllerTests (courseControllerTestSuite)
 import Controllers.GraphControllerTests (graphControllerTestSuite)
+import Test.HUnit (Test (..))
 
 -- Single test encompassing all controller test suites
 controllerTests :: Test
