4c40d0dcbf029df0a3ff525271c2c526ed33b427
Aayush Karki <akarki2005@gmail.com>
2025-08-08 10:51:26 -0400

    cabal changes, test formatting fixes

--- backend-test/Controllers/GenerateControllerTests.hs
+++ backend-test/Controllers/GenerateControllerTests.hs
@@ -16,12 +16,13 @@ import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import Database.Persist.Sqlite (insert_)
+import Database.Tables (Courses (..))
 import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (testCase)
 import TestHelpers (clearDatabase, withDatabase)
 
 -- | List of test cases as (input course, course data, expected # of nodes in prereq graph)
-findAndSavePrereqsResponseTestCases :: [(T.Text, Map.Map T.Text T.Text, Integer)]
+findAndSavePrereqsResponseTestCases :: [(String, Map.Map T.Text T.Text, Integer)]
 findAndSavePrereqsResponseTestCases =
     [("CSC148",
     Map.fromList [
@@ -29,8 +30,8 @@ findAndSavePrereqsResponseTestCases =
         ("title", "Introduction to Computer Science"),
         ("description", "Abstract data types and data structures for implementing them. Linked data structures. Encapsulation and information-hiding. Object-oriented programming. Specifications. Analyzing the efficiency of programs. Recursion. This course assumes programming experience as provided by CSC108H1. Students who already have this background may consult the Computer Science Undergraduate Office for advice about skipping CSC108H1. Practical (P) sections consist of supervised work in the computing laboratory. These sections are offered when facilities are available, and attendance is required. Note: Students may request to move from CSC148H1 to CSC108H1 after the last day to add classes and before a deadline set by the course instructors, if space is available in CSC108H1 at the time of the request."),
         ("prereqs", "CSC108H1"),
-        ("exclusions", "CSC111H1/  CSC207H1/  CSC148H5/  CSCA48H3/  CSCB07H3"),
-        ("breadth", "The Physical and Mathematical Universes (5)"),
+        ("exclusions", ""),
+        ("breadth", "null"),
         ("distribution", "null"),
         ("prereqString", "CSC108H1"),
         ("coreqs", "null"),
@@ -40,7 +41,7 @@ findAndSavePrereqsResponseTestCases =
     )]
 
 -- | Run a test case (input course, expected # of nodes) on the findAndSavePrereqsResponse function.
-runfindAndSavePrereqsResponseTest :: T.Text -> Map.Map T.Text T.Text -> Integer -> TestTree
+runfindAndSavePrereqsResponseTest :: String -> Map.Map T.Text T.Text -> Integer -> TestTree
 runfindAndSavePrereqsResponseTest course courseData expected =
     testCase course $ do
         let currCourseName = fromMaybe "" $ Map.lookup "name" courseData
@@ -51,12 +52,12 @@ runfindAndSavePrereqsResponseTest course courseData expected =
                     , coursesTitle = Map.lookup "title" courseData
                     , coursesDescription = Map.lookup "description" courseData
                     , coursesPrereqs = Map.lookup "prereqs" courseData
-                    , coursesExclusions = Map.lookup "exclusions" courseData
-                    , coursesBreadth = Map.lookup "breadth" courseData
+                    , coursesExclusions = Nothing
+                    , coursesBreadth = Nothing
                     , coursesDistribution = Nothing
                     , coursesPrereqString = Map.lookup "prereqString" courseData
                     , coursesCoreqs = Nothing
-                    , coursesVideoUrls = Nothing
+                    , coursesVideoUrls = []
                     }
 
         runDb $ do
