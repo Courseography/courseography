b22d9df999df233f967093906490137fd6c3def8
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2025-09-15 22:22:34 -0400

    Refactored Database files to MVC architecture (#1604)
    * created Program file in models folder
    * created Meeting file in models folder
    * removed CourseQueries.hs and CourseInsertion.hs
    * renamed post -> program across the codebase, excluding database tables and route names

--- backend-test/Database/CourseQueriesTests.hs
+++ backend-test/Database/CourseQueriesTests.hs
@@ -13,42 +13,42 @@ import Config (runDb)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text as T
 import Data.Time (getCurrentTime)
-import Database.CourseQueries (reqsForPost)
 import Database.DataType (PostType (..))
 import Database.Persist.Sqlite (insert_)
 import Database.Tables (Post (..))
+import Models.Program (reqsForProgram)
 import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (assertEqual, testCase)
 import TestHelpers (clearDatabase, withDatabase)
 
 -- | List of test cases as (label, requirements to insert, input program, expected output)
-reqsForPostTestCases :: [(String, T.Text, T.Text, String)]
-reqsForPostTestCases =
+reqsForProgramTestCases :: [(String, T.Text, T.Text, String)]
+reqsForProgramTestCases =
     [ ("No program", "", "", "[]")
     , ("Valid program", "/CSC199H1/", "ASMAJ1689", "[\"CSC199H1\"]")
     , ("Invalid program", "", "ABCDE1234", "[]")
     ]
 
--- | Run a test case (case, requirements, input, expected output) on the reqsForPost function.
-runReqsForPostTest :: String -> T.Text -> T.Text -> String -> TestTree
-runReqsForPostTest label reqsToInsert program expected =
+-- | Run a test case (case, requirements, input, expected output) on the reqsForProgram function.
+runReqsForProgramTest :: String -> T.Text -> T.Text -> String -> TestTree
+runReqsForProgramTest label reqsToInsert program expected =
     testCase label $ do
         currentTime <- liftIO getCurrentTime
-        let testPost = Post Major "Computer Science" program "Sample post description" reqsToInsert currentTime currentTime
+        let testProgram = Post Major "Computer Science" program "Sample post description" reqsToInsert currentTime currentTime
 
         runDb $ do
             clearDatabase
-            insert_ testPost
+            insert_ testProgram
 
-        let requirements = reqsForPost testPost
+        let requirements = reqsForProgram testProgram
         let actual = show requirements
         assertEqual ("Unexpected response body for " ++ label) expected actual
 
--- | Run all the reqsForPost test cases
-runReqsForPostTests :: [TestTree]
-runReqsForPostTests = map (\(label, reqsToInsert, program, expected) -> runReqsForPostTest label reqsToInsert program expected) reqsForPostTestCases
+-- | Run all the reqsForProgram test cases
+runReqsForProgramTests :: [TestTree]
+runReqsForProgramTests = map (\(label, reqsToInsert, program, expected) -> runReqsForProgramTest label reqsToInsert program expected) reqsForProgramTestCases
 
 -- | Test suite for CourseQueries Module
 test_courseQueries :: TestTree
 test_courseQueries =
-    withDatabase "Course Queries tests" runReqsForPostTests
+    withDatabase "Course Queries tests" runReqsForProgramTests
