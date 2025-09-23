73389a85a230915df8981217b1ae2da236b3320a
Aayush Karki <akarki2005@gmail.com>
2025-09-23 15:37:07 -0400

    updated hard-coded schema and all dependent modules

--- app/Database/Migrations.hs
+++ app/Database/Migrations.hs
@@ -1,9 +1,21 @@
 module Database.Migrations
-    (renamePostTable) where
+    (migrateDatabase) where
 
-import Database.Persist.Sql (Migration, addMigration)
+import Control.Monad.Reader (MonadIO, ReaderT)
+import Database.Persist.Sql (Migration, SqlBackend, addMigration, runMigration, runMigrationUnsafe)
+import Database.Tables (migrateAll)
 
--- | Runs a SQL migration which renames the Post table to Program
-renamePostTable :: Migration
-renamePostTable = do
-    addMigration True "ALTER TABLE Post RENAME TO Program;"
+-- | Runs a migration which renames the Post tables to Program
+renamePostTables :: Migration
+renamePostTables = do
+    addMigration True "ALTER TABLE post RENAME TO program;"
+    addMigration True "ALTER TABLE postcategory RENAME TO programcategory;"
+    addMigration True "ALTER TABLE programcategory RENAME COLUMN post_id TO program_id;"
+
+-- | Migrates the database
+migrateDatabase :: MonadIO m => ReaderT SqlBackend m ()
+migrateDatabase = do
+    -- Run unsafe migrations
+    runMigrationUnsafe renamePostTables
+    -- Run safe migrations
+    runMigration migrateAll
