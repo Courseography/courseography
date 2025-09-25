9de309c36373f482f05d2a0b53507b42a5d64a21
matt-dahlgren <matthew.dahlgren@bell.net>
2025-09-25 19:57:13 -0400

    most of the attempts that I have tried

--- js/components/generate/AutocompleteDropdown.js
+++ js/components/generate/AutocompleteDropdown.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react"
 import Autocomplete from "@mui/material/Autocomplete"
 import Chip from "@mui/material/Chip"
+// import Typography from '@mui/material/Typography';
 import { useField } from "formik"
 import PropTypes from "prop-types"
+//import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// const theme = createTheme({
+//   components: {
+//     MuiChip: {
+//       styleOverrides: {
+//         label: {
+//           fontFamily: 'Trebuchet MS',
+//           color: '#76609c'
+//         }
+//       }
+//     }
+//   }
+// });
 
 export default function AutocompleteDropdown({
   name,
@@ -34,6 +49,7 @@ export default function AutocompleteDropdown({
   }, [])
 
   return (
+    // <ThemeProvider theme={theme} >
     <Autocomplete
       multiple
       onChange={(event, newValues) => {
@@ -46,10 +62,26 @@ export default function AutocompleteDropdown({
       disableCloseOnSelect
       popupIcon={null}
       sx={{ width: "100%" }}
+      // I also did a hail mary and tried adding what is on line 72 here as well and obviously it didn't work.
       renderValue={(value, getItemProps) =>
         value.map((option, index) => {
+          // I completely removed ...itemProps as it has a className element and
           const { key, ...itemProps } = getItemProps({ index })
-          return <Chip variant="outlined" label={option} key={key} {...itemProps} />
+          return (
+            <Chip
+              variant="outlined"
+              //label={<Typography variant="caption"
+              //sx={{ fontFamily: 'Trebuchet MS', color: '#76609c', fontSize: '12px'}}>
+              //{option} </Typography>}
+              label={option}
+              key={key}
+              {...itemProps}
+              // className="autocomplete-input" just wanted the same attributes was going to pick a more suiting name for the className as a whole
+              // sx={{ 'fontFamily': 'Trebuchet MS'}}
+              // sx={{ '& .MuiChip-label': {fontFamily: 'Trebuchet MS',}}} // https://mui.com/material-ui/react-chip/
+              // I also tried with style={{ fontFamily: 'Trebuchet MS'}} again obviously didn't work
+            />
+          )
         })
       }
       renderInput={params => (
@@ -66,6 +98,7 @@ export default function AutocompleteDropdown({
       )}
       {...props}
     />
+    // </ThemeProvider>
   )
 }
 
