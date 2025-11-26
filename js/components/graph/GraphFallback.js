57e09912f98f9e739d58cdc8c8dcfdb2fc03837c
Aayush Karki <akarki2005@gmail.com>
2025-11-26 11:37:02 -0500

    removed linting bug patches

--- js/components/graph/GraphFallback.js
+++ js/components/graph/GraphFallback.js
@@ -1,4 +1,3 @@
-import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"
 import PropTypes from "prop-types"
