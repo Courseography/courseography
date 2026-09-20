5ce1e0516f158b1a2548e619152c205e5f73a611
Daniel Rafailov <danielrafailov7@gmail.com>
2026-09-20 15:27:13 -0400

    Added test case for getGraphJSON when graph is not found in database

--- backend-test/Controllers/GraphControllerTests.hs
+++ backend-test/Controllers/GraphControllerTests.hs
@@ -207,9 +207,28 @@ runGetGraphJSONTest (label, (texts', shapes', paths')) =
                 assertEqual ("Shapes differ for " ++ label) shapes' (map (\shape -> shape{shapeGraph = toSqlKey 1}) parsedShapes)
                 assertEqual ("Paths differ for " ++ label) paths' (map (\path -> path{pathGraph = toSqlKey 1}) parsedPaths)
 
+-- | Run a specific test case that verifies the behaviour of getGraphJSON when graphName does not correspond to a graph in the database.
+testGraphNotFound :: TestTree
+testGraphNotFound =
+    let label = "Graph not found returns empty components"
+     in testCase label $ do
+            let graphName = "Test Graph Name"
+            runDb clearDatabase
+            response <-
+                runServerPartWith Controllers.Graph.getGraphJSON $
+                    mockGetRequest "/get-json-data" [("graphName", T.unpack graphName)] ""
+            let body = rsBody response
+            let jsonObj = parseGraphComponentsJSON body
+            case jsonObj of
+                Nothing -> assertFailure ("Maybe ([Text], [Shape], [Path]) returned as Nothing for " ++ label)
+                Just (parsedTexts, parsedShapes, parsedPaths) -> do
+                    assertEqual ("Texts differ for " ++ label) [] parsedTexts
+                    assertEqual ("Shapes differ for " ++ label) [] parsedShapes
+                    assertEqual ("Paths differ for " ++ label) [] parsedPaths
+
 -- | Run all getGraphJSON tests
 runGetGraphJSONTests :: [TestTree]
-runGetGraphJSONTests = map runGetGraphJSONTest getGraphJSONTestCases
+runGetGraphJSONTests = map runGetGraphJSONTest getGraphJSONTestCases ++ [testGraphNotFound]
 
 -- | Test suite for Graph Controller Module
 test_graphController :: TestTree
